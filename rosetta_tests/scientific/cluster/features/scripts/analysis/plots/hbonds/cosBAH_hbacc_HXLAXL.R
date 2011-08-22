5c724588d66917200a56bb4e3eeedeb7280bac6a
Andrew Leaver-Fay <aleaverfay@gmail.com>
2011-08-22 20:15:05 +0000

    Adding several feature plotting scripts for looking at the geometry of sp2 hybridized acceptors
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@44166 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: cb452a9e4a9044cbbe2c26b74d03cf498d852ca4

---
+++ rosetta_tests/scientific/cluster/features/scripts/analysis/plots/hbonds/cosBAH_hbacc_HXLAXL.R
@@ -0,0 +1,55 @@
+# -*- tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+# vi: set ts=2 noet:
+#
+# (c) Copyright Rosetta Commons Member Institutions.
+# (c) This file is part of the Rosetta software suite and is made available under license.
+# (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+# (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+# (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+check_setup()
+
+sele <-"
+SELECT
+  geom.cosBAH,
+  acc_site.HBChemType AS acc_chem_type,
+  don_site.HBChemType AS don_chem_type
+FROM
+  hbond_geom_coords AS geom,
+  hbonds AS hbond,
+  hbond_sites AS don_site,
+  hbond_sites AS acc_site
+WHERE
+  geom.cosBAH < 0 AND
+  (acc_site.HBChemType = 'hbacc_HXL' OR acc_site.HBChemType = 'hbacc_AHX') AND
+	don_site.HBChemType != 'hbdon_PBA' AND
+  hbond.struct_id = geom.struct_id AND
+  hbond.hbond_id =  geom.hbond_id AND
+  hbond.struct_id = don_site.struct_id AND
+  hbond.don_id = don_site.site_id AND
+  hbond.struct_id = acc_site.struct_id AND
+  hbond.acc_id = acc_site.site_id AND
+  ABS(don_site.resNum - acc_site.resNum) > 5;"
+
+all_geom <- query_sample_sources(sample_sources, sele)
+
+print(summary(all_geom))
+stop("STOPPING")
+
+dens <- estimate_density_1d(
+  data = all_geom,
+  ids = c("sample_source" ),
+  variable = "cosBAH")
+
+# The 'hbdon_' part of the donor labels doesn't fit so strip them out
+# dens$don_chem_type <- sub("^hbdon_", '', dens$don_chem_type)
+
+plot_id = "cosBAH_scsc_sp3acc"
+ggplot(data=dens) +
+	geom_line(aes(x=(pi-acos(x))*180/pi, y=y, colour=sample_source),size=3) +
+	#geom_indicator(aes(colour=sample_source, indicator=counts)) +
+	#facet_grid( ~ ) +
+	opts(title = "Sidechain/Sidechain Hydrogen Bonds to SP3 Acceptors. BAH Angle by Chemical Type\n(normalized for equal volume per unit distance)") +
+	scale_x_continuous(paste('Base -- Acceptor -- Hydrogen (degrees)')) +
+	scale_y_continuous("FeatureDensity")
+save_plots(plot_id, sample_sources, output_dir, output_formats)
