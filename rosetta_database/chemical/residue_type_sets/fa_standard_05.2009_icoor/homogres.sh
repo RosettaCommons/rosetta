44c05a105a33a66bb5a3ecafc2a1d25469020a84
William Sheffler <willsheffler@gmail.com>
2011-08-25 17:45:52 +0000

    added optional slightly altered residue geometries with more homogeneoes coordinates
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@44251 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 63fde17b2bc2cda1a8c4d41d7159eaffc79ccf3d

---
+++ rosetta_database/chemical/residue_type_sets/fa_standard_05.2009_icoor/homogres.sh
@@ -0,0 +1,4 @@
+rm -f residue_types
+rm -f residue_types.txt
+ln -s residue_types_HOMOGENEOUS     residue_types
+ln -s residue_types_HOMOGENEOUS.txt residue_types.txt
---
+++ rosetta_database/chemical/residue_type_sets/fa_standard_05.2009_icoor/homogres_revert.sh
@@ -0,0 +1,4 @@
+rm -f residue_types
+rm -f residue_types.txt
+ln -s residue_types_STD     residue_types
+ln -s residue_types_STD.txt residue_types.txt
