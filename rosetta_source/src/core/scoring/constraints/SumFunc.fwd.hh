c55646b97cd987c8d1a3f024e7afbf9141da2b45
Rhiju Das <rhiju@stanford.edu>
2011-08-26 02:22:26 +0000

    Another Func (needed for new rna_torsion_potential,  about to be checked in). Also adding fwd file for SumFunc.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@44271 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 1fec17cdfd00cee00785dbe318dd0f445692dc58

---
+++ rosetta_source/src/core/scoring/constraints/SumFunc.fwd.hh
@@ -0,0 +1,37 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file
+/// @brief
+
+#ifndef INCLUDED_core_scoring_constraints_SumFunc_FWD_HH
+#define INCLUDED_core_scoring_constraints_SumFunc_FWD_HH
+
+#include <utility/pointer/owning_ptr.fwd.hh>
+
+// C++ Headers
+//#include <cstdlib>
+//#include <iostream>
+//#include <map>
+//#include <utility>
+
+namespace core {
+namespace scoring {
+namespace constraints {
+
+///
+
+class SumFunc;
+typedef utility::pointer::owning_ptr< SumFunc > SumFuncOP;
+
+}
+}
+}
+
+#endif
