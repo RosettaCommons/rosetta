e3796d0cb288a0531dbf6a9491448cad615dbc51
Rhiju Das <rhiju@stanford.edu>
2011-08-26 02:04:38 +0000

    this goes with above (not sure if server will run options.py). Update to options in preparation for das lab branch integration
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@44269 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 371dafa7759b3e25d5a860ff07a080e663d54320

---
+++ rosetta_source/src/basic/options/keys/swa.OptionKeys.gen.hh
@@ -0,0 +1,43 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   basic/options/keys/swa.OptionKeys.gen.hh
+/// @brief  basic::options::OptionKeys collection
+/// @author Stuart G. Mentzer (Stuart_Mentzer@objexx.com)
+/// @author James M. Thompson (tex@u.washington.edu)
+
+#ifndef INCLUDED_basic_options_keys_swa_OptionKeys_gen_HH
+#define INCLUDED_basic_options_keys_swa_OptionKeys_gen_HH
+
+// Unit headers
+#include <basic/options/keys/OptionKeys.hh>
+
+namespace basic {
+namespace options {
+namespace OptionKeys {
+
+namespace swa { extern BooleanOptionKey const swa; }
+namespace swa { extern StringVectorOptionKey const s1; }
+namespace swa { extern StringVectorOptionKey const s2; }
+namespace swa { extern StringVectorOptionKey const silent1; }
+namespace swa { extern StringVectorOptionKey const silent2; }
+namespace swa { extern StringVectorOptionKey const tags1; }
+namespace swa { extern StringVectorOptionKey const tags2; }
+namespace swa { extern IntegerVectorOptionKey const slice_res1; }
+namespace swa { extern IntegerVectorOptionKey const slice_res2; }
+namespace swa { extern IntegerVectorOptionKey const input_res1; }
+namespace swa { extern IntegerVectorOptionKey const input_res2; }
+namespace swa { extern IntegerVectorOptionKey const input_res; }
+namespace swa { extern BooleanOptionKey const backbone_only1; }
+namespace swa { extern BooleanOptionKey const backbone_only2; }
+
+} // namespace OptionKeys
+} // namespace options
+} // namespace basic
+
+#endif
