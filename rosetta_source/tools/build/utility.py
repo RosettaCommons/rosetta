RosettaCommons/rosetta

commit 619cb6617bc92ed859f3e9a6f104c4b5a8e49a73

Fix a long standing bug with scons where each application was getting built twice. Now the app.<extra>.<platform> and app.<platform> are symlinked to the same executable in the build directory, rather than being symlinked to different copies of the same executable. This shouldn't cause any new test failures.
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@45697 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: 7501ea0b55f49a6c3c86eab6125d0e2455c37f76

rosetta_source/tools/build/utility.py
@@ -175,6 +175,34 @@ def install_links(target, source, env):
 #    os.chmod(target, stat.S_IMODE(status[stat.ST_MODE]) | stat.S_IWRITE)
     return 0
 
+def install_links_with_stripped_target(target, source, env):
+    """Install via symlink if possible, otherwise via copy.  The
+       stripped target removes the second to last part of the
+       extensions. For example:
+
+       target: app_name.default.linuxgccrelease
+       stripped_target: app_name.linuxgccrelease
+    """
+    import os
+    source = "%s/%s" % (relative_path(source, target), source)
+
+    split_target_name = str(target).split(".")
+    stripped_target = ".".join(split_target_name[:-2] + split_target_name[-1:])
+
+    if os.__dict__.has_key("symlink"):
+        os.symlink(source, target)
+        os.symlink(source, stripped_target)
+    else:
+        import shutil
+        shutil.copy2(source, target)
+        shutil.copy2(source, stripped_target)
+# XXX: Not sure if this works yet.
+#    import stat
+#    status = os.stat(source)
+#    os.chmod(target, stat.S_IMODE(status[stat.ST_MODE]) | stat.S_IWRITE)
+    return 0
+
+
 def salt(build_options, separator = "_"):
     """Generate string to disambiguate binary names for different build variants.
     Currently only 'mode' is used.

rosetta_source/src/SConscript.apps
@@ -13,6 +13,7 @@ Import("build", "project")
 # Python imports
 from tools.build.settings import Settings, ProjectSettings
 from tools.build import utility
+from os import symlink
 
 # Load the project settings.
 if ( "my" in map(str, BUILD_TARGETS) or "my_pilot_apps" in map(str, BUILD_TARGETS) ) and project == "pilot_apps" :
@@ -117,10 +118,8 @@ for entry in actual.sources:
     target =  last_list_entry[0:len(last_list_entry)-3] + "." + "".join(build.options.extras)+ "." + build.options.os + build.options.cxx + build.options.mode
   last_target = last_list_entry[0:len(last_list_entry)-3] + "." + build.options.os + build.options.cxx + build.options.mode
   exe = env.Program( target, entry )
-  default_exe = env.Program(last_target, entry)
   Alias("#bin/" + target, "#build/src/" + build.platform + "/" + target)
   Alias("#bin/" + last_target, "#build/src/" + build.platform+ "/" + target)
-  env["INSTALL"] = utility.install_links
+  env["INSTALL"] = utility.install_links_with_stripped_target
   install_bin = env.Install("#bin", exe)
-  install_bin = env.Install("#bin",default_exe)
 

commit 57c9f7c0c7651a7fae1999a205f63f2fbad0dade

Have scons attemt to delete old symlinks in #bin/ before creating them.  This fixes a bug introduced in r45697.  Thanks Sergey and Andrew for the bug report. No test failures are expected.
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@45700 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: 0658a944467a9dbf7825cc0f2aa1121735874224

rosetta_source/tools/build/utility.py
@@ -190,7 +190,12 @@ def install_links_with_stripped_target(target, source, env):
     stripped_target = ".".join(split_target_name[:-2] + split_target_name[-1:])
 
     if os.__dict__.has_key("symlink"):
+        try: os.unlink(target)
+        except: pass
         os.symlink(source, target)
+
+        try: os.unlink(stripped_target)
+        except: pass
         os.symlink(source, stripped_target)
     else:
         import shutil