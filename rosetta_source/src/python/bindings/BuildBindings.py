d69a93878ee6a2a55adad5d183e99c4cf202b726
Sergey Lyskov <sergey.lyskov@jhu.edu>
2011-12-09 19:17:32 +0000

    Fixing PyRosetta build.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46396 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 0607b65a879e15482137efc481645ce6cfd719b4

--- rosetta_source/src/python/bindings/BuildBindings.py
+++ rosetta_source/src/python/bindings/BuildBindings.py
@@ -483,10 +483,11 @@ def prepareMiniLibs(mini_path, bindings_path):
     # Now the funny part - we rebuild all libs to produce just one lib file...
     all_sources = []
     for scons_file in ['ObjexxFCL', 'utility', 'numeric', 'basic', 'core.1', 'core.2', 'core.3', 'core.4', 'core.5',
-        'protocols.1', 'protocols_a.2', 'protocols_b.2',
+        'protocols.1',
+        'protocols_a.2', 'protocols_b.2',
         'protocols.3',
-        'protocols_a.4', 'protocols_b.4', 'protocols_c.4', 'protocols_d.4', 'protocols_e.4', 'protocols_f.4',
-        'protocols_a.5', 'protocols_b.5', 'protocols_c.5',
+        'protocols_a.4', 'protocols_b.4', 'protocols_c.4', 'protocols_d.4', 'protocols_e.4', 'protocols_f.4', 'protocols_g.4', 'protocols_h.4',
+        'protocols_a.5', 'protocols_b.5', 'protocols_c.5', 'protocols_d.5', 'protocols_e.5', 'protocols_f.5',
         'protocols.6', 'protocols.7', ]:
 
     #for scons_file in ['utility']:
