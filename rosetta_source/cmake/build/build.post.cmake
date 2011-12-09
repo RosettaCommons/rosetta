26a68bfe6411c0316198332dd1c66b6d8a5aa456
Rocco Moretti <rmoretti@u.washington.edu>
2011-12-09 02:27:52 +0000

    Fix cmake build
    * Adjust rosetta_source/cmake/build/build.post.cmake to conform to new library levels (Anyone have a way to autogenerate this?)
    * Reorder rosetta_source/projects.settings to list projects in dependancy order
    * Remove leftover rosetta_source/src/protocols_c.6
    
    
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46381 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 9f59ef532c076cee456d67a6891151a0f5aca155

--- rosetta_source/cmake/build/build.post.cmake
+++ rosetta_source/cmake/build/build.post.cmake
@@ -1,7 +1,7 @@
 #SET( LIBRARIES ObjexxFCL utility numeric basic core protocols devel )
 SET( LIBRARIES
 	ObjexxFCL utility numeric basic
-	core.1 core.2 core.3 core.4 core.5 protocols.1 protocols.2 protocols.3 protocols_a.4 protocols_b.4 protocols_c.4 protocols_d.4 protocols_e.4 protocols_f.4 protocols_a.5 protocols_b.5 protocols_c.5 protocols_a.6 protocols_b.6 protocols_c.6 protocols.7 devel
+	core.1 core.2 core.3 core.4 core.5 protocols.1 protocols_a.2 protocols_b.2 protocols.3 protocols_a.4 protocols_b.4 protocols_c.4 protocols_d.4 protocols_e.4 protocols_f.4 protocols_g.4 protocols_h.4 protocols_a.5 protocols_b.5 protocols_c.5 protocols_d.5 protocols_e.5 protocols_f.5 protocols.6 protocols.7 devel
 )
 
 ## External built libraries
