d59fffa82b66fa0f7dca15947f8166c8bf1d808e
Sarel Fleishman <sarel@weizmann.ac.il>
2011-12-14 15:50:06 +0000

    adding a wrapper-mover for MembraneTopology to enable rosettascripts with membrane proteins. At this point can only set the topology through a span file
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46482 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: c29afceb22dd122a3e709671a3e2bdede9e67d3b

---
+++ rosetta_source/src/protocols/simple_moves/MembraneTopology.cc
@@ -0,0 +1,89 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 sw=2 noet:
+//
+// This file is part of the Rosetta software suite and is made available under license.
+// The Rosetta software is developed by the contributing members of the Rosetta Commons consortium.
+// (C) 199x-2009 Rosetta Commons participating institutions and developers.
+// For more information, see http://www.rosettacommons.org/.
+
+// Unit headers
+#include <protocols/simple_moves/MembraneTopology.hh>
+#include <protocols/simple_moves/MembraneTopologyCreator.hh>
+#include <core/scoring/MembraneTopology.hh>
+#include <core/pose/datacache/CacheableDataType.hh>
+#include <basic/datacache/BasicDataCache.hh>
+
+// Project Headers
+#include <core/pose/Pose.hh>
+//parsing
+#include <utility/tag/Tag.hh>
+#include <protocols/moves/Mover.fwd.hh> //Movers_map
+#include <protocols/filters/Filter.fwd.hh> //Filters_map
+#include <basic/Tracer.hh>
+
+// Utility Headers
+
+// Unit Headers
+
+// C++ headers
+
+namespace protocols {
+namespace simple_moves {
+
+using namespace std;
+
+using core::pose::Pose;
+
+static basic::Tracer TR( "protocols.simple_moves.MembraneTopology" );
+
+std::string
+MembraneTopologyCreator::keyname() const
+{
+	return MembraneTopologyCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+MembraneTopologyCreator::create_mover() const {
+	return new MembraneTopology;
+}
+
+std::string
+MembraneTopologyCreator::mover_name()
+{
+	return "MembraneTopology";
+}
+
+MembraneTopology::~MembraneTopology() {}
+
+///@brief default ctor
+MembraneTopology::MembraneTopology() :
+	parent(),
+	span_file_("")
+{}
+
+void MembraneTopology::apply( Pose & pose ) {
+	core::scoring::MembraneTopologyOP membrane_topology = new core::scoring::MembraneTopology;
+	membrane_topology->initialize( span_file_ );
+	pose.data().set( core::pose::datacache::CacheableDataType::MEMBRANE_TOPOLOGY, membrane_topology );
+	TR<<"Setting pose's membrane topology according to span file "<<span_file()<<std::endl;
+}
+
+std::string
+MembraneTopology::get_name() const {
+	return MembraneTopologyCreator::mover_name();
+}
+
+void MembraneTopology::parse_my_tag( utility::tag::TagPtr const tag,
+		protocols::moves::DataMap &,
+		protocols::filters::Filters_map const &,
+		protocols::moves::Movers_map const &,
+		Pose const & pose)
+{
+
+	span_file( tag->getOption< std::string >( "span_file" ) );
+	TR<<"Span file defined as "<<span_file()<<std::endl;
+}
+
+
+} // moves
+} // protocols
---
+++ rosetta_source/src/protocols/simple_moves/MembraneTopology.hh
@@ -0,0 +1,78 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 sw=2 noet:
+//
+// This file is part of the Rosetta software suite and is made available under license.
+// The Rosetta software is developed by the contributing members of the Rosetta Commons consortium.
+// (C) 199x-2009 Rosetta Commons participating institutions and developers.
+// For more information, see http://www.rosettacommons.org/.
+
+/// @file
+/// @brief
+
+#ifndef INCLUDED_protocols_simple_moves_MembraneTopology_hh
+#define INCLUDED_protocols_simple_moves_MembraneTopology_hh
+
+#include <protocols/simple_moves/MembraneTopology.fwd.hh>
+#include <protocols/moves/Mover.hh>
+
+// Project Headers
+#include <core/pose/Pose.fwd.hh>
+#include <core/types.hh>
+// AUTO-REMOVED #include <core/chemical/AA.hh>
+
+//parsing
+#include <utility/tag/Tag.fwd.hh>
+#include <protocols/moves/DataMap.fwd.hh>
+#include <protocols/moves/Mover.fwd.hh> //Movers_map
+#include <protocols/filters/Filter.fwd.hh> //Filters_map
+
+#include <utility/vector1.hh>
+
+
+// Utility headers
+
+// C++ headers
+
+// Unit headers
+
+// @brief Simple wrapper to MembraneTopology which lives in core/scoring
+
+namespace protocols {
+namespace simple_moves {
+
+/// @brief A mover to change one torsion angle
+class MembraneTopology : public protocols::moves::Mover
+{
+private:
+	typedef protocols::moves::Mover parent;
+public:
+	///@brief default ctor
+	MembraneTopology();
+	virtual ~MembraneTopology();
+
+	virtual void apply( core::pose::Pose & pose );
+	virtual std::string get_name() const;
+	virtual protocols::moves::MoverOP clone() const {
+		return (protocols::moves::MoverOP( new protocols::simple_moves::MembraneTopology( *this ) ) );
+	}
+	virtual protocols::moves::MoverOP fresh_instance() const {
+		return protocols::moves::MoverOP( new MembraneTopology );
+	}
+
+	void parse_my_tag( utility::tag::TagPtr const tag,
+		protocols::moves::DataMap &,
+		protocols::filters::Filters_map const &,
+		protocols::moves::Movers_map const &,
+		core::pose::Pose const & );
+	std::string span_file() const { return span_file_; }
+  void span_file( std::string const s ){ span_file_ = s; }
+
+private:
+	std::string span_file_;
+};
+
+} // moves
+} // protocols
+
+#endif //INCLUDED_protocols_simple_moves_MembraneTopology_HH_
+
