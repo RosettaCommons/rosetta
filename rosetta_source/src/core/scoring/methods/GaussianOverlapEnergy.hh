4c4e24d66ad8577b934bdd2c37ab212dc86f2f5b
Ben Borgo <bborgo@genetics.wustl.edu>
2011-12-15 18:40:13 +0000

    Added Gaussian Overlap Energy Term
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46499 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 8d2a9ba87ed34fb15cc6e873e9aeb3e91e643c79

---
+++ rosetta_source/src/core/scoring/methods/GaussianOverlapEnergy.hh
@@ -0,0 +1,128 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   core/scoring/methods/GaussianOverlapEnergy.hh
+/// @brief  energy of overlaps
+/// @author  ben
+
+
+#ifndef INCLUDED_core_scoring_methods_GaussianOverlapEnergy_hh
+#define INCLUDED_core_scoring_methods_GaussianOverlapEnergy_hh
+
+// Unit Headers
+#include <core/scoring/methods/GaussianOverlapEnergy.fwd.hh>
+
+// Package headers
+#include <core/scoring/methods/ContextIndependentTwoBodyEnergy.hh>
+#include <core/scoring/ScoreFunction.fwd.hh>
+
+#include <numeric/interpolation/spline/Interpolator.hh>
+
+// Project headers
+#include <core/pose/Pose.fwd.hh>
+#include <core/kinematics/DomainMap.fwd.hh>
+
+// Utility headers
+
+
+namespace core {
+namespace scoring {
+namespace methods {
+
+///
+class GaussianOverlapEnergy : public ContextIndependentTwoBodyEnergy  {
+public:
+	typedef ContextIndependentTwoBodyEnergy  parent;
+public:
+
+	///
+	GaussianOverlapEnergy();
+	~GaussianOverlapEnergy();
+
+
+	/// clone
+	virtual
+	EnergyMethodOP
+	clone() const;
+
+
+	/////////////////////////////////////////////////////////////////////////////
+	// scoring
+	/////////////////////////////////////////////////////////////////////////////
+
+	virtual
+	void
+	residue_pair_energy(
+		conformation::Residue const & rsd1,
+		conformation::Residue const & rsd2,
+		pose::Pose const & pose,
+		ScoreFunction const & scorefxn,
+		EnergyMap & emap
+	) const;
+
+
+	virtual
+	void
+	eval_atom_derivative(
+		id::AtomID const & atom_id,
+		pose::Pose const & pose,
+		kinematics::DomainMap const & domain_map,
+		ScoreFunction const &,
+		EnergyMap const & weights,
+		Vector & F1,
+		Vector & F2
+	) const;
+
+
+	virtual
+	Distance
+	atomic_interaction_cutoff() const;
+
+	/// @details non-virtual accessor for speed
+	Distance
+	interaction_cutoff() const;
+
+
+	virtual
+	void indicate_required_context_graphs( utility::vector1< bool > & context_graphs_required ) const;
+
+	virtual
+	bool
+	defines_intrares_energy( EnergyMap const & /*weights*/ ) const {
+		return true;
+	}
+
+	virtual
+	void
+	eval_intrares_energy(
+		conformation::Residue const & rsd,
+		pose::Pose const & pose,
+		ScoreFunction const & sfxn,
+		EnergyMap & emap
+	) const;
+
+
+	/////////////////////////////////////////////////////////////////////////////
+	// data
+	/////////////////////////////////////////////////////////////////////////////
+
+private:
+
+	numeric::interpolation::spline::InterpolatorOP interp_;
+virtual
+core::Size version() const;
+
+};
+
+
+}
+}
+}
+
+#endif // INCLUDED_core_scoring_ScoreFunction_HH
