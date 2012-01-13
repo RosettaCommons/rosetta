513f71bac64be0dcc90f0e199bef41c67b2f003a
James Thompson <tex@u.washington.edu>
2012-01-13 16:58:02 +0000

    more robust approach for calculating self-selfRMS on small coordinate systems (eg 3mers). Check for matrices that look like the identity matrix (all values compared to 3x3 identity within a tolerance of 1e-5) and return an RMS of 0 in those cases. No unit test changes, but perhaps small integration test changes for fragmentpicker_integration_demo.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46874 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: e8b563b8ba128f640580304e6983a40567859b6d

--- rosetta_source/src/numeric/model_quality/rms.cc
+++ rosetta_source/src/numeric/model_quality/rms.cc
@@ -85,6 +85,22 @@ rms_wrapper_slow_and_correct(
 	return std::sqrt(tot/natoms);
 }
 
+bool close_enough( numeric::Real val1, numeric::Real val2, numeric::Real TOL = 1e-5 ) {
+	return std::abs(val1-val2) <= TOL;
+}
+
+bool is_identity_matrix(ObjexxFCL::FArray2D< numeric::Real > uu ) {
+	for ( int ii = 1; ii <= 3; ++ii ) {
+		for ( int jj = 1; jj <= 3; ++jj ) {
+			//if ( ii == jj && !close_enough(uu(ii,jj),1) ) return false;
+			//else if ( !close_enough(uu(ii,jj),0) ) return false;
+			numeric::Real const identity_val = (ii == jj) ? 1 : 0;
+			if ( !close_enough(uu(ii,jj),identity_val) ) return false;
+		}
+	}
+	return true;
+}
+
 // Calculate an RMS based on aligned set of points in p1a and p2a composed
 // each representing a list of natoms.
 numeric::Real
@@ -97,10 +113,12 @@ rms_wrapper(
 	FArray2D< numeric::Real > uu( 3, 3, 0.0 );
 	numeric::Real ctx;
 
-	if ( natoms <= 5 ) {
-		return rms_wrapper_slow_and_correct(natoms,p1a,p2a);
-	}
 	findUU( p1a, p2a, ww, natoms, uu, ctx );
+
+	// correct for rounding error in UU calculation,
+	// usually in small coordinate systems.
+	if ( is_identity_matrix(uu) ) return 0.0;
+
 	float fast_rms;
 	calc_rms_fast( fast_rms, p1a, p2a, ww, natoms, ctx );
 	return fast_rms;
