e8d0fafff71da1a0adb04d7c10b76c4503698441
Christopher Miles <cmiles@uw.edu>
2012-01-16 20:49:30 +0000

    Two small experiments for evaluating chainbreak closing methods
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46915 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 3e85259e70e3d35ca376f268ec76baa0c377842c

---
+++ rosetta_source/src/apps/pilot/cmiles/close_loops_cart.cc
@@ -0,0 +1,64 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file apps/pilot/cmiles/close_loops_cart.cc
+/// @author Christopher Miles (cmiles@uw.edu)
+
+// Utility headers
+#include <basic/options/option.hh>
+#include <basic/options/keys/OptionKeys.hh>
+#include <basic/options/keys/constraints.OptionKeys.gen.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
+
+// Project headers
+#include <core/chemical/ChemicalManager.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/kinematics/MoveMap.hh>
+#include <core/optimization/CartesianMinimizer.hh>
+#include <core/optimization/MinimizerOptions.hh>
+#include <core/pose/Pose.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <core/scoring/constraints/util.hh>
+#include <protocols/simple_moves/SwitchResidueTypeSetMover.hh>
+
+int main(int argc, char* argv[]) {
+  using core::kinematics::MoveMap;
+  using core::optimization::CartesianMinimizer;
+  using core::optimization::MinimizerOptions;
+  using core::pose::PoseOP;
+  using core::scoring::ScoreFunctionFactory;
+  using core::scoring::ScoreFunctionOP;
+  using protocols::simple_moves::SwitchResidueTypeSetMover;
+  using namespace basic::options;
+  using namespace basic::options::OptionKeys;
+  devel::init(argc, argv);
+
+  PoseOP pose = core::import_pose::pose_from_pdb(option[OptionKeys::in::file::s]()[1]);
+  SwitchResidueTypeSetMover to_centroid(core::chemical::CENTROID);
+  to_centroid.apply(*pose);
+  pose->dump_pdb("starting_cart.pdb");
+
+  ScoreFunctionOP score = ScoreFunctionFactory::create_score_function("score4_smooth_cart");
+  core::scoring::constraints::add_constraints_from_cmdline(*pose, *score);
+  (*score)(*pose);
+
+  MinimizerOptions options_lbfgs("lbfgs_armijo_nonmonotone", 0.01, true, false, false);
+  options_lbfgs.max_iter(1000);
+
+  MoveMap mm;
+  mm.set_bb(true);
+  mm.set_chi(true);
+  mm.set_jump(true);
+
+  CartesianMinimizer minimizer;
+  minimizer.run(*pose, mm, *score, options_lbfgs);
+  pose->dump_pdb("ending_cart.pdb");
+}
---
+++ rosetta_source/src/apps/pilot/cmiles/close_loops_ccd.cc
@@ -0,0 +1,73 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file apps/pilot/cmiles/close_loops_ccd.cc
+/// @author Christopher Miles (cmiles@uw.edu)
+
+// Utility headers
+#include <basic/options/option.hh>
+#include <basic/options/keys/OptionKeys.hh>
+#include <basic/options/keys/constraints.OptionKeys.gen.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
+#include <utility/vector1.hh>
+
+// Project headers
+#include <core/chemical/ChemicalManager.hh>
+#include <core/fragment/FragmentIO.hh>
+#include <core/fragment/FragSet.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/pose/Pose.hh>
+#include <protocols/comparative_modeling/LoopRelaxMover.hh>
+#include <protocols/loops/Loops.hh>
+#include <protocols/simple_moves/SwitchResidueTypeSetMover.hh>
+
+int main(int argc, char* argv[]) {
+  using core::fragment::FragSetOP;
+  using core::fragment::FragSetOP;
+  using core::pose::PoseOP;
+  using protocols::comparative_modeling::LoopRelaxMover;
+  using protocols::loops::Loops;
+  using protocols::loops::LoopsOP;
+  using protocols::simple_moves::SwitchResidueTypeSetMover;  
+  using utility::vector1;
+  using namespace basic::options;
+  using namespace basic::options::OptionKeys;
+
+  devel::init(argc, argv);
+
+  FragSetOP frag_sm = core::fragment::FragmentIO().read_data(option[in::file::frag3]());
+  FragSetOP frag_lg = core::fragment::FragmentIO().read_data(option[in::file::frag9]());
+
+  PoseOP pose = core::import_pose::pose_from_pdb(option[OptionKeys::in::file::s]()[1]);
+  SwitchResidueTypeSetMover to_centroid(core::chemical::CENTROID);
+  to_centroid.apply(*pose);
+  pose->dump_pdb("starting_ccd.pdb");
+
+  // Choose chainbreaks automatically
+  LoopsOP empty = new protocols::loops::Loops();
+  protocols::comparative_modeling::LoopRelaxMover closure;
+  closure.remodel("quick_ccd");
+  closure.intermedrelax("no");
+  closure.refine("no");
+  closure.relax("no");
+  closure.loops(empty);
+
+  utility::vector1<core::fragment::FragSetOP> fragments;
+  fragments.push_back(frag_lg);
+  fragments.push_back(frag_sm);
+  closure.frag_libs(fragments);
+
+  // Use atom pair constraints when available
+  closure.cmd_line_csts(option[constraints::cst_fa_file].user());
+
+  // Simple kinematics
+  closure.apply(*pose);
+  pose->dump_pdb("ending_ccd.pdb");
+}
