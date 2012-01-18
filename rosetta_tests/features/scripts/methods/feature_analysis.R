d5e4a54aabd59eedc71f60f236dd88694cb61b8e
Matthew O'Meara <mattjomeara@gmail.com>
2012-01-18 15:22:47 +0000

    Adding missing file to r46932, thanks Steven Combs.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@46958 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 5684564e577522974c81e1b1a9eacbd86d19a5ef

---
+++ rosetta_tests/features/scripts/methods/feature_analysis.R
@@ -0,0 +1,62 @@
+# -*- tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+# vi: set ts=2 noet:
+#
+# (c) Copyright Rosetta Commons Member Institutions.
+# (c) This file is part of the Rosetta software suite and is made available under license.
+# (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+# (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+# (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+setClass("FeatureAnalysis",
+	representation(
+		# Unique identifier for the feature analysis
+		# e.g. if unspecified use the filename instead
+		id = "character",
+
+		# For feature analyses that come from scripts, this is
+		# the filename of the script
+		filename = "character",
+
+		# The author of the script
+		author = "character",
+
+		# A breif description of the features this script analyses
+		brief_description = "character",
+
+		# A description of the purpose of the analysis script
+		# and any specific transformations, or methods for
+		# plotting etc.
+		long_description = "character",
+
+		# Keywords can be used to locate analysis scripts
+		keywords = "vector",
+
+		# Which FeatureReporter objects need to be extracted
+		# into the feature database for each sample source in
+		# order to run this feature analysis
+		# See https://wiki.rosettacommons.org/index.php/FeaturesDatabaseSchema
+		feature_reporter_dependencies = "vector",
+
+		# The feature analysis to be run
+		run="function"),
+	prototype(
+		id = "",
+		filename="",
+		author="",
+		brief_description = "Class for feature analysis script.",
+		long_description = "Instantiate this class to create feature a feature analysis.  This is usually done in a feature analysis script. See scripts/analysis/plots/EXAMPLE_PLOT.R",
+		keywords = NA,
+		feature_reporter_dependencies = NA,
+		run=function(){print("Implement this function to preform the feature analysis script")}))
+
+
+
+# Here is an example of how to create a FeatureAnalysis object
+#z <- new("FeatureAnalysis",
+#	id ="test_feature_analysis",
+#	brief_description="breif_description",
+#	long_description="long_description",
+#	feature_reporter_dependencies=c("abc"),
+#	run=function(){print("Running test feature analysis")})
+
+
