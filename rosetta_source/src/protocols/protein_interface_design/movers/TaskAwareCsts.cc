8cdf0045489651c494d00199b9168389645c1f87
Sarel Fleishman <sarel@weizmann.ac.il>
2012-02-06 09:16:22 +0000

    adding TaskAwareCsts mover, which places coordinate constraints on calphas of all residues defined as designable by the taskoperations. This can be useful to set constraints, e.g., on one chain only. See wiki for details.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@47257 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 242826b3f054f85a31710b463bdac64ebd749c36

---
+++ rosetta_source/src/protocols/protein_interface_design/movers/TaskAwareCsts.cc
@@ -0,0 +1,122 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file protocols/protein_interface_design/movers/TaskAwareCsts.cc
+/// @brief
+/// @author Sarel Fleishman (sarelf@u.washington.edu)
+
+// Unit headers
+#include <protocols/protein_interface_design/movers/TaskAwareCsts.hh>
+#include <protocols/protein_interface_design/movers/TaskAwareCstsCreator.hh>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+// Package headers
+#include <core/pose/Pose.hh>
+#include <core/conformation/util.hh>
+#include <core/conformation/Conformation.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <basic/Tracer.hh>
+#include <core/pack/task/operation/TaskOperations.hh>
+#include <utility/tag/Tag.hh>
+#include <utility/vector1.hh>
+#include <protocols/moves/DataMap.hh>
+#include <protocols/moves/Mover.hh>
+#include <protocols/rosetta_scripts/util.hh>
+//Auto Headers
+#include <core/conformation/Residue.hh>
+#include <numeric/xyzVector.hh>
+#include <core/pack/task/operation/TaskOperations.hh>
+#include <core/scoring/constraints/CoordinateConstraint.hh>
+#include <core/scoring/constraints/HarmonicFunc.hh>
+#include <core/id/AtomID.hh>
+
+namespace protocols {
+namespace protein_interface_design {
+namespace movers {
+
+static basic::Tracer TR( "protocols.protein_interface_design.movers.TaskAwareCsts" );
+std::string
+TaskAwareCstsCreator::keyname() const
+{
+	return TaskAwareCstsCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+TaskAwareCstsCreator::create_mover() const {
+	return new TaskAwareCsts;
+}
+
+std::string
+TaskAwareCstsCreator::mover_name()
+{
+	return "TaskAwareCsts";
+}
+
+TaskAwareCsts::TaskAwareCsts() :
+	Mover( TaskAwareCstsCreator::mover_name() ),
+	task_factory_( NULL ),
+	cst_type_( "coordinate" )
+{
+}
+
+
+TaskAwareCsts::~TaskAwareCsts() {}
+
+void
+TaskAwareCsts::apply( core::pose::Pose & pose )
+{
+	using namespace protocols::rosetta_scripts;
+	using namespace core::scoring::constraints;
+	using core::id::AtomID;
+
+	ConstraintCOPs cst;
+	utility::vector1< core::Size > const designable( residue_packer_states( pose, task_factory(), true/*designable*/, false/*packable*/ ) );
+	runtime_assert( designable.size() );
+	AtomID const anchor_atom( AtomID( pose.residue( designable[ 1 ] ).atom_index( "CA" ), designable[ 1 ] ) ); // anchor to first designable CA
+	HarmonicFuncOP coord_cst_func( new HarmonicFunc( 0.0, 1.0/*sd*/ ) ); // hardwired for now
+	TR<<"Adding constraints to pose: ";
+	foreach( core::Size const resid, designable ){
+		core::conformation::Residue const rsd_i( pose.residue( resid ) );
+		if( cst_type_ == "coordinate" ){
+
+			cst.push_back( new CoordinateConstraint( AtomID( rsd_i.atom_index( "CA" ), resid ), anchor_atom, rsd_i.xyz( "CA" ), coord_cst_func ) );
+			TR<<"coordinate cst at resid: "<<resid<<'\n';
+		}
+	}
+	pose.add_constraints( cst );
+	TR.flush();
+}
+
+std::string
+TaskAwareCsts::get_name() const {
+	return TaskAwareCstsCreator::mover_name();
+}
+
+void
+TaskAwareCsts::parse_my_tag( TagPtr const tag, protocols::moves::DataMap &data, protocols::filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const & pose )
+{
+	using namespace protocols::rosetta_scripts;
+	task_factory( parse_task_operations( tag, data ) );
+	cst_type( tag->getOption< std::string >( "cst_type", "coordinate" ) );
+}
+
+protocols::moves::MoverOP
+TaskAwareCsts::clone() const {
+    return( protocols::moves::MoverOP( new TaskAwareCsts( *this ) ));
+}
+
+core::pack::task::TaskFactoryOP
+TaskAwareCsts::task_factory() const{ return task_factory_; }
+
+void
+TaskAwareCsts::task_factory( core::pack::task::TaskFactoryOP tf ){ task_factory_ = tf; }
+
+} //movers
+} //protein_interface_design
+} //protocols
---
+++ rosetta_source/src/protocols/protein_interface_design/movers/TaskAwareCsts.hh
@@ -0,0 +1,55 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file protocols/protein_interface_design/movers/TaskAwareCsts.hh
+/// @author Sarel Fleishman (sarelf@u.washington.edu)
+
+#ifndef INCLUDED_protocols_protein_interface_design_movers_TaskAwareCsts_hh
+#define INCLUDED_protocols_protein_interface_design_movers_TaskAwareCsts_hh
+#include <protocols/protein_interface_design/movers/TaskAwareCsts.fwd.hh>
+#include <core/pose/Pose.fwd.hh>
+#include <utility/tag/Tag.fwd.hh>
+#include <protocols/filters/Filter.fwd.hh>
+#include <protocols/moves/Mover.hh>
+#include <protocols/moves/DataMap.fwd.hh>
+#include <core/pack/task/TaskFactory.fwd.hh>
+
+namespace protocols {
+namespace protein_interface_design {
+namespace movers {
+
+/// @brief applies csts (currently only coordinate csts) to every designable position in pose according to taskoperations
+class TaskAwareCsts : public protocols::moves::Mover
+{
+public:
+	typedef core::pose::Pose Pose;
+public:
+	TaskAwareCsts();
+	void apply( Pose & pose );
+	virtual std::string get_name() const;
+	protocols::moves::MoverOP clone() const;
+	protocols::moves::MoverOP fresh_instance() const { return protocols::moves::MoverOP( new TaskAwareCsts ); }
+		void parse_my_tag( utility::tag::TagPtr const tag, protocols::moves::DataMap &, protocols::filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const & );
+	virtual ~TaskAwareCsts();
+	core::pack::task::TaskFactoryOP task_factory() const;
+	void task_factory( core::pack::task::TaskFactoryOP tf );
+	std::string cst_type() const{ return cst_type_; }
+	void cst_type( std::string const type ){ cst_type_ = type; }
+private:
+	core::pack::task::TaskFactoryOP task_factory_; /// dflt NULL (design all); designable residues will have the constraints applied to them.
+	std::string cst_type_; //dflt coordinate;
+};
+
+
+} // movers
+} // protein_interface_design
+} // protocols
+
+
+#endif /*INCLUDED_protocols_protein_interface_design_movers_TaskAwareCsts_HH*/
