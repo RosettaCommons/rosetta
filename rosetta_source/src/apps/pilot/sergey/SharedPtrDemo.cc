6f52d5488572294e689fba937002382e947ebbe2
Sergey Lyskov <sergey.lyskov@jhu.edu>
2012-02-17 17:06:00 +0000

    PilotApps, adding SharedPtrDemo.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@47511 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 92172e546f22872fa1abaceef3be7daca2de3cb0

---
+++ rosetta_source/src/apps/pilot/sergey/SharedPtrDemo.cc
@@ -0,0 +1,75 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+// :noTabs=false:tabSize=4:indentSize=4:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file
+///
+/// @brief  Demo for boost shared_ptr
+/// @author Sergey Lyskov
+
+
+#include <utility/pointer/owning_ptr.hh>
+#include <utility/pointer/ReferenceCount.hh>
+
+
+#include <boost/shared_ptr.hpp>
+
+#include <memory>
+
+typedef std::shared_ptr<int> intSP__;
+
+
+typedef boost::shared_ptr<int> intSP;
+typedef boost::shared_ptr<int const> intCSP;
+
+
+class Base : public utility::pointer::ReferenceCount { };
+typedef utility::pointer::owning_ptr< Base > BaseOP;
+typedef utility::pointer::owning_ptr< Base const > BaseCOP;
+typedef boost::shared_ptr<Base> BaseSP;
+typedef boost::shared_ptr<Base const> BaseCSP;
+
+class Son : public Base { };
+typedef utility::pointer::owning_ptr< Son > SonOP;
+typedef utility::pointer::owning_ptr< Son const > SonCOP;
+typedef boost::shared_ptr<Son> SonSP;
+typedef boost::shared_ptr<Son const> SonCSP;
+
+
+void foo_baseOP(BaseOP) {}
+void foo_baseCOP(BaseCOP) {}
+
+void foo_baseSP(BaseSP) {}
+void foo_baseCSP(BaseCSP) {}
+
+void foo_const_intSP(intCSP) {}
+
+int main( int argc, char * argv [] )
+{
+	intSP a;
+	foo_const_intSP(a);
+
+	BaseCOP base_cop;
+	//foo_baseOP(base_cop);  <-- Does not work...
+	foo_baseCOP(base_cop);
+
+	SonCOP son_cop;
+	//foo_baseOP(son_cop);  <-- Does not work...
+	foo_baseCOP(son_cop);
+
+	BaseCSP base_csp;
+	//foo_baseSP(base_csp);  <-- Does not work...
+	foo_baseCSP(base_csp);
+
+	SonCSP son_csp;
+	//foo_baseSP(son_csp);  <-- Does not work...
+	foo_baseCSP(son_csp);
+
+	return 0;
+}
