7399a941d0ca95bbdf1fd8329828d314be224ebb
Christopher Miles <cmiles@uw.edu>
2012-02-28 20:21:21 +0000

    Simple pilot app for running dssp on a structure
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@47658 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: bb77f887f45d563f873fd69c2d3fc083a4756afe

---
+++ rosetta_source/src/apps/pilot/cmiles/dssp.cc
@@ -0,0 +1,42 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file apps/pilot/cmiles/dssp.cc
+/// @author Christopher Miles (cmiles@uw.edu)
+
+// C/C++ headers
+#include <iostream>
+
+// Utility headers
+#include <basic/options/option.hh>
+#include <basic/options/keys/OptionKeys.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <devel/init.hh>
+
+// Project headers
+#include <core/types.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/pose/Pose.hh>
+#include <core/scoring/dssp/Dssp.hh>
+
+int main(int argc, char* argv[]) {
+  using namespace basic::options;
+  using namespace basic::options::OptionKeys;
+
+  devel::init(argc, argv);
+  core::pose::PoseCOP pose = core::import_pose::pose_from_pdb(option[OptionKeys::in::file::s]()[1]);
+
+  core::scoring::dssp::Dssp dssp(*pose);
+  dssp.dssp_reduced();
+
+  std::cout << "Residue" << "\t" << "Secondary Structure" << std::endl;
+  for (core::Size i = 1; i <= pose->total_residue(); ++i) {
+    std::cout << i << "\t" << dssp.get_dssp_secstruct(i) << std::endl;
+  }
+}
