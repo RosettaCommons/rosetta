RosettaCommons/rosetta

commit ce02d454171a4444bc3b452e8d4c2e2841009653

Minor fix on StepWiseRNA_BaseCentroidScreener.cc to resolve a memory leaking bug.
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@47981 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: eda95995f3a84f2730f222a5b08df897f29c00f9

rosetta_source/src/protocols/swa/rna/StepWiseRNA_BaseCentroidScreener.cc
@@ -96,7 +96,8 @@ namespace rna {
 	StepWiseRNA_BaseCentroidScreener::Initialize_base_stub_list( pose::Pose const & pose, bool const verbose ){
 
 		ObjexxFCL::FArray1D< bool > const & partition_definition = job_parameters_->partition_definition();
-		bool const root_partition = partition_definition( pose.fold_tree().root() );
+		Size const moving_res = job_parameters_ -> moving_res();
+		bool const moving_partition = partition_definition( moving_res );
 
 		moving_residues_.clear();
 		fixed_residues_.clear();
@@ -123,8 +124,8 @@ namespace rna {
 
 			if(is_virtual_base_( seq_num )==true) continue;
 
-			if ( partition_definition( seq_num ) == root_partition ) {
-				// This is a "fixed" residue -- on the ssame side of the moving suite as the root.
+			if ( partition_definition( seq_num ) != moving_partition ) {
+				// This is a "fixed" residue -- on the same side of the moving suite as the root.
 				fixed_residues_.push_back( seq_num );
 				if ( verbose ) std::cout << " FIXED POSITION  --> " << seq_num << std::endl;
 				is_fixed_res_( seq_num ) = true;

commit 543140b278082be20e33c0e004ba7fc29d5fedb2

Minor fix on StepWiseRNA_BaseCentroidScreener.cc to resolve bug introduced in R47981 .
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@48012 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: f7157a8a09dd36c5e93db78b7e61b6e71d2ff54c

rosetta_source/src/protocols/swa/rna/StepWiseRNA_BaseCentroidScreener.cc
@@ -96,8 +96,30 @@ namespace rna {
 	StepWiseRNA_BaseCentroidScreener::Initialize_base_stub_list( pose::Pose const & pose, bool const verbose ){
 
 		ObjexxFCL::FArray1D< bool > const & partition_definition = job_parameters_->partition_definition();
-		Size const moving_res = job_parameters_ -> moving_res();
-		bool const moving_partition = partition_definition( moving_res );
+
+		//Used to be this before March 19, 2012. Fang switched to the version below to fix a memory leak problem.
+		//bool const root_partition = partition_definition( pose.fold_tree().root() ); 
+
+		//To Fang: This version is buggy since it assumes full-length pose which is not always the case!
+		//Fang modified to this version On March 18, 2012
+		//Size const moving_res = job_parameters_ -> moving_res();
+		//bool const moving_partition = partition_definition( moving_res );
+
+		Size const working_moving_res = job_parameters_->working_moving_res();
+		utility::vector1 < core::Size > const & working_moving_partition_pos = job_parameters_->working_moving_partition_pos();
+
+		if(working_moving_partition_pos.size()==0) utility_exit_with_message("working_moving_partition_pos.size()==0!");
+
+		bool const moving_partition = partition_definition( working_moving_res ); 
+		bool const moving_partition_check = partition_definition( working_moving_partition_pos[1] );
+
+		if(moving_partition!=moving_partition_check){
+			std::cout << "working_moving_res= " << working_moving_res << std::endl;
+			Output_seq_num_list("working_moving_partition_pos= ", job_parameters_->working_moving_partition_pos(), 40);
+			std::cout << "moving_partition= " << moving_partition << std::endl;
+			std::cout << "moving_partition_check= " << moving_partition_check << std::endl;
+			utility_exit_with_message("moving_partition!=moving_partition_check!");
+		}
 
 		moving_residues_.clear();
 		fixed_residues_.clear();