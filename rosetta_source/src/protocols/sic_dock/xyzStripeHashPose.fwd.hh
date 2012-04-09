30aa985a86051bb71f6bc8aff54eaec830e03c85
William Sheffler <willsheffler@gmail.com>
2012-04-09 23:54:47 +0000

    added missing file for pilot SIC docking
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@48378 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: ee1c7db914a557a4773dc0358e8d33835652a2e0

---
+++ rosetta_source/src/protocols/sic_dock/xyzStripeHashPose.fwd.hh
@@ -0,0 +1,16 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+// :noTabs=false:tabSize=4:indentSize=4:
+
+#ifndef INCLUDED_protocols_sic_dock_xyzStripeHashPose_fwd_hh
+#define INCLUDED_protocols_sic_dock_xyzStripeHashPose_fwd_hh
+
+namespace protocols {
+  namespace sic_dock {
+
+    struct xyzStripeHashPose;
+
+  }
+}
+
+#endif
