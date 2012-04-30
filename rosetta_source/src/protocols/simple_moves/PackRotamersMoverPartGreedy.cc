8a94cd6226103de898aa0c50b2cddd5047ec342b
Sagar Khare <khares@u.washington.edu>
2012-04-30 00:59:26 +0000

    Adding a partly-greedy PackRotamers mover. Given a task and a set of target residues, this mover will first greedily choose the neighbors of these residues, and then perform the usual simulated annealing on the rest (while maintaining the identity of the greedily chosen sidechains). The greedy choices are made one by one, i.e. first convert every neighbor of a given target sidechain to Ala, choose the lowest energy neighbor rotamer, then look at the rest of the neighbors and choose the best for interacting with the two chosen so far, and so on, until you're out of neighbor positions.
    No test changes expected.
     
    
    
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@48693 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: d96418090ce95470f82438989477ea5203596d96

---
+++ rosetta_source/src/protocols/simple_moves/PackRotamersMoverPartGreedy.cc
@@ -0,0 +1,354 @@
+/// @file
+/// @brief Partly greedy pack rotamers mover. given a set of target residues, choose their neighbors greedily and then
+/// @brief pack with usual simulated annealing maintaining these neighbor choices
+/// @author Sagar Khare (khares@uw.edu)
+
+// Unit headers
+#include <protocols/simple_moves/PackRotamersMoverPartGreedy.hh>
+#include <protocols/simple_moves/PackRotamersMover.hh>
+#include <protocols/simple_moves/PackRotamersMoverPartGreedyCreator.hh>
+
+#include <protocols/moves/DataMap.hh>
+#include <protocols/rosetta_scripts/util.hh>
+
+#include <core/pack/task/PackerTask.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <core/pack/task/operation/TaskOperations.hh>
+#include <core/kinematics/MoveMap.hh>
+#include <core/pose/Pose.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <core/pack/task/IGEdgeReweightContainer.hh>
+#include <protocols/toolbox/IGEdgeReweighters.hh>
+#include <protocols/toolbox/pose_manipulation.hh>
+#include <protocols/simple_moves/MinMover.hh>
+#include <core/scoring/Energies.hh>
+#include <core/conformation/Residue.hh>
+#include <basic/Tracer.hh>
+#include <protocols/enzdes/enzdes_util.hh>
+//#include <utility/exit.hh>
+#include <utility/tag/Tag.hh>
+
+//#include <core/pack/task/operation/TaskOperation.fwd.hh>
+//#include <utility/vector0.hh>
+#include <utility/vector1.hh>
+//#include <utility/string_util.hh>
+
+
+namespace protocols {
+namespace simple_moves {
+
+using namespace core;
+using namespace pack;
+using namespace task;
+using namespace operation;
+using namespace scoring;
+
+using basic::Warning;
+using basic::t_warning;
+static basic::Tracer TR("protocols.simple_moves.PackRotamersMoverPartGreedy");
+
+std::string
+PackRotamersMoverPartGreedyCreator::keyname() const
+{
+  return PackRotamersMoverPartGreedyCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+PackRotamersMoverPartGreedyCreator::create_mover() const
+{
+  return new PackRotamersMoverPartGreedy;
+}
+
+PackRotamersMoverPartGreedy::PackRotamersMoverPartGreedy(
+		ScoreFunctionOP scorefxn,
+		PackerTaskOP task,
+		utility::vector1 <core::Size> target_residues
+	) :
+
+	protocols::moves::Mover("PackRotamersMoverPartGreedy"),
+	scorefxn_repack_ (scorefxn),
+	scorefxn_minimize_ (scorefxn),
+	task_ (task),
+	target_residues_ (target_residues)
+{}
+
+PackRotamersMoverPartGreedy::PackRotamersMoverPartGreedy() : 
+	protocols::moves::Mover("PackRotamersMoverPartGreedy"),
+	scorefxn_repack_(0),
+	scorefxn_minimize_(0),
+	task_factory_(0),
+	target_residues_(0),
+	threshold_(0)
+{}
+
+std::string
+PackRotamersMoverPartGreedyCreator::mover_name()
+{
+        return "PackRotamersMoverPartGreedy";
+}
+
+
+PackRotamersMoverPartGreedy::~PackRotamersMoverPartGreedy(){}
+
+void
+PackRotamersMoverPartGreedy::parse_my_tag(
+	TagPtr const tag,
+	protocols::moves::DataMap & datamap,
+	Filters_map const &,
+	protocols::moves::Movers_map const &,
+	Pose const & pose
+)
+{
+	//task operations
+	if( tag->hasOption("task_operations") ) task_factory( protocols::rosetta_scripts::parse_task_operations( tag, datamap ) );
+  	else task_factory_ = NULL;
+	//Scorefunctions
+	std::string const scorefxn_repack( tag->getOption<std::string>( "scorefxn_repack", "score12" ) );
+	std::string const scorefxn_repack_greedy( tag->getOption<std::string>( "scorefxn_repack_greedy", "score12" ) );
+  	std::string const scorefxn_minimize( tag->getOption<std::string>( "scorefxn_minimize", "score12" ) );
+  	using namespace core::scoring;
+  	scorefxn_repack_ = new ScoreFunction( *datamap.get< ScoreFunction * >( "scorefxns", scorefxn_repack ) );
+  	scorefxn_repack_greedy_ = new ScoreFunction( *datamap.get< ScoreFunction * >( "scorefxns", scorefxn_repack_greedy ) );
+  	scorefxn_minimize_ = new ScoreFunction( *datamap.get< ScoreFunction * >( "scorefxns", scorefxn_minimize ) );
+	//target residues for greedy opt around
+	 if( tag->hasOption("target_residues") ) {
+      		target_residues_ = protocols::rosetta_scripts::get_resnum_list(tag, "target_residues",pose);
+	}
+	use_cstids_ = false;
+	if( tag->hasOption("target_cstids") ) {
+		cstid_list_ = tag->getOption<std::string>("target_cstids", "" );
+		use_cstids_ = true;
+    	}
+	//distance threshold for neighbor check in greedy opt
+	 threshold_  =  tag->getOption<core::Real>("distance_threshold", 8.0 );
+}
+
+std::string
+PackRotamersMoverPartGreedy::get_name() const {
+  return PackRotamersMoverPartGreedyCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+PackRotamersMoverPartGreedy::clone() const
+{
+  return new PackRotamersMoverPartGreedy( *this );
+}
+
+protocols::moves::MoverOP
+PackRotamersMoverPartGreedy::fresh_instance() const
+{
+  return new PackRotamersMoverPartGreedy;
+}
+
+
+void
+PackRotamersMoverPartGreedy::apply( Pose & pose )
+{
+  //create task
+  using namespace core::pack::task;
+  core::pose::Pose greedy_pose = pose;
+  TR<<"Creating packer task based on specified task operations..."<< std::endl;
+  if ( task_factory_ !=0 ){
+  	task_factory_->push_back( new core::pack::task::operation::InitializeFromCommandline );
+  	task_ = task_factory_->create_task_and_apply_taskoperations( greedy_pose );
+  }
+  else{
+	core::pack::task::TaskFactoryOP tf = new core::pack::task::TaskFactory();
+        task_ = tf->create_task_and_apply_taskoperations( greedy_pose );
+  }
+  (*scorefxn_repack_)(greedy_pose);
+  // make sure we have some target residues
+  if (use_cstids_){ // enzdes csts if specified
+	protocols::enzdes::enzutil::get_resnum_from_cstid_list( cstid_list_, greedy_pose, target_residues_ );
+   }
+  runtime_assert(target_residues_.size()>0);
+  //
+  //Make sure target residues are held fixed
+   for( utility::vector1< core::Size >::const_iterator pos_it = target_residues_.begin(); pos_it != target_residues_.end(); ++pos_it ){
+	task_->nonconst_residue_task( *pos_it ).prevent_repacking();
+   }
+  //Convert to Ala
+  utility::vector1< core::Size > toAla;
+  for (core::Size ii=1;ii<=greedy_pose.total_residue();++ii){
+	if (task_->being_designed(ii)) toAla.push_back( ii );
+  }
+  protocols::toolbox::pose_manipulation::construct_poly_ala_pose( greedy_pose, toAla, true, true, true );
+  //Then, greedy opt around user specified target residues, this will modify the task and pose
+  greedy_around( greedy_pose, target_residues_, task_, scorefxn_repack_greedy_);
+  //greedy_pose.dump_pdb("after_greedy.pdb");
+  //Now just replace the target_residues and the greedily optimized positions in original pose with greedy pose, set task to reflect these
+  for( utility::vector1< core::Size >::const_iterator pos_it = target_residues_.begin(); pos_it != target_residues_.end(); ++pos_it ){
+	pose.replace_residue( *pos_it, greedy_pose.residue(*pos_it), true);
+   }
+  for( utility::vector1< core::Size >::const_iterator res=restrict_to_repacking_.begin(); res!=restrict_to_repacking_.end(); ++res ){
+	if (!(greedy_pose.residue(*res).name3() == "ALA")) pose.replace_residue( *res, greedy_pose.residue(*res), true);
+  }
+  //old task is no longer valid. create a new one.
+  PackerTaskOP task = task_factory_->create_task_and_apply_taskoperations( pose );
+  TR<<" Restrcting the following greedily found residues to repack: ";
+  for( utility::vector1< core::Size >::const_iterator res=restrict_to_repacking_.begin(); res!=restrict_to_repacking_.end(); ++res )
+    {
+	 if (!(greedy_pose.residue(*res).name3() == "ALA")) task->nonconst_residue_task( *res ).restrict_to_repacking();
+	TR<< *res << "+";
+    }
+  TR<< std::endl;
+
+  //Next hand over these to regular PackRotamers mover and we're done!
+  protocols::simple_moves::PackRotamersMoverOP pack =  new protocols::simple_moves::PackRotamersMover( scorefxn_repack_ , task );
+  pack->apply( pose );
+  
+}
+
+void
+PackRotamersMoverPartGreedy::greedy_around(
+	core::pose::Pose & pose,
+	utility::vector1<core::Size > const & target_residues,
+	core::pack::task::PackerTaskOP task,
+	core::scoring::ScoreFunctionCOP scorefxn
+)
+{	
+	using namespace core::pack;
+	using namespace core::pack::task;
+
+	core::pose::Pose cur_pose = pose;
+	
+	
+	for( utility::vector1< core::Size >::const_iterator pos_it = target_residues.begin(); pos_it != target_residues.end(); ++pos_it ){
+		TR<<"Considering target residue "<< cur_pose.residue( *pos_it ).name3() << *pos_it <<": "<<std::endl;	
+		utility::vector1< core::Size > neighbors  = compute_designable_neighbors( *pos_it, task, cur_pose );
+		utility::vector1< core::Size > cur_neighbors = neighbors;
+		utility::vector1< bool > allow_minimization (pose.total_residue(), false);
+		for (utility::vector1< core::Size>::const_iterator iter = neighbors.begin(); iter != neighbors.end(); ++iter){
+			allow_minimization[ *iter ] = true;
+		}
+		allow_minimization[*pos_it] = true;
+		// Iterate neighbors.size() times
+		TR << "Total number of neighbors are "<<neighbors.size()<<std::endl;
+		for (core::Size ii=1;ii<=neighbors.size();++ii){
+	 		protocols::toolbox::pose_manipulation::construct_poly_ala_pose( cur_pose, cur_neighbors, false, false, true );	
+		//	cur_pose.dump_pdb("polyA_"+utility::to_string(ii)+".pdb");
+			TR << "At iteration "<< ii << ", current neighbor size is "<< cur_neighbors.size()<<std::endl;	
+			core::Size best_neigh (0);
+			//calculate energy in polyAla state before choosing
+			core::Real best_energy (cur_pose.energies().total_energies()[core::scoring::total_score]);
+			TR <<" Poly Ala energy is :"<< best_energy<<std::endl;
+			core::pose::Pose inner_pose = cur_pose; 
+			// Iterate over all neighbors at which no choice has been made so far and choose best in the context of current state
+		 	for( utility::vector1< core::Size >::const_iterator neigh_it = cur_neighbors.begin(); neigh_it != cur_neighbors.end(); ++neigh_it ){
+				core::pose::Pose working_pose = inner_pose;
+				// make a task
+				core::pack::task::TaskFactoryOP mut_res = new core::pack::task::TaskFactory();
+				core::pack::task::PackerTaskOP mutate_residue = mut_res->create_task_and_apply_taskoperations( working_pose );
+			        mutate_residue->initialize_from_command_line().or_include_current( true );
+				// upweight interactions of this position to those within the cluster (to favor intracluster interactions)
+				utility::vector1< core::Size > upweight_1;
+				upweight_1.push_back( *neigh_it );
+				utility::vector1< core::Size > upweight_2 = neighbors;
+				upweight_2.push_back( *pos_it );
+				//upweight interactions between this position and neighbors
+				IGEdgeReweighterOP ig_up = new protocols::toolbox::ResidueGroupIGEdgeUpweighter( 2.5, upweight_1 , upweight_2 );
+				mutate_residue->set_IGEdgeReweights()->add_reweighter( ig_up );
+				// fix everything else except this position
+				utility::vector1< bool > keep_aas( core::chemical::num_canonical_aas, true );
+				
+				for (core::Size i=1; i<=pose.total_residue(); ++i){
+				if (i== *neigh_it) mutate_residue->nonconst_residue_task( i ).restrict_absent_canonical_aas( keep_aas );
+				else mutate_residue->nonconst_residue_task( i ).prevent_repacking();
+				}
+				// Now run design on one position
+				protocols::simple_moves::PackRotamersMoverOP prm =  new protocols::simple_moves::PackRotamersMover( scorefxn, mutate_residue, 1 );
+				prm->apply( working_pose );
+				//Minimize the sidechains of all designed residues so far
+				core::kinematics::MoveMapOP movemap = new core::kinematics::MoveMap();
+				movemap->set_jump( false );
+  				movemap->set_bb( false );
+				movemap->set_chi( allow_minimization );
+				protocols::simple_moves::MinMoverOP minmover = new protocols::simple_moves::MinMover( movemap, scorefxn_minimize_, "dfpmin_armijo_nonmonotone_atol", 0.02, true /*use_nblist*/);
+				minmover->apply(working_pose);
+
+				//Check energy
+				core::Real this_energy = working_pose.energies().total_energies()[core::scoring::total_score];
+				//TR << "Energy for best residue at position "<< *neigh_it << "is "<< this_energy << std::endl;
+				// see if this energy is best so far. if so replace this neighbor in cur_pose 
+				if ( (this_energy<best_energy) || (best_neigh==0) ) { 
+					//TR<< "Best neighbor at position "<<*neigh_it <<" is "<< working_pose.residue( *neigh_it ).name3() << " with energy "<< this_energy<<std::endl;
+					cur_pose = working_pose;
+					best_neigh = *neigh_it;
+					best_energy = this_energy;
+				}
+			} // all currently unchosen neighbors so far
+			//Now that a choice has been made at the position best_neigh, inform cur_neighbors and task to take this position out from future design 
+			update_task_and_neighbors( best_neigh, task, cur_neighbors );
+		} //iterate neighbors.size() times around each target residue
+	} //all target residues
+	pose = cur_pose;
+} // greedy around
+
+utility::vector1< core::Size > 
+PackRotamersMoverPartGreedy::compute_designable_neighbors(
+core::Size const & position,
+core::pack::task::PackerTaskCOP task,
+core::pose::Pose const & pose
+)
+{
+	utility::vector1< core::Size > neighbors;	
+	TR << "neighbors of position "<< position << " are: ";
+	core::conformation::Residue const res_pos (pose.residue( position ));
+	for (core::Size ii=1;ii<=pose.total_residue();++ii){
+		if ( pose.residue( ii ).is_protein() && task->being_designed( ii ) ){
+			core::conformation::Residue const res_ii (pose.residue( ii ));
+			core::Real distance = res_ii.xyz( res_ii.nbr_atom() ).distance( res_pos.xyz(res_pos.nbr_atom()) );
+			if (distance<=threshold_)  {
+				neighbors.push_back( ii );
+				TR <<ii<<" + "; 
+			}
+		}
+	}
+	TR<<std::endl;
+//	TR<< "In function total neighbors are"<< neighbors.size() <<std::endl;
+	return neighbors;
+}
+
+void
+PackRotamersMoverPartGreedy::update_task_and_neighbors(
+core::Size const & best_neigh,
+core::pack::task::PackerTaskOP task,
+utility::vector1< core::Size > & current_neighbors
+)
+{
+	task->nonconst_residue_task( best_neigh ).restrict_to_repacking();
+	restrict_to_repacking_.push_back( best_neigh );
+	utility::vector1< core::Size > temp_neighbors;
+	for( utility::vector1< core::Size >::const_iterator neigh_it = current_neighbors.begin(); neigh_it != current_neighbors.end(); ++neigh_it ){
+		if (*neigh_it != best_neigh) temp_neighbors.push_back(*neigh_it);
+		else TR<<" Updated current neighbors by removing "<<*neigh_it << " from it."<<std::endl; 
+	}
+	current_neighbors = temp_neighbors;
+
+
+}
+
+void
+PackRotamersMoverPartGreedy::task_factory( core::pack::task::TaskFactoryOP p ) {
+  task_factory_ = p;
+}
+
+void 
+PackRotamersMoverPartGreedy::task( core::pack::task::PackerTaskOP task ){
+	task_ = task;
+}
+
+void 
+PackRotamersMoverPartGreedy::target_residues( utility::vector1< core::Size > & trg_res ){
+	target_residues_ = trg_res;
+}
+
+/*core::PackerEnergy PackRotamersMoverPartGreedy::run_with_ig( Pose & pose, utility::vector0< int > rot_to_pack, InteractionGraphBaseOP ig) const
+{
+	return pack_rotamers_run( pose, task(), rotamer_sets(), ig, rot_to_pack );
+}
+*/
+
+}//moves
+}//protocols
---
+++ rosetta_source/src/protocols/simple_moves/PackRotamersMoverPartGreedy.hh
@@ -0,0 +1,111 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file
+/// @brief
+/// @author Sagar Khare (khares@uw.edu)
+
+#ifndef INCLUDED_protocols_simple_moves_PackRotamersMoverPartGreedy_hh
+#define INCLUDED_protocols_simple_moves_PackRotamersMoverPartGreedy_hh
+
+// Unit headers
+//#include <protocols/simple_moves/PackRotamersMoverPartGreedy.fwd.hh>
+#include <protocols/moves/Mover.hh>
+
+// Project headers
+#include <core/types.hh>
+
+#include <core/pack/task/PackerTask.fwd.hh>
+#include <core/pack/task/TaskFactory.fwd.hh>
+#include <core/pose/Pose.fwd.hh>
+#include <core/scoring/ScoreFunction.fwd.hh>
+#include <protocols/filters/Filter.fwd.hh>
+#include <utility/tag/Tag.fwd.hh>
+#include <utility/vector1.hh>
+
+
+
+namespace protocols {
+namespace simple_moves {
+
+/// @brief a mover that packs the side-chains around a given set of target residues in a greedy fashion, and then packs the rest using the sim annealer.
+
+class PackRotamersMoverPartGreedy: public protocols::moves::Mover {
+  
+	typedef core::pack::task::PackerTaskOP PackerTaskOP;
+  typedef core::pack::task::TaskFactoryOP TaskFactoryOP;
+  typedef core::scoring::ScoreFunctionOP ScoreFunctionOP;
+
+public:
+	///@brief default constructors
+	 PackRotamersMoverPartGreedy(
+		ScoreFunctionOP scorefxn,
+		PackerTaskOP task,
+		utility::vector1 <core::Size> target_residues
+	);
+	
+	PackRotamersMoverPartGreedy();
+	
+	//std::string PackRotamersMoverPartGreedyCreator::mover_name();
+
+	// destructor
+	virtual ~PackRotamersMoverPartGreedy();
+
+	 //parser stuff
+  void parse_my_tag( utility::tag::TagPtr const tag, protocols::moves::DataMap &, protocols::filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const &pose );
+  void apply( core::pose::Pose &pose );
+  virtual std::string get_name() const;
+  protocols::moves::MoverOP clone() const;
+  protocols::moves::MoverOP fresh_instance() const;
+
+	// methods
+	void greedy_around(
+        core::pose::Pose & pose,
+        utility::vector1<core::Size > const & target_res,
+        core::pack::task::PackerTaskOP task,
+        core::scoring::ScoreFunctionCOP scorefxn
+	);
+	
+	utility::vector1< core::Size > compute_designable_neighbors(
+																	core::Size const & position,
+																	core::pack::task::PackerTaskCOP task,
+																	core::pose::Pose const & pose
+	);
+	
+	void update_task_and_neighbors(
+				core::Size const & best_neigh,
+				core::pack::task::PackerTaskOP task,
+				utility::vector1< core::Size > & current_neighbors
+	);
+
+	// getters and setters
+
+	void task_factory( core::pack::task::TaskFactoryOP p );
+	void task( core::pack::task::PackerTaskOP task );
+  void target_residues (utility::vector1< core::Size > & trg_res);
+
+	
+private:
+	core::scoring::ScoreFunctionOP scorefxn_repack_;
+	core::scoring::ScoreFunctionOP scorefxn_repack_greedy_;
+  core::scoring::ScoreFunctionOP scorefxn_minimize_;
+  core::pack::task::PackerTaskOP task_;
+  core::pack::task::TaskFactoryOP task_factory_; 
+	utility::vector1< core::Size > target_residues_;
+	utility::vector1< core::Size > restrict_to_repacking_;
+	bool use_cstids_;
+	core::Real threshold_;
+	std::string cstid_list_;
+};
+
+
+} // moves
+} // protocols
+
+#endif
