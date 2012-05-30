0326eca223d6333c265b9103544151e18cbb206e
Ben Borgo <bborgo@genetics.wustl.edu>
2012-05-30 16:24:52 +0000

    Recommiting code from 49104, hopefully without incidient this time
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49181 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: ac0641838ee6c05a3d92f9d4054f788f2b7696e6

--- rosetta_source/src/core/scoring/ScoreTypeManager.cc
+++ rosetta_source/src/core/scoring/ScoreTypeManager.cc
@@ -109,7 +109,7 @@ ScoreTypeManager::setup_score_type_names()
 	name2score_type_[ "cenpack" ] = cenpack;
 	name2score_type_[ "hybrid_vdw" ] = hybrid_vdw;
 	name2score_type_[ "fa_cust_pair_dist" ] = fa_cust_pair_dist;
-//        name2score_type_[ "gauss" ] = gauss;
+	name2score_type_[ "gauss" ] = gauss;
 
 
 	// PyRosetta score types
