3b81f8f8b894bd5bcf59b4ba667c557d0fccc710
Sagar Khare <khares@u.washington.edu>
2012-06-07 05:00:23 +0000

    Adding a Residue(type) interaction energy filter. compares interaction energy of a residue (sum_eij) to user-defined (typically pdb-derived) threshold value and returns a penalty which is the difference between the two, intended to be used in GreedyOpt. Calculation can be done for whole pose, across interface (e.g. all trps at the interface) or for specified residue numbers. No changes expected.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49324 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: c0737fae53310768f6142501882d6baccfa18ff0

---
+++ rosetta_source/src/protocols/simple_filters/ResidueIEFilter.cc
@@ -0,0 +1,252 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file protocols/simple_filters/ResidueIEFilter.cc
+/// @brief
+/// @author Sagar Khare (khares@u.washington.edu)
+
+#include <protocols/simple_filters/ResidueIEFilter.hh>
+#include <protocols/simple_filters/ResidueIEFilterCreator.hh>
+
+#include <protocols/filters/Filter.hh>
+#include <basic/Tracer.hh>
+#include <utility/tag/Tag.hh>
+#include <protocols/moves/DataMap.hh>
+#include <protocols/scoring/Interface.hh>
+#include <core/scoring/Energies.hh>
+#include <core/scoring/EnergyGraph.hh>
+#include <core/pose/Pose.hh>
+#include <core/conformation/Conformation.hh>
+#include <utility/vector1.hh>
+#include <boost/foreach.hpp>
+#include <core/pose/selection.hh>
+
+#define foreach BOOST_FOREACH
+
+namespace protocols {
+namespace simple_filters {
+
+static basic::Tracer tr( "protocols.simple_filters.ResidueIEFilter" );
+
+protocols::filters::FilterOP
+ResidueIEFilterCreator::create_filter() const { return new ResidueIEFilter; }
+
+std::string
+ResidueIEFilterCreator::keyname() const { return "ResidueIE"; }
+
+ResidueIEFilter::ResidueIEFilter( 
+	utility::vector1< core::Size > const resnums,
+	std::string const restype,
+	core::scoring::ScoreFunctionCOP scorefxn,
+	core::scoring::ScoreType const score_type,
+	core::Real const threshold,
+	bool const whole_pose,
+	bool const whole_interface,
+	core::Size const rb_jump,
+	core::Real const interface_distance_cutoff,
+	core::Real max_penalty,
+	core::Real penalty_factor
+	) : 
+	filters::Filter( "ResidueIE" ),
+	resnums_( resnums ),
+	restype_ (restype),
+	score_type_( score_type ),
+	threshold_( threshold ),
+	whole_pose_ ( whole_pose ),
+	whole_interface_ ( whole_interface ),
+	rb_jump_ ( rb_jump ),
+	interface_distance_cutoff_ ( interface_distance_cutoff ),
+	max_penalty_ (max_penalty),
+	penalty_factor_ (penalty_factor)
+	{
+		using namespace core::scoring;
+
+		if( scorefxn ) scorefxn_ = new core::scoring::ScoreFunction( *scorefxn );
+		if( score_type_ != total_score ) {
+			core::Real const old_weight( scorefxn_->get_weight( score_type_ ) );
+			scorefxn_->reset();
+			scorefxn_->set_weight( score_type_, old_weight );
+
+		}
+	}
+ResidueIEFilter::ResidueIEFilter( ResidueIEFilter const &init ) :
+	Filter( init ), resnums_( init.resnums_ ),
+	restype_( init.restype_ ),
+	score_type_( init.score_type_ ),
+	threshold_( init.threshold_ ),
+	whole_pose_ (init.whole_pose_),
+	whole_interface_ (init.whole_interface_),
+	rb_jump_ (init.rb_jump_),
+	interface_distance_cutoff_ ( init.interface_distance_cutoff_),
+	max_penalty_ ( init.max_penalty_),
+	penalty_factor_ ( init.penalty_factor_)
+{
+	using namespace core::scoring;
+	if( init.scorefxn_ ) scorefxn_ = new core::scoring::ScoreFunction( *init.scorefxn_ );
+}
+
+core::scoring::ScoreFunctionOP
+ResidueIEFilter::scorefxn() const{
+	return scorefxn_;
+}
+
+void
+ResidueIEFilter::scorefxn( core::scoring::ScoreFunctionOP scorefxn ){
+	scorefxn_ = scorefxn;
+}
+
+core::scoring::ScoreType
+ResidueIEFilter::score_type() const{
+	return score_type_;
+}
+
+void
+ResidueIEFilter::score_type( core::scoring::ScoreType score_type ){
+	score_type_ = score_type;
+}
+
+core::Real
+ResidueIEFilter::threshold() const{
+	return threshold_;
+}
+
+void
+ResidueIEFilter::threshold( core::Real const th ){
+	threshold_ = th;
+}
+
+utility::vector1 <core::Size>
+ResidueIEFilter::resnums() const{
+	return resnums_;
+}
+
+void
+ResidueIEFilter::resnums( utility::vector1<core::Size> const & rn ){
+	resnums_ = rn;
+}
+
+ResidueIEFilter::~ResidueIEFilter() {}
+
+void
+ResidueIEFilter::parse_my_tag( utility::tag::TagPtr const tag, moves::DataMap & data, filters::Filters_map const &, moves::Movers_map const &, core::pose::Pose const & pose )
+{
+	using namespace core::scoring;
+	std::string const scorefxn_name( tag->getOption<std::string>( "scorefxn", "score12" ) );
+	scorefxn_ = new ScoreFunction( *(data.get< ScoreFunction * >( "scorefxns", scorefxn_name ) ));
+	score_type_ = core::scoring::score_type_from_name( tag->getOption<std::string>( "score_type", "total_score" ) );
+	threshold_ = tag->getOption<core::Real>( "energy_cutoff", 0.0 );
+	whole_pose_ = tag->getOption<bool>( "whole_pose" , 0 );
+	whole_interface_ = tag->getOption<bool>( "interface" , 0 );
+	rb_jump_ = tag->getOption<core::Size>( "jump_number", pose.num_jump() );
+	interface_distance_cutoff_ = tag->getOption<core::Real>( "interface_distance_cutoff" , 8.0 );
+	restype_ =  tag->getOption<std::string>( "restype3", "TRP" );
+	penalty_factor_ =  tag->getOption<core::Real>( "penalty_factor", 1.0 );
+	max_penalty_ =  tag->getOption<core::Real>( "max_penalty", 1000.0 );
+	if(tag->hasOption("residues")) {
+		resnums_ = core::pose::get_resnum_list(tag, "residues", pose);
+		if(resnums_.empty()){
+			whole_pose_=1;	
+			tr<< "Failed to parse residues: " << tag->getOption<std::string> ("residues") << ". Using whole pose." << std::endl;
+		}
+	}
+	runtime_assert(tag->hasOption("residues") || whole_pose_ || whole_interface_);
+}
+
+bool
+ResidueIEFilter::apply( core::pose::Pose const & pose ) const
+{
+	  if ( whole_interface_)  {
+    if ( pose.conformation().num_chains() < 2 ) {
+      tr << "pose must contain at least two chains!" << std::endl;
+      return false;
+    }
+    core::pose::Pose in_pose = pose;
+    (*scorefxn_)(in_pose);
+    protocols::scoring::Interface interface_obj(rb_jump_);
+    in_pose.update_residue_neighbors();
+    interface_obj.distance( interface_distance_cutoff_ );
+    interface_obj.calculate( in_pose );
+     for ( core::Size resnum = 1; resnum <= pose.total_residue(); ++resnum) {
+      if ( in_pose.residue(resnum).is_protein()  &&  interface_obj.is_interface( resnum ) && (in_pose.residue_type(resnum).name3() == restype_) ) resnums_.push_back( resnum );
+    }
+  }
+  else if ( whole_pose_ ){
+    core::pose::Pose in_pose = pose;
+    for ( core::Size resnum = 1; resnum <= in_pose.total_residue(); ++resnum) {
+      if ( in_pose.residue(resnum).is_protein()  && (in_pose.residue_type(resnum).name3() == restype_) ) resnums_.push_back( resnum );
+    }
+  }
+
+  if (resnums_.size() == 0) {
+    tr << "No residues found. Skipping calculation."<< std::endl;
+    return false;
+  }
+  std::unique( resnums_.begin(), resnums_.end() );
+  tr << "The following residues will be considered for interaction energy calculation:"<< std::endl;
+  foreach (core::Size const res, resnums_){
+    tr << pose.residue_type( res ).name3() << res <<"+";
+    if (!(pose.residue_type( res ).name3() == restype_)) {
+      tr << "Residue " << res << " in pose is of type "<< pose.residue_type( res ).name3() << ". Requested restype3 is "<< restype_<<". Skipping!"<<std::endl;
+      return false;
+    }
+  }
+  tr<<std::endl;
+	
+	core::Real const penalty( compute( pose ) );
+	if (penalty>max_penalty_) return false;
+	return true;
+}
+
+
+void
+ResidueIEFilter::report( std::ostream & out, core::pose::Pose const & pose ) const 
+{
+		core::Real const penalty( compute( pose ) );
+		out << "Total penalty is "<< penalty << std::endl;
+}
+
+
+core::Real
+ResidueIEFilter::report_sm( core::pose::Pose const & pose ) const
+{
+	core::Real const penalty( compute( pose ) );
+	return( penalty );
+}
+
+core::Real
+ResidueIEFilter::compute( core::pose::Pose const & pose ) const
+{
+	using namespace core::scoring;
+	using namespace core::graph;
+	 
+	core::pose::Pose in_pose = pose;
+	(*scorefxn_)(in_pose);
+	core::Real penalty (0.0);
+  EnergyMap const curr_weights = in_pose.energies().weights();
+  foreach (core::Size const res, resnums_){
+		core::Real res_intE (0.0);      
+	//Fill residue energies by traversing energy graph
+		for( EdgeListConstIterator egraph_it = in_pose.energies().energy_graph().get_node( res )->const_edge_list_begin(); egraph_it != in_pose.energies().energy_graph().get_node( res )->const_edge_list_end(); ++egraph_it){
+			core::Size const int_resi = (*egraph_it)->get_other_ind( res );
+			EnergyEdge const * Eedge = static_cast< EnergyEdge const * > (*egraph_it);
+			res_intE += Eedge->dot( curr_weights );
+		}//for each egraph_it
+		tr << "Residue "<< pose.residue_type( res ).name3()<<res<< " has an interaction energy "<< res_intE <<", threshold is "<< threshold_<<" and penalty is ";
+		if (res_intE > threshold_) {
+			penalty+= (res_intE - threshold_);
+			tr<< (res_intE - threshold_) << std::endl;
+		}
+		else tr << " 0"<<std::endl;
+	} //foreach res
+	
+	return( penalty * penalty_factor_ );
+}
+
+}
+}
---
+++ rosetta_source/src/protocols/simple_filters/ResidueIEFilter.hh
@@ -0,0 +1,80 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file protocols/simple_filters/ResidueIEFilter.hh
+/// @brief definition of filter class ResidueIEFilter.
+/// @author Sagar Khare (khares@u.washington.edu)
+
+#ifndef INCLUDED_protocols_simple_filters_ResidueIEFilter_hh
+#define INCLUDED_protocols_simple_filters_ResidueIEFilter_hh
+
+#include <protocols/simple_filters/ResidueIEFilter.fwd.hh>
+#include <protocols/filters/Filter.hh>
+#include <utility/tag/Tag.fwd.hh>
+#include <protocols/moves/DataMap.fwd.hh>
+#include <protocols/moves/Mover.fwd.hh>
+#include <core/pose/Pose.fwd.hh>
+#include <core/scoring/ScoreType.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <utility/vector1.hh>
+
+namespace protocols {
+namespace simple_filters {
+
+class ResidueIEFilter : public filters::Filter
+{
+public:
+	ResidueIEFilter() : filters::Filter( "ResidueIE" ) {}
+
+	ResidueIEFilter( utility::vector1<core::Size> const resnums, std::string const restype, core::scoring::ScoreFunctionCOP scorefxn,
+	   core::scoring::ScoreType const score_type, core::Real const threshold,
+	   bool const whole_pose = false, bool const whole_interface = false, core::Size const rb_jump = 1,
+	   core::Real const interface_distance_cutoff =  8.0 , core::Real max_penalty = 0.0, core::Real penalty_factor = 1.0 );
+
+	ResidueIEFilter( ResidueIEFilter const &init );
+	bool apply( core::pose::Pose const & pose ) const;
+	filters::FilterOP clone() const {
+		return new ResidueIEFilter( *this );
+	}
+	filters::FilterOP fresh_instance() const{
+		return new ResidueIEFilter();
+	}
+
+	void report( std::ostream & out, core::pose::Pose const & pose ) const;
+	core::Real report_sm( core::pose::Pose const & pose ) const;
+	core::Real compute( core::pose::Pose const & pose ) const;
+	virtual ~ResidueIEFilter();
+	void parse_my_tag( utility::tag::TagPtr const tag, protocols::moves::DataMap &, filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const & );
+	utility::vector1<core::Size> resnums() const;
+	core::scoring::ScoreFunctionOP scorefxn() const;
+	core::scoring::ScoreType score_type() const;
+	core::Real threshold() const;
+	void resnums( utility::vector1<core::Size> const & rn );
+	void scorefxn( core::scoring::ScoreFunctionOP scorefxn );
+	void score_type( core::scoring::ScoreType score_type );
+	void threshold( core::Real const th );
+private:
+	mutable utility::vector1<core::Size> resnums_; 
+	std::string restype_;
+	core::scoring::ScoreFunctionOP scorefxn_; 
+	core::scoring::ScoreType score_type_;
+	core::Real threshold_;
+	bool whole_pose_;
+	bool whole_interface_;
+	core::Size rb_jump_;
+	core::Real interface_distance_cutoff_;
+	core::Real max_penalty_;
+	core::Real penalty_factor_;
+
+};
+
+}
+}
+
+#endif
