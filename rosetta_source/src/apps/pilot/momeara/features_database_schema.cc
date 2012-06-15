cfcb2b5f9300cdd65ac55f9982c75141e6890c24
Matthew O'Meara <mattjomeara@gmail.com>
2012-06-15 18:24:45 +0000

    Add an integration test that just writes the database schema for all registered features reporters. This will test that there is no syntactic problems with the schemas. With comming schema specification refactoring, it will show record the changes to the schema even for features reporters that are not part of the features integration test for what ever reason. Also writing out the schema may be useful for auto-documentation.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49458 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: b4d698fb5f981d3f75dee4dd91bb5e16e3df4a17

---
+++ rosetta_source/src/apps/pilot/momeara/features_database_schema.cc
@@ -0,0 +1,78 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file features_database_schema.cc
+/// @brief simple application for interconverting Rosetta recognized structure formats
+/// @author Matthew O'Meara
+///
+
+// Project Headers
+#include <utility/sql_database/DatabaseSessionManager.hh>
+#include <basic/database/sql_utils.hh>
+
+// Platform Headers
+#include <devel/init.hh>
+#include <basic/options/option.hh>
+#include <basic/options/option_macros.hh>
+#include <basic/options/keys/inout.OptionKeys.gen.hh>
+
+#include <protocols/features/FeaturesReporter.hh>
+#include <protocols/features/FeaturesReporterFactory.hh>
+#include <utility/vector1.hh>
+#include <utility/excn/EXCN_Base.hh>
+#include <core/types.hh>
+
+using std::string;
+using std::cout;
+using std::endl;
+using core::Size;
+using utility::sql_database::sessionOP;
+using utility::vector1;
+using basic::database::get_db_session;
+using protocols::features::FeaturesReporterOP;
+using protocols::features::FeaturesReporterFactory;
+
+void
+write_schema_to_db(
+	sessionOP db_session
+) {
+	FeaturesReporterFactory * features_reporter_factory(
+		FeaturesReporterFactory::get_instance());
+
+	vector1<string> features_reporter_names(
+		features_reporter_factory->get_all_features_names());
+	for(Size i=1; i <= features_reporter_names.size(); ++i){
+		FeaturesReporterOP features_reporter(
+			features_reporter_factory->get_features_reporter(
+				features_reporter_names[i]));
+
+		features_reporter->write_schema_to_db(db_session);
+	}
+}
+
+int
+main( int argc, char* argv [] ) {
+	using namespace basic::options;
+	using namespace basic::options::OptionKeys;
+	// define relevant options
+
+	try {
+		// options, random initialization
+		devel::init( argc, argv );
+
+
+		sessionOP db_session(get_db_session(option[inout::database_filename]));
+		write_schema_to_db(db_session);
+
+	} catch ( utility::excn::EXCN_Base& excn ) {
+		std::cerr << "Exception: " << std::endl;
+		excn.show( std::cerr );
+	}
+	return 0;
+} // main
