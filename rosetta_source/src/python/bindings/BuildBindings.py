7c699ca6c1fa751118b098c83cf255c6cd110a40
Sergey Lyskov <sergey.lyskov@jhu.edu>
2012-07-02 23:00:40 +0000

    PyRosetta. Updating default dest path for cross-compile mode.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49696 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 417960cb8022ed2e9956f6137c65b1946f8cbb06

--- rosetta_source/src/python/bindings/BuildBindings.py
+++ rosetta_source/src/python/bindings/BuildBindings.py
@@ -236,7 +236,7 @@ def main(args):
     #bindings_path += '.debug' if Options.debug else '.release'
 
     if Options.cross_compile:
-        bindings_path = os.path.abspath('rosetta')
+        bindings_path = os.path.abspath('rosetta.window')
         if not os.path.isdir(bindings_path): os.makedirs(bindings_path)
         execute('Generating svn_version files...', 'cd ./../../../ && python svn_version.py')  # Now lets generate svn_version.* files and copy it to destination (so windows build could avoid running it).
         shutil.copyfile('./../../core/svn_version.cc', bindings_path + '/svn_version.cc')
