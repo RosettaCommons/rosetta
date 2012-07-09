5ec0ec66d07c0580cb93ede8a463d26f8afbdf61
Rhiju Das <rhiju@stanford.edu>
2012-07-09 20:12:12 +0000

    fixing rotation again & now adding water rotation
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49798 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 75d591687b78ffdc6c524697c19ab13a98388e28

--- rosetta_source/src/apps/pilot/fcchou/adenosine_sample_around.cc
+++ rosetta_source/src/apps/pilot/fcchou/adenosine_sample_around.cc
@@ -28,11 +28,13 @@
 #include <core/kinematics/FoldTree.hh>
 #include <core/kinematics/Jump.hh>
 #include <core/kinematics/MoveMap.hh>
+#include <core/kinematics/Stub.hh>
 
 #include <core/io/silent/SilentFileData.hh>
 #include <core/io/silent/BinaryProteinSilentStruct.hh>
 
 #include <protocols/idealize/idealize.hh>
+#include <protocols/swa/StepWiseUtil.cc>
 
 #include <core/pose/util.hh>
 #include <core/pose/Pose.hh>
@@ -74,6 +76,7 @@ using utility::vector1;
 
 typedef  numeric::xyzMatrix< Real > Matrix;
 
+OPT_KEY( Boolean, sample_water )
 
 /////////////////////////////////////////////////////////////////////////////
 //FCC: Adding Virtual res
@@ -117,13 +120,73 @@ rotate_into_nucleobase_frame( core::pose::Pose & pose ){
 
 	Vector centroid = get_rna_base_centroid( rsd, true /*verbose*/ );
 	Matrix M = get_rna_base_coordinate_system( rsd, centroid );
+	kinematics::Stub stub( M, centroid );
 
 	for (Size i = 1; i <= rsd.natoms(); i++ ){
-		Vector xyz_new = M * ( rsd.xyz( i ) - centroid ); // it is either this or M-inverse.
+		Vector xyz_new = stub.global2local( rsd.xyz( i ) ); // it is either this or M-inverse.
 		pose.set_xyz( AtomID( i, base_pos ), xyz_new );
 	}
 
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// This is imported from protocols/swa/RigidBodySampler.cco
+void
+sample_all_rotations_at_jump( pose::Pose & pose, Size const num_jump ){
+
+	Real alpha_, alpha_min_( 0 ), alpha_max_( 180.0 ), alpha_increment_( 60.0 );
+	Real beta_, cosbeta_min_( -1.0 ), cosbeta_max_( 1.0 ), cosbeta_increment_( 0.5 );
+	Real gamma_, gamma_min_( 0 ), gamma_max_( 180.0 ), gamma_increment_( 60.0 );
+
+	Matrix M;
+	Vector axis1( 1.0, 0.0, 0.0 ), axis2( 0.0, 1.0, 0.0 ), axis3( 0.0, 0.0, 1.0 );
+
+
+	Size  count( 0 );
+	for ( alpha_ = alpha_min_; alpha_ <= alpha_max_;  alpha_ += alpha_increment_ ){
+
+		//std::cout << i++ << " out of " << N_SAMPLE_ALPHA << ". Current count: " << count_total_ <<
+		//			". num poses that pass cuts: " << count_good_ << std::endl;
+
+		for ( Real cosbeta = cosbeta_min_; cosbeta <= cosbeta_max_;  cosbeta += cosbeta_increment_ ){
+			if ( cosbeta < -1.0 ){
+				beta_ = -1.0 * degrees( std::acos( -2.0 - cosbeta ) );
+			} else if ( cosbeta > 1.0 ){
+				beta_ = -1.0 * degrees( std::acos( 2.0 - cosbeta ) );
+			} else {
+				beta_ = degrees( std::acos( cosbeta ) );
+			}
+
+			std::cout << "BETA: " << beta_ << std::endl;
+
+			// Try to avoid singularity at pole.
+			Real gamma_min_local = gamma_min_;
+			Real gamma_max_local = gamma_max_;
+			Real gamma_increment_local = gamma_increment_;
+			if ( (beta_<-179.999 || beta_>179.999) ){
+				gamma_min_local = 0.0;
+				gamma_max_local = 0.0;
+				gamma_increment_local = 1.0;
+			}
+
+			for ( gamma_ = gamma_min_local; gamma_ <= gamma_max_local;  gamma_ += gamma_increment_local ){
+
+				protocols::swa::create_euler_rotation( M, alpha_, beta_, gamma_, axis1, axis2, axis3 );
+
+				kinematics::Jump jump = pose.jump( num_jump );
+				jump.set_rotation( M );
+				pose.set_jump( num_jump, jump );
+
+				pose.dump_pdb( "S_" + ObjexxFCL::string_of( count++ ) + ".pdb" );
+
+			} // gamma
+		} // beta
+	}// alpha
+
+
+}
+
 /////////////////////////////////////////////////////////////////////////////////
 void
 methane_pair_score_test()
@@ -143,21 +206,36 @@ methane_pair_score_test()
 	import_pose::pose_from_pdb( pose, *rsd_set, infile );
 
 	rotate_into_nucleobase_frame( pose );
+	pose.dump_pdb( "a_rotated.pdb" );
+
 	add_virtual_res(pose);
 	core::chemical::ResidueTypeSet const & residue_set = pose.residue_type ( 1 ).residue_type_set();
-	core::chemical::ResidueTypeCAPs const & rsd_type_list ( residue_set.name3_map ( "CCC" ) );
-	core::conformation::ResidueOP new_res ( core::conformation::ResidueFactory::create_residue ( *rsd_type_list[1] ) );
+
+	core::conformation::ResidueOP new_res;
+
+	bool const sample_water_ = option[ sample_water ]();
+
+	if ( sample_water_ ) {
+		core::chemical::ResidueTypeCAPs const & rsd_type_list ( residue_set.name3_map ( "TP3" ) );
+		new_res = ( core::conformation::ResidueFactory::create_residue ( *rsd_type_list[1] ) );
+	} else {
+		core::chemical::ResidueTypeCAPs const & rsd_type_list ( residue_set.name3_map ( "CCC" ) );
+		new_res = ( core::conformation::ResidueFactory::create_residue ( *rsd_type_list[1] ) );
+	}
 	pose.append_residue_by_jump ( *new_res , 2 );
+
+	pose.dump_pdb( "START.pdb" );
+
 
 	pose::add_variant_type_to_pose_residue( pose, "VIRTUAL_PHOSPHATE", 1 );
 
-	pose.dump_pdb( "a_rotated.pdb" );
 
 	//////////////////////////////////////////////////
 	// Set up fold tree -- "chain break" between two ligands, right?
 	// Uh, why not?
 	kinematics::FoldTree f ( pose.fold_tree() );
-	f.set_jump_atoms( 2,"ORIG"," C1 ");
+	std::string probe_atom_name = sample_water_ ? " O  " : " C1 ";
+	f.set_jump_atoms( 2,"ORIG",probe_atom_name);
 	pose.fold_tree( f );
 
 	//////////////////////////////////////////////////////////////////
@@ -177,6 +255,12 @@ methane_pair_score_test()
 	jump.set_translation( Vector( 0.0, 0.0, 5.0 ) );
 	pose.set_jump( 2, jump );
 	pose.dump_pdb( "shift_z.pdb" );
+
+	/// This is a code snippet to test if we are sampling water rotations properly -- could make this a little class,
+	//  and then include within translation scan.
+	if ( sample_water_ ) {
+		sample_all_rotations_at_jump( pose, 2 );
+	}
 
 	//////////////////////////////////////////////////////////////////
 	// OK, how about a score function?
@@ -229,6 +313,8 @@ my_main()
 int
 main( int argc, char * argv [] )
 {
+
+	NEW_OPT( sample_water, "use a water probe instead of carbon", false );
 
 	////////////////////////////////////////////////////////////////////////////
 	// setup
