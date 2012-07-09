fb2d938a7a76f52b7a1b6188c9bc682a983af6c0
Fang-Chieh Chou <fcchou@stanford.edu>
2012-07-09 19:15:02 +0000

    checkin personal app
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49794 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 5b299fce2e5fb6880d6ddb6560c826b77b8cd5ee

--- rosetta_source/src/apps/pilot/fcchou/adenosine_sample_around.cc
+++ rosetta_source/src/apps/pilot/fcchou/adenosine_sample_around.cc
@@ -15,30 +15,26 @@
 #include <core/types.hh>
 #include <core/chemical/AA.hh>
 #include <core/conformation/Residue.hh>
-#include <core/chemical/ResidueTypeSet.hh>
 #include <core/conformation/ResidueFactory.hh>
+#include <core/chemical/ResidueTypeSet.hh>
 #include <core/chemical/util.hh>
 #include <core/chemical/ChemicalManager.hh>
-
-#include <core/id/NamedAtomID.hh>
-#include <core/id/AtomID.hh>
 
 #include <core/scoring/ScoreFunction.hh>
 #include <core/scoring/ScoreFunctionFactory.hh>
 
+#include <core/scoring/rna/RNA_Util.hh>
 
 #include <core/kinematics/FoldTree.hh>
-#include <core/kinematics/Stub.hh>
 #include <core/kinematics/Jump.hh>
 #include <core/kinematics/MoveMap.hh>
 
 #include <core/io/silent/SilentFileData.hh>
 #include <core/io/silent/BinaryProteinSilentStruct.hh>
 
-#include <core/scoring/rna/RNA_Util.hh>
-
 #include <protocols/idealize/idealize.hh>
 
+#include <core/pose/util.hh>
 #include <core/pose/Pose.hh>
 #include <core/init.hh>
 
@@ -47,7 +43,6 @@
 #include <utility/io/izstream.hh>
 
 #include <numeric/xyzVector.hh>
-#include <numeric/xyzVector.string.hh>
 #include <numeric/conversions.hh>
 
 #include <ObjexxFCL/format.hh>
@@ -106,7 +101,6 @@ add_virtual_res ( core::pose::Pose & pose, bool set_res_as_root = true ) {
 		pose.fold_tree ( newF );
 	}
 }
-
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Rhiju -- rotate to my favorite frame. Base centroid is now at origin.
 //         X points to N1 atom. Z points normal to base. Y is orthonormal and points towards Hoogsteen edge, I think.
@@ -116,7 +110,6 @@ rotate_into_nucleobase_frame( core::pose::Pose & pose ){
 	using namespace core::conformation;
 	using namespace core::scoring::rna;
 	using namespace core::id;
-	using namespace core::kinematics;
 
 	// assuming pose has an RNA at residue 1 -- will rotate just that residue.
 	Size const base_pos( 1 );
@@ -124,18 +117,13 @@ rotate_into_nucleobase_frame( core::pose::Pose & pose ){
 
 	Vector centroid = get_rna_base_centroid( rsd, true /*verbose*/ );
 	Matrix M = get_rna_base_coordinate_system( rsd, centroid );
-	Stub stub( M, centroid );
 
 	for (Size i = 1; i <= rsd.natoms(); i++ ){
-		//		Vector xyz_new = M.transpose() * ( rsd.xyz( i ) - centroid ); // it is either M or its transpose
-		Vector xyz_new = stub.global2local( rsd.xyz(i) );
+		Vector xyz_new = M * ( rsd.xyz( i ) - centroid ); // it is either this or M-inverse.
 		pose.set_xyz( AtomID( i, base_pos ), xyz_new );
 	}
 
-	//	std::cout << pose.xyz( NamedAtomID( " N1 ", base_pos ) ) << std::endl;
-
 }
-
 /////////////////////////////////////////////////////////////////////////////////
 void
 methane_pair_score_test()
@@ -146,7 +134,7 @@ methane_pair_score_test()
 
 	//////////////////////////////////////////////////
 	ResidueTypeSetCAP rsd_set;
-	rsd_set = core::chemical::ChemicalManager::get_instance()->residue_type_set( "rna" );
+	rsd_set = core::chemical::ChemicalManager::get_instance()->residue_type_set( "fa_standard" );
 
 	// Read in pose with two methane. "Z" = ligand. Note need flag:
 	//         -extra_res_fa CH4.params -s two_methane.pdb
@@ -154,68 +142,74 @@ methane_pair_score_test()
 	std::string infile  = option[ in ::file::s ][1];
 	import_pose::pose_from_pdb( pose, *rsd_set, infile );
 
-	rotate_into_nucleobase_frame( pose );
-	pose.dump_pdb( "a_rotated.pdb" );
-
 	add_virtual_res(pose);
 	core::chemical::ResidueTypeSet const & residue_set = pose.residue_type ( 1 ).residue_type_set();
 	core::chemical::ResidueTypeCAPs const & rsd_type_list ( residue_set.name3_map ( "CCC" ) );
 	core::conformation::ResidueOP new_res ( core::conformation::ResidueFactory::create_residue ( *rsd_type_list[1] ) );
 	pose.append_residue_by_jump ( *new_res , 2 );
+	rotate_into_nucleobase_frame( pose );
+
+	pose::add_variant_type_to_pose_residue( pose, "VIRTUAL_PHOSPHATE", 1 );
+
+	pose.dump_pdb( "a_rotated.pdb" );
 
 	//////////////////////////////////////////////////
 	// Set up fold tree -- "chain break" between two ligands, right?
-	kinematics::FoldTree f( pose.total_residue() );
-	Size start( 1 ), end( 2 );
-	f.new_jump( start, end, start );
 	// Uh, why not?
-	f.set_jump_atoms( 1," C1 "," C1 ");
+	kinematics::FoldTree f ( pose.fold_tree() );
+	f.set_jump_atoms( 2,"ORIG"," C1 ");
 	pose.fold_tree( f );
 
 	//////////////////////////////////////////////////////////////////
 	// displace in z by 2.0 A... just checking coordinate system
 	//This jump should code for no translation or rotation -- two_benzenes.pdb
 	// has the two benzenes perfectly superimposed.
-	kinematics::Jump jump( pose.jump( 1 ) );
+	kinematics::Jump jump( pose.jump( 2 ) );
 
 	jump.set_translation( Vector( 5.0, 0.0, 0.0 ) );
-	pose.set_jump( 1, jump );
-	pose.dump_pdb( "shift_z.pdb" );
+	pose.set_jump( 2, jump );
+	pose.dump_pdb( "shift_x.pdb" );
 
+	jump.set_translation( Vector( 0.0, 5.0, 0.0 ) );
+	pose.set_jump( 2, jump );
+	pose.dump_pdb( "shift_y.pdb" );
+
+	jump.set_translation( Vector( 0.0, 0.0, 5.0 ) );
+	pose.set_jump( 2, jump );
+	pose.dump_pdb( "shift_z.pdb" );
 
 	//////////////////////////////////////////////////////////////////
 	// OK, how about a score function?
-	ScoreFunctionOP scorefxn = ScoreFunctionFactory::create_score_function( "vdw_test" );
+	ScoreFunctionOP scorefxn = ScoreFunctionFactory::create_score_function( "rna/rna_hires_07232011_with_intra_base_phosphate" );
 	//scorefxn->set_weight( hack_elec, 1.0 );
 
-	jump.set_translation( Vector( 1.3, 0.0, 0.0 ) );
-	pose.set_jump( 1, jump );
-	pose.dump_pdb( "test.pdb" );
 	(*scorefxn)( pose );
 	scorefxn->show( std::cout, pose );
 
 	//////////////////////////////////////////////////////////////////
-	// Now iterate over a bunch of translations and rotations.
+	// compute scores on a plane for now.
 	using namespace core::io::silent;
 	SilentFileData silent_file_data;
 	std::string const silent_file = option[ out::file::silent  ]();
 
 	Size n( 0 );
-	for (int i = 1; i <= 600; i++ ) {
-			Real const x = i * 0.01;
-			Real const y = 0.0;
+
+	for (Size i = -200; i <= 200; ++i) {
+		for (Size j = -200; j <= 200; ++j) {
+			Real const x = i * 0.1;
+			Real const y = j * 0.1;
 			Real const z = 0.0;
-
 			jump.set_translation( Vector( x, y, z ) ) ;
-			pose.set_jump( 1, jump );
-
+			pose.set_jump( 2, jump );
 			(*scorefxn)( pose );
 
 			n++;
-			std::string out_tag( "S_"+lead_zero_string_of(n,4) );
+			std::string out_tag( "S_" + lead_zero_string_of(n,10) );
 			BinaryProteinSilentStruct s( pose,  out_tag);
 			s.add_energy( "x", x );
+			s.add_energy( "y", y );
 			silent_file_data.write_silent_struct( s, silent_file, true /*write score only*/ );
+		}
 	}
 }
 
