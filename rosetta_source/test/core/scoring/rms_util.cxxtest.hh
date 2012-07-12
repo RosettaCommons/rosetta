bc1bfbe96848dbfaa9f5687051b35478da7e1582
Christopher Miles <cmiles@uw.edu>
2012-07-12 05:47:10 +0000

    Added unit tests for gdtsc
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49859 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 3f8d0063ff3c5bf822ada54668e71e50bd838ca0

--- rosetta_source/test/core/scoring/rms_util.cxxtest.hh
+++ rosetta_source/test/core/scoring/rms_util.cxxtest.hh
@@ -31,6 +31,7 @@
 using core::Real;
 using core::Size;
 using core::pose::Pose;
+using core::pose::PoseOP;
 
 class RmsUtilTest : public CxxTest::TestSuite {
  public:
@@ -67,6 +68,41 @@ class RmsUtilTest : public CxxTest::TestSuite {
     UT << "    no superposition, with automorphisms: " << automorphic_rmsd(pose1.residue(1), pose2.residue(1), false /*don't superimpose*/) << "\n";
     UT << "  with superposition, with automorphisms: " << automorphic_rmsd(pose1.residue(1), pose2.residue(1), true /*superimpose*/) << "\n";
   }
+
+	void test_gdtsc() {
+		PoseOP ref = core::import_pose::pose_from_pdb("core/scoring/4dO8B.pdb");
+		PoseOP mod = core::import_pose::pose_from_pdb("core/scoring/model-5.pdb");
+
+		utility::vector1<Size> all_residues;
+		for (Size i = 1; i <= ref->total_residue(); ++i) {
+			all_residues.push_back(i);
+		}
+
+		TS_ASSERT_DELTA(0.745, core::scoring::gdtsc(*ref, *mod, all_residues), 0.01);
+	}
+
+	void test_gdtsc_self() {
+		PoseOP mod = core::import_pose::pose_from_pdb("core/scoring/model-5.pdb");
+
+		utility::vector1<Size> all_residues;
+		for (Size i = 1; i <= mod->total_residue(); ++i) {
+			all_residues.push_back(i);
+		}
+
+		TS_ASSERT_DELTA(1.0, core::scoring::gdtsc(*mod, *mod, all_residues), 0.0001);
+	}
+
+	void test_gdtsc_mismatch_seq() {
+		PoseOP ref = core::import_pose::pose_from_pdb("core/scoring/4dO8B.pdb");
+		PoseOP mod = core::import_pose::pose_from_pdb("core/scoring/2GB3.pdb");
+
+		utility::vector1<Size> all_residues;
+		for (Size i = 1; i <= ref->total_residue(); ++i) {
+			all_residues.push_back(i);
+		}
+
+		TS_ASSERT_DELTA(-1, core::scoring::gdtsc(*ref, *mod, all_residues), 0.0001);
+	}
 
   void test_superimpose_self() {
     Pose pose = *core::import_pose::pose_from_pdb("core/scoring/2GB3.pdb");
