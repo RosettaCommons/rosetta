fdc042d4f98872ed8285cb5ce9838d7172b847bc
Steven Lewis <smlewi@gmail.com>
2012-07-21 17:59:04 +0000

    better fix for bug 41 (no explicit doubles, all core::Real).  No changes expected.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50044 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 12de2fafa5cfd5fe649752dd6f8b1e97d8e982c4

--- rosetta_source/src/protocols/canonical_sampling/ParallelTempering.cc
+++ rosetta_source/src/protocols/canonical_sampling/ParallelTempering.cc
@@ -197,9 +197,9 @@ ParallelTempering::finalize_simulation(
 			//Original code (line below) fails on some versions of GCC (4.4.3 and 4.3.4 reported by users)
 			//core::Real frequency(core::Real(exchange_accepts_[elem])/core::Real(exchange_attempts_[elem]));
 			//this replacement code is reported to work for those compilers
-			double a = exchange_accepts_[elem];
-      double b = exchange_attempts_[elem];
-      core::Real frequency(core::Real(a)/core::Real(b));
+			core::Real const a = exchange_accepts_[elem];
+			core::Real const b = exchange_attempts_[elem];
+			core::Real const frequency(a/b);
 			tr << temperature(i+1) << " <-> " << temperature(i+2) << ": " << frequency
 				 << " (" << exchange_accepts_[elem] << " of " << exchange_attempts_[elem] << ")" << std::endl;
 		}
