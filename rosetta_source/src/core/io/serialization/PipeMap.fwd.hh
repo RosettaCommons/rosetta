e99191147db0f86c713c6e50b511869e5e8f8e83
Kenneth Jung <kenjung@uw.edu>
2012-08-15 19:10:25 +0000

    Adds PipeMap class
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50424 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: d7c76da1c90660f71c6c310428e16b67a052a16f

---
+++ rosetta_source/src/core/io/serialization/PipeMap.fwd.hh
@@ -0,0 +1,36 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file core/io/serialization/PipeMap.fwd.hh
+/// @brief Quick typedef of a map of strings to PipeSP
+/// @author Ken Jung
+
+#ifndef INCLUDED_core_io_serialization_PipeMap_fwd_hh
+#define INCLUDED_core_io_serialization_PipeMap_fwd_hh
+
+#include <core/io/serialization/Pipe.fwd.hh>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <map>
+
+namespace core {
+namespace io {
+namespace serialization {
+
+typedef std::map< std::string, PipeSP > PipeMap;
+typedef boost::shared_ptr< PipeMap > PipeMapSP;
+typedef boost::weak_ptr< PipeMap > PipeMapWP;
+
+} //serialization
+} //io
+} //core
+
+#endif
+
+
