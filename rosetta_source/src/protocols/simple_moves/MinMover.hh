b64a19b1c6e6260713c96786f5dfe72b50655777
Kenneth Jung <kenjung@uw.edu>
2012-08-15 19:11:20 +0000

    Adds parse_def() to MinMover
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50468 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 1a1f3e0c977cf9c4187c0ef2b732de83d9a5807e

--- rosetta_source/src/protocols/simple_moves/MinMover.hh
+++ rosetta_source/src/protocols/simple_moves/MinMover.hh
@@ -1,7 +1,5 @@
 // -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
 // vi: set ts=2 noet:
-//
-// (c) Copyright Rosetta Commons Member Institutions.
 // (c) This file is part of the Rosetta software suite and is made available under license.
 // (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
 // (c) For more information, see http://www.rosettacommons.org. Questions about this can be
@@ -102,6 +100,16 @@ public:
 		Pose const & );
 
 	void parse_chi_and_bb( TagPtr const );
+
+  virtual void parse_def_opts( utility::lua::LuaObject const & def,
+		utility::lua::LuaObject const & score_fxns,
+		utility::lua::LuaObject const & tasks,
+		protocols::moves::MoverCacheSP cache );
+
+  virtual void parse_def( utility::lua::LuaObject const & def,
+		utility::lua::LuaObject const & score_fxns,
+		utility::lua::LuaObject const & tasks,
+		protocols::moves::MoverCacheSP cache );
 
 	/// @brief allow non-const access to the internal minimizer options object
 	virtual MinimizerOptionsOP min_options();
