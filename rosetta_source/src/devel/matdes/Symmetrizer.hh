0468f3852ea61f84b132f713a465f1a939570d00
Kenneth Jung <kenjung@uw.edu>
2012-08-15 19:11:27 +0000

    Adds parse_def() to Symmetrizer
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50473 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 83687fea3f34ead812fc5846d60bd4677b5ebd04

--- rosetta_source/src/devel/matdes/Symmetrizer.hh
+++ rosetta_source/src/devel/matdes/Symmetrizer.hh
@@ -67,6 +67,12 @@ public:
 														 Filters_map const &,
 														 Movers_map const &,
 														 Pose const & );
+
+	void parse_def( utility::lua::LuaObject const & def,
+					utility::lua::LuaObject const & score_fxns,
+					utility::lua::LuaObject const & tasks,
+					protocols::moves::MoverCacheSP cache );
+
 private:
 	Real get_radial_disp();
 	Real get_angle();
