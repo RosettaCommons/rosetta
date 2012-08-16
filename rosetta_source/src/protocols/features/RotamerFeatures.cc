RosettaCommons/rosetta

commit 0538f18a65ab75499885bbec65393415dd72a677

Extend schema generator use to all features reporters
Schema generator changes
  * Add the ability to add an index to the schema generator
  * Add Columns typedef
  * Add GreaterThan constraint to the schema generator

FeaturesReporter changes
  * write_schema_to_db is now pure virtual
  * the main report_features function is now pure virtual

feature database schema changes
  * White space changes in how schemas are printed
  * Add registrator for ProteinBondGeometryFeatures -> tables are added to schema

  * Remove nchi table from rotamer recovery RRReporterSQLite features level reporting because it's accessible with the RotamerFeatures reporter
  * constraints now have a regular naming scheme
  * In the geometric_solvation table change the type of hbond_site_id from TEXT to INTEGER
  * In the protein_backbone_atom_atom_pairs table change the type of resNum1/2 from TEXT to INTEGER 
  * Change BOOLEAN types to INTEGER types because they are the same in SQLite (are they the same in MySQL/Postgres?)
  * Fix typo in the hbond_dehydron table
  * Fix capitalization in a few places for greater consistency
  
  * change the foreign key in score_types table from protocol_id to batch_id. Recall that a batch is corresponds to a set of structures where a protocol correpsonds to a single run of rosetta.

This will cause integration test changes in integration tests that use FeatureReporter classes


git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49549 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: ddceb19e2b6813d7f7e8ea3b46e5fb6aab331709

rosetta_source/src/protocols/features/RotamerFeatures.cc
@@ -91,15 +91,16 @@ RotamerFeatures::type_name() const {
 	return "RotamerFeatures";
 }
 
-string
-RotamerFeatures::schema(
+void
+RotamerFeatures::write_schema_to_db(
+	sessionOP db_session
 ) const {
-
-	return residue_rotamers_schema().print() + "\n";
+	write_residue_rotamers_table_schema(db_session);
 }
 
-basic::database::schema_generator::Schema
-RotamerFeatures::residue_rotamers_schema(
+void
+RotamerFeatures::write_residue_rotamers_table_schema(
+	sessionOP db_session
 ) const {
 	using namespace basic::database::schema_generator;
 
@@ -161,7 +162,7 @@ RotamerFeatures::residue_rotamers_schema(
 	residue_rotamers.add_column(chi4_deviation);
 	residue_rotamers.add_column(rotamer_bin_probability);
 	residue_rotamers.add_foreign_key(residues_foreign_key);
-	return residue_rotamers;
+	residue_rotamers.write(db_session);
 }
 
 
@@ -213,7 +214,7 @@ RotamerFeatures::report_features(
   }
 
 	string insert_sql(
-		"INSERT INTO residue_rotamers ("
+		"INSERT INTO residue_rotamers (struct_id, residue_number, rotamer_bin, nchi, semi_rotameric, chi1_mean, chi2_mean, chi3_mean, chi4_mean, chi1_standard_deviation, chi2_standard_deviation, chi3_standard_deviation, chi4_standard_deviation, chi1_deviation, chi2_deviation, chi3_deviation, chi4_deviation, rotamer_bin_probability) ("
 		"	struct_id,"
 		"	residue_number,"
 		"	rotamer_bin,"

commit dee655e5d38b8d01dc9d0c4a924da2ef98677c1a

Fix typo in RotamerFeatures introduced in r49549. No test failures expected.
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50586 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: b8b6e00aa5bb6665d8c3a844fbfab14be83eb129

rosetta_source/src/protocols/features/RotamerFeatures.cc
@@ -214,7 +214,7 @@ RotamerFeatures::report_features(
   }
 
 	string insert_sql(
-		"INSERT INTO residue_rotamers (struct_id, residue_number, rotamer_bin, nchi, semi_rotameric, chi1_mean, chi2_mean, chi3_mean, chi4_mean, chi1_standard_deviation, chi2_standard_deviation, chi3_standard_deviation, chi4_standard_deviation, chi1_deviation, chi2_deviation, chi3_deviation, chi4_deviation, rotamer_bin_probability) ("
+		"INSERT INTO residue_rotamers ("
 		"	struct_id,"
 		"	residue_number,"
 		"	rotamer_bin,"