RosettaCommons/rosetta

commit 600653fd43a4390dffb202f2fb87f0f1e97ece51

Improve the reporting of scores to features databases
* Modify references to ScoreTypes to include batch_id to allow score types to change between runs. 

* Add ResidueTotalScores and ScoreFunction Features reporters. Getting the total scores from the ResidueScores table was surprisingly annoying using SQL.

* Add a function getScoreFunctionName to the ScoreFunctionFactory namespace that creates a name for a score function using the same logic as getScoreFunction. An alternative solution would be to store a name in each score function.

* Add a function get_batch_id(struct_id, db_session) that looks up in the database the batch_id.

* Fix a few warnings

* Reorganize the features and features_postgres integration test parser_script.xml to match the wiki documentation layout

This will cause acceptable integration test changes in the features related integration tests



git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@49684 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: c58d348479b1b8e53f89c4421eb1eebeca0046ee

rosetta_source/src/core/scoring/ScoreFunctionFactory.cc
@@ -36,7 +36,7 @@
 // AUTO-REMOVED #include <basic/options/keys/constraints.OptionKeys.gen.hh>
 #include <basic/options/keys/abinitio.OptionKeys.gen.hh>
 #include <basic/options/keys/symmetry.OptionKeys.gen.hh>
-
+#include <sstream>
 
 using basic::T;
 
@@ -299,6 +299,69 @@ core::scoring::ScoreFunctionOP getScoreFunction( bool const is_fullatom /* defau
 
 	return scorefxn;
 }
+
+std::string
+getScoreFunctionName(
+	bool const is_fullatom /* default true */
+) {
+	using basic::options::option;
+	using namespace basic::options::OptionKeys;
+
+	if( option[ score::empty ]() ) return "empty";
+
+	std::string weight_set = option[ score::weights ];
+	utility::vector1< std::string > patch_tags = option[ score::patch ]();
+
+
+	if ( !option[ score::weights ].user() && !is_fullatom ){
+
+		// Defalt score of centroid is cen_wts when is_fullatom is false and user has not specified a score weights
+		weight_set = CENTROID_WTS;
+
+	}else{
+
+		/// Default score is score12 if the user has not specified a score weights file or a patch file
+		/// on the command line.  If the user has specified that they would like the standard weight set,
+		/// and has not also asked for the score12 patch, then do not apply the score12 patch to it.
+		if ( ( weight_set == "standard" && !option[ score::weights ].user() ) &&
+				 ( !option[ score::patch ].user() ) ) {
+			patch_tags.push_back( "score12" );
+			if( option[ corrections::correct ]) {
+				weight_set = "score12_w_corrections";
+				patch_tags.clear();
+			} else if ( option[ corrections::score::score12prime ] ) {
+				weight_set = "score12prime";
+				patch_tags.clear();
+			}
+		}
+	}
+
+	if ( patch_tags.size() != 0 &&
+		weight_set == STANDARD_WTS &&
+		option[ corrections::score::score12prime ] ) {
+
+		bool sc12patch = false;
+		for ( Size ii = 1; ii <= patch_tags.size(); ++ii ) {
+			if ( patch_tags[ ii ] == SCORE12_PATCH ) {
+				patch_tags[ ii ] = "NOPATCH";
+				sc12patch = true;
+			}
+		}
+		if ( sc12patch ) {
+			weight_set = "score12prime";
+		}
+	}
+
+	std::stringstream patch_string;
+	for(Size ii=1; ii <- patch_tags.size(); ++ii){
+		if( patch_tags[ii] == "NOPATCH" ) continue;
+		patch_string << "_" << patch_tags[ii];
+	}
+
+	return weight_set + patch_string.str();
+}
+
+
 
 } // namespace scoring
 } // namespace core

commit 4e6fd84bfa74b963207f0655df24a01104f9289e

Add UnrecognizedAtomFeatures reporter. Fix a bug in getScoreFunctionName() introduced in r49684. I expect changes in the features_database_schema integration test.
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50584 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: 2987824338a14169d8b43f36fc9cd3a0826de4c6

rosetta_source/src/core/scoring/ScoreFunctionFactory.cc
@@ -353,7 +353,7 @@ getScoreFunctionName(
 	}
 
 	std::stringstream patch_string;
-	for(Size ii=1; ii <- patch_tags.size(); ++ii){
+	for(Size ii=1; ii <= patch_tags.size(); ++ii){
 		if( patch_tags[ii] == "NOPATCH" ) continue;
 		patch_string << "_" << patch_tags[ii];
 	}

rosetta_source/src/protocols/features/PairFeatures.cc
@@ -174,7 +174,7 @@ PairFeatures::report_residue_pairs(
 
 	for(Size resNum1=1; resNum1 <= pose.total_residue(); ++resNum1){
 		if(!relevant_residues[resNum1]) continue;
-		Residue res1( pose.residue(resNum1) );
+		Residue const & res1( pose.residue(resNum1) );
 
 		Size res1_10A_neighbors(
 			tenA.get_node(resNum1)->num_neighbors_counting_self_static());
@@ -182,7 +182,7 @@ PairFeatures::report_residue_pairs(
 		// TODO: just iterate over the neighbors of res1
 		for(Size resNum2=resNum1+1; resNum2 <= pose.total_residue(); ++resNum2){
 			if(!relevant_residues[resNum2]) continue;
-			Residue res2( pose.residue(resNum2) );
+			Residue const & res2( pose.residue(resNum2) );
 
 			Distance const actcoord_dist( res1.actcoord().distance( res2.actcoord() ) );
 

rosetta_source/src/protocols/features/UnrecognizedAtomFeatures.cc
@@ -1,0 +1,417 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/features/UnrecognizedAtomFeatures.cc
+/// @brief  report unrecognized atoms features to features Statistics Scientific Benchmark
+/// @author Matthew O'Meara
+
+// Unit Headers
+#include <protocols/features/UnrecognizedAtomFeatures.hh>
+
+//External
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+// Project Headers
+#include <basic/options/option.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
+#include <basic/options/keys/inout.OptionKeys.gen.hh>
+#include <core/conformation/Residue.hh>
+#include <core/pose/Pose.hh>
+#include <core/pose/PDBInfo.hh>
+#include <core/types.hh>
+#include <core/io/pdb/file_data.hh>
+#include <protocols/moves/DataMap.hh>
+#include <utility/tag/Tag.hh>
+
+//Basic Headers
+#include <basic/database/sql_utils.hh>
+#include <basic/database/schema_generator/PrimaryKey.hh>
+#include <basic/database/schema_generator/ForeignKey.hh>
+#include <basic/database/schema_generator/Column.hh>
+#include <basic/database/schema_generator/Schema.hh>
+#include <basic/database/schema_generator/Constraint.hh>
+#include <basic/Tracer.hh>
+#include <basic/options/option.hh>
+#include <basic/options/keys/inout.OptionKeys.gen.hh>
+
+#include <basic/database/insert_statement_generator/InsertGenerator.hh>
+#include <basic/database/insert_statement_generator/RowData.hh>
+
+
+// Utility Headers
+#include <utility/vector1.hh>
+#include <utility/sql_database/DatabaseSessionManager.hh>
+#include <utility/tools/make_vector.hh>
+
+// External Headers
+#include <cppdb/frontend.h>
+
+// Boost Headers
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+namespace protocols{
+namespace features{
+
+static basic::Tracer TR("protocols.features.UnrecognizedAtomFeatures");
+
+using std::string;
+using std::map;
+using std::endl;
+using std::pair;
+using std::make_pair;
+using core::Size;
+using core::Real;
+using core::Distance;
+using core::pose::UnrecognizedAtomRecord;
+using core::pose::Pose;
+using core::pose::PDBInfoCOP;
+using core::conformation::Residue;
+using protocols::filters::Filters_map;
+using protocols::moves::DataMap;
+using protocols::moves::Movers_map;
+using utility::tag::TagPtr;
+using utility::vector1;
+using utility::tools::make_vector;
+using utility::sql_database::sessionOP;
+using boost::uuids::uuid;
+using cppdb::statement;
+using basic::database::insert_statement_generator::InsertGenerator;
+using basic::database::insert_statement_generator::RowDataBaseOP;
+using basic::database::insert_statement_generator::RowData;
+
+UnrecognizedAtomFeatures::UnrecognizedAtomFeatures() :
+	neighbor_distance_cutoff_(12.0)
+{
+	if(!basic::options::option[basic::options::OptionKeys::in::remember_unrecognized_res]() || !basic::options::option[basic::options::OptionKeys::in::remember_unrecognized_water]()){
+		TR.Warning << "Use -in:remember_unrecognized_res and -in:remember_unrecognized_water to locate unrecognized atoms." << endl;
+	}
+}
+
+UnrecognizedAtomFeatures::UnrecognizedAtomFeatures(
+	Real neighbor_distance_cutoff
+) :
+	neighbor_distance_cutoff_(neighbor_distance_cutoff)
+{}
+
+UnrecognizedAtomFeatures::UnrecognizedAtomFeatures(
+	UnrecognizedAtomFeatures const & src
+) :
+	FeaturesReporter(),
+	neighbor_distance_cutoff_(src.neighbor_distance_cutoff_)
+{}
+
+UnrecognizedAtomFeatures::~UnrecognizedAtomFeatures()
+{}
+
+string
+UnrecognizedAtomFeatures::type_name() const { return "UnrecognizedAtomFeatures"; }
+
+void
+UnrecognizedAtomFeatures::write_schema_to_db(
+	sessionOP db_session
+) const {
+	write_unrecognized_residues_table_schema(db_session);
+	write_unrecognized_atoms_table_schema(db_session);
+	write_unrecognized_neighbors_table_schema(db_session);
+}
+
+void
+UnrecognizedAtomFeatures::write_unrecognized_residues_table_schema(
+	sessionOP db_session
+) const {
+
+	using namespace basic::database::schema_generator;
+
+	Column struct_id("struct_id", DbUUID());
+	Column residue_number("residue_number", DbInteger());
+	Column name3("name3", DbText());
+	Column max_temperature("max_temperature", DbReal());
+
+	Columns residues_pkey_cols;
+	residues_pkey_cols.push_back(struct_id);
+	residues_pkey_cols.push_back(residue_number);
+
+	Columns foreign_key_columns;
+	foreign_key_columns.push_back(struct_id);
+	vector1< string > reference_columns;
+	reference_columns.push_back("struct_id");
+	ForeignKey foreign_key(foreign_key_columns, "structures", reference_columns, true);
+
+	Schema table("unrecognized_residues", PrimaryKey(residues_pkey_cols));
+	table.add_column(name3);
+	table.add_column(max_temperature);
+	table.add_foreign_key(foreign_key);
+	table.write(db_session);
+}
+
+
+void
+UnrecognizedAtomFeatures::write_unrecognized_atoms_table_schema(
+	sessionOP db_session
+) const {
+
+	using namespace basic::database::schema_generator;
+
+	Column struct_id("struct_id", DbUUID());
+	Column residue_number("residue_number", DbInteger());
+	Column atom_name("atom_name", DbText());
+	Column coord_x("coord_x", DbReal());
+	Column coord_y("coord_y", DbReal());
+	Column coord_z("coord_z", DbReal());
+	Column temperature("temperature", DbReal());
+
+	Columns residues_pkey_cols;
+	residues_pkey_cols.push_back(struct_id);
+	residues_pkey_cols.push_back(residue_number);
+	residues_pkey_cols.push_back(atom_name);
+
+	Columns foreign_key_columns;
+	foreign_key_columns.push_back(struct_id);
+	vector1< string > reference_columns;
+	reference_columns.push_back("struct_id");
+	ForeignKey foreign_key(foreign_key_columns, "structures", reference_columns, true);
+
+	Schema table("unrecognized_atoms", PrimaryKey(residues_pkey_cols));
+	table.add_column(coord_x);
+	table.add_column(coord_y);
+	table.add_column(coord_z);
+	table.add_column(temperature);
+	table.add_foreign_key(foreign_key);
+	table.write(db_session);
+}
+
+void
+UnrecognizedAtomFeatures::write_unrecognized_neighbors_table_schema(
+	sessionOP db_session
+) const {
+
+	using namespace basic::database::schema_generator;
+
+	Column struct_id("struct_id", DbUUID(), false);
+	Column residue_number("residue_number", DbInteger(), false);
+	Column unrecognized_residue_number("unrecognized_residue_number", DbReal(), false);
+	Column closest_contact("closest_contact", DbReal(), false);
+
+	Columns primary_key_columns;
+	primary_key_columns.push_back(struct_id);
+	primary_key_columns.push_back(residue_number);
+	PrimaryKey primary_key(primary_key_columns);
+
+	Columns foreign_key_columns;
+	foreign_key_columns.push_back(struct_id);
+	foreign_key_columns.push_back(residue_number);
+	vector1< std::string > reference_columns;
+	reference_columns.push_back("struct_id");
+	reference_columns.push_back("resNum");
+	ForeignKey foreign_key(foreign_key_columns, "residues", reference_columns, true);
+	// No foreign key for neighbor_residue_number, because as there is
+	// currently just unrecognized atoms, not unrecognized residues.
+
+	Schema table("unrecognized_neighbors", primary_key);
+	table.add_column(unrecognized_residue_number);
+	table.add_column(closest_contact);
+	table.add_foreign_key(foreign_key);
+	table.write(db_session);
+}
+
+
+utility::vector1<std::string>
+UnrecognizedAtomFeatures::features_reporter_dependencies() const {
+	utility::vector1<std::string> dependencies;
+	dependencies.push_back("StructureFeatures");
+	dependencies.push_back("ResidueFeatures");
+	return dependencies;
+}
+
+void
+UnrecognizedAtomFeatures::parse_my_tag(
+	TagPtr const tag,
+	DataMap & /*data*/,
+	Filters_map const & /*filters*/,
+	Movers_map const & /*movers*/,
+	Pose const & /*pose*/
+) {
+	neighbor_distance_cutoff_ = tag->getOption<Real>("neighbor_distance_cutoff", 12.0);
+}
+
+Size
+UnrecognizedAtomFeatures::report_features(
+	Pose const & pose,
+	vector1< bool > const & relevant_residues,
+	boost::uuids::uuid const struct_id,
+	sessionOP db_session
+){
+	insert_unrecognized_residues_rows(pose, struct_id, db_session);
+	insert_unrecognized_atoms_rows(pose, struct_id, db_session);
+	insert_unrecognized_neighbors_rows(pose, relevant_residues, struct_id, db_session);
+	return 0;
+}
+
+
+void
+UnrecognizedAtomFeatures::insert_unrecognized_residues_rows(
+	Pose const & pose,
+	boost::uuids::uuid const struct_id,
+	sessionOP db_session
+){
+	PDBInfoCOP pdb_info(pose.pdb_info());
+	if(!pdb_info) return;
+
+
+	InsertGenerator insert_generator("unrecognized_residues");
+	insert_generator.add_column("struct_id");
+	insert_generator.add_column("residue_number");
+	insert_generator.add_column("name3");
+	insert_generator.add_column("max_temperature");
+
+	RowDataBaseOP struct_id_data = new RowData<boost::uuids::uuid>("struct_id", struct_id);
+
+	map< Size, UnrecognizedAtomRecord const * > ur_found;
+
+	for(
+		vector1< UnrecognizedAtomRecord >::const_iterator
+			ua(pdb_info->get_unrecognized_atoms().begin()),
+			ua_end(pdb_info->get_unrecognized_atoms().end());
+		ua != ua_end; ++ua){
+		map< Size, UnrecognizedAtomRecord const * >::const_iterator
+			i(ur_found.find(ua->res_num()));
+		if( i == ur_found.end()){
+			ur_found[ua->res_num()] = &(*ua);
+		} else {
+			if (i->second->temp() > ua->temp()){
+				ur_found[ua->res_num()] = &(*ua);
+			}
+		}
+	}
+
+	for(
+		map<Size, UnrecognizedAtomRecord const * >::const_iterator
+			i = ur_found.begin(), ie = ur_found.end();
+		i != ie; ++i){
+
+		UnrecognizedAtomRecord const & ua(*(i->second));
+
+		insert_generator.add_row(
+			make_vector(
+				struct_id_data,
+				RowDataBaseOP(new RowData<Size>("residue_number", ua.res_num())),
+				RowDataBaseOP(new RowData<string>("name3", ua.res_name())),
+				RowDataBaseOP(new RowData<Real>("max_temperature", ua.temp()))));
+	}
+
+	insert_generator.write_to_database(db_session);
+}
+
+
+void
+UnrecognizedAtomFeatures::insert_unrecognized_atoms_rows(
+	Pose const & pose,
+	uuid const struct_id,
+	sessionOP db_session
+){
+	PDBInfoCOP pdb_info(pose.pdb_info());
+	if(!pdb_info) return;
+
+
+	InsertGenerator insert_generator("unrecognized_atoms");
+	insert_generator.add_column("struct_id");
+	insert_generator.add_column("residue_number");
+	insert_generator.add_column("atom_name");
+	insert_generator.add_column("coord_x");
+	insert_generator.add_column("coord_y");
+	insert_generator.add_column("coord_z");
+	insert_generator.add_column("temperature");
+
+	RowDataBaseOP struct_id_data = new RowData<uuid>("struct_id", struct_id);
+
+	foreach( UnrecognizedAtomRecord ua, pdb_info->get_unrecognized_atoms()){
+
+		insert_generator.add_row(
+			make_vector(
+				struct_id_data,
+				RowDataBaseOP(new RowData<Size>("residue_number", ua.res_num())),
+				RowDataBaseOP(new RowData<string>("atom_name", ua.atom_name())),
+				RowDataBaseOP(new RowData<Real>("coord_x", ua.coords().x())),
+				RowDataBaseOP(new RowData<Real>("coord_y", ua.coords().y())),
+				RowDataBaseOP(new RowData<Real>("coord_z", ua.coords().z())),
+				RowDataBaseOP(new RowData<Real>("temperature", ua.temp()))));
+
+	}
+
+	insert_generator.write_to_database(db_session);
+}
+
+void
+UnrecognizedAtomFeatures::insert_unrecognized_neighbors_rows(
+	Pose const & pose,
+	vector1< bool > const & relevant_residues,
+	uuid const struct_id,
+	sessionOP db_session
+){
+	PDBInfoCOP pdb_info(pose.pdb_info());
+	if(!pdb_info) return;
+
+
+	InsertGenerator insert_generator("unrecognized_neighbors");
+	insert_generator.add_column("struct_id");
+	insert_generator.add_column("residue_number");
+	insert_generator.add_column("unrecognized_residue_number");
+	insert_generator.add_column("closest_contact");
+
+	RowDataBaseOP struct_id_data = new RowData<uuid>("struct_id", struct_id);
+
+	map< Size, pair< Size, Real > > closest_contact;
+
+
+	for(Size resNum=1; resNum <= pose.total_residue(); ++resNum){
+		if(!relevant_residues[resNum]) continue;
+		Residue const & res(pose.residue(resNum));
+
+		Size closest_ua_resNum(0);
+		Distance closest_ua_distance(10000000);
+		foreach( UnrecognizedAtomRecord ua, pdb_info->get_unrecognized_atoms()){
+			Distance const ua_distance(res.actcoord().distance(ua.coords()));
+			if(
+				ua_distance < neighbor_distance_cutoff_ &&
+				ua_distance < closest_ua_distance){
+				closest_ua_resNum = ua.res_num();
+				closest_ua_distance = ua_distance;
+			}
+		}
+		if(closest_ua_distance < neighbor_distance_cutoff_){
+			insert_generator.add_row(
+				make_vector(
+					struct_id_data,
+					RowDataBaseOP(new RowData<Size>("residue_number", resNum)),
+					RowDataBaseOP(
+						new RowData<Size>("unrecognized_residue_number", closest_ua_resNum)),
+					RowDataBaseOP(
+						new RowData<Distance>("closest_contact", closest_ua_distance))));
+		}
+	}
+
+	insert_generator.write_to_database(db_session);
+}
+
+
+void
+UnrecognizedAtomFeatures::delete_record(
+	boost::uuids::uuid struct_id,
+	sessionOP db_session) {
+
+	delete_records_from_table("unrecognized_atoms", struct_id, db_session);
+	delete_records_from_table("unrecognized_neighbors", struct_id, db_session);
+}
+
+
+
+} // namesapce
+} // namespace

rosetta_source/src/protocols/features/UnrecognizedAtomFeatures.fwd.hh
@@ -1,0 +1,30 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/features/UnrecognizedAtomFeatures.fwd.hh
+/// @brief  report residue features to features Statistics Scientific Benchmark
+/// @author Matthew O'Meara
+
+#ifndef INCLUDED_protocols_features_UnrecognizedAtomFeatures_fwd_hh
+#define INCLUDED_protocols_features_UnrecognizedAtomFeatures_fwd_hh
+
+// Utility headers
+#include <utility/pointer/owning_ptr.hh>
+
+namespace protocols{
+namespace features{
+
+class UnrecognizedAtomFeatures;
+typedef utility::pointer::owning_ptr< UnrecognizedAtomFeatures > UnrecognizedAtomFeaturesOP;
+typedef utility::pointer::owning_ptr< UnrecognizedAtomFeatures const > UnrecognizedAtomFeaturesCOP;
+
+}//features
+}//protocols
+
+#endif //INCLUDED_protocols_features_UnrecognizedAtomFeatures_fwd_hh

rosetta_source/src/protocols/features/UnrecognizedAtomFeatures.hh
@@ -1,0 +1,133 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/features/UnrecognizedAtomFeatures.hh
+/// @brief  report residue to features Statistics Scientific Benchmark
+/// @author Matthew O'Meara
+
+#ifndef INCLUDED_protocols_features_UnrecognizedAtomFeatures_hh
+#define INCLUDED_protocols_features_UnrecognizedAtomFeatures_hh
+
+// Unit Headers
+#include <protocols/features/FeaturesReporter.hh>
+#include <protocols/features/UnrecognizedAtomFeatures.fwd.hh>
+
+//External
+#include <boost/uuid/uuid.hpp>
+
+// Project Headers
+#include <core/pose/Pose.fwd.hh>
+#include <core/types.hh>
+#include <utility/vector1.hh>
+#include <protocols/moves/DataMap.fwd.hh>
+#include <utility/tag/Tag.fwd.hh>
+
+// C++ Headers
+#include <string>
+
+
+
+namespace protocols{
+namespace features{
+
+class UnrecognizedAtomFeatures : public protocols::features::FeaturesReporter {
+public:
+	UnrecognizedAtomFeatures();
+
+	UnrecognizedAtomFeatures(core::Real neighbor_distance_cutoff);
+
+	UnrecognizedAtomFeatures(UnrecognizedAtomFeatures const & src);
+
+	virtual ~UnrecognizedAtomFeatures();
+
+	///@brief return string with class name
+	std::string
+	type_name() const;
+
+	///@brief generate the table schemas and write them to the database
+	virtual void
+	write_schema_to_db(utility::sql_database::sessionOP db_session) const;
+
+private:
+	///@brief generate the unrecognized_atoms table schema
+	virtual void
+	write_unrecognized_atoms_table_schema(
+		utility::sql_database::sessionOP db_session
+	) const;
+
+	///@brief generate the unrecognized_residues table schema
+	virtual void
+	write_unrecognized_residues_table_schema(
+		utility::sql_database::sessionOP db_session
+	) const;
+
+	///@brief generate the unrecognized_neighbors table schema
+	virtual void
+	write_unrecognized_neighbors_table_schema(
+		utility::sql_database::sessionOP db_session
+	) const;
+
+public:
+	///@brief return the set of features reporters that are required to
+	///also already be extracted by the time this one is used.
+	utility::vector1<std::string>
+	features_reporter_dependencies() const;
+
+	void
+	parse_my_tag(
+		utility::tag::TagPtr const tag,
+		protocols::moves::DataMap & /*data*/,
+		protocols::filters::Filters_map const & /*filters*/,
+		protocols::moves::Movers_map const & /*movers*/,
+		core::pose::Pose const & /*pose*/);
+
+	///@brief collect all the feature data for the pose
+	core::Size
+	report_features(
+		core::pose::Pose const & pose,
+		utility::vector1< bool > const & relevant_residues,
+		boost::uuids::uuid struct_id,
+		utility::sql_database::sessionOP db_session);
+
+private:
+	void
+	insert_unrecognized_residues_rows(
+		core::pose::Pose const & pose,
+		boost::uuids::uuid struct_id,
+		utility::sql_database::sessionOP db_session);
+
+	void
+	insert_unrecognized_atoms_rows(
+		core::pose::Pose const & pose,
+		boost::uuids::uuid struct_id,
+		utility::sql_database::sessionOP db_session);
+
+	void
+	insert_unrecognized_neighbors_rows(
+		core::pose::Pose const & pose,
+		utility::vector1< bool > const & relevant_residues,
+		boost::uuids::uuid struct_id,
+		utility::sql_database::sessionOP db_session);
+
+public:
+	void
+	delete_record(
+		boost::uuids::uuid struct_id,
+		utility::sql_database::sessionOP db_sesion);
+
+private:
+
+	core::Real neighbor_distance_cutoff_;
+
+};
+
+} // namespace
+} // namespace
+
+#endif // include guard

rosetta_source/src/protocols/features/UnrecognizedAtomFeaturesCreator.cc
@@ -1,0 +1,40 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/feature/UnrecognizedAtomFeaturesCreator.hh
+/// @brief  Header for UnrecognizedAtomFeaturesCreator for the UnrecognizedAtomFeatures load-time factory registration scheme
+/// @author Matthew O'Meara
+
+// Unit Headers
+#include <protocols/features/UnrecognizedAtomFeaturesCreator.hh>
+
+// Package Headers
+
+#include <protocols/features/FeaturesReporterCreator.hh>
+// AUTO-REMOVED #include <protocols/features/FeaturesReporterFactory.hh>
+
+#include <protocols/features/UnrecognizedAtomFeatures.hh>
+#include <utility/vector1.hh>
+
+
+namespace protocols {
+namespace features {
+
+UnrecognizedAtomFeaturesCreator::UnrecognizedAtomFeaturesCreator() {}
+UnrecognizedAtomFeaturesCreator::~UnrecognizedAtomFeaturesCreator() {}
+FeaturesReporterOP UnrecognizedAtomFeaturesCreator::create_features_reporter() const {
+	return new UnrecognizedAtomFeatures;
+}
+
+std::string UnrecognizedAtomFeaturesCreator::type_name() const {
+	return "UnrecognizedAtomFeatures";
+}
+
+} //namespace features
+} //namespace protocols

rosetta_source/src/protocols/features/UnrecognizedAtomFeaturesCreator.hh
@@ -1,0 +1,45 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/features/UnrecognizedAtomFeaturesCreator.hh
+/// @brief  Header for UnrecognizedAtomFeaturesCreator for the UnrecognizedAtomFeatures load-time factory registration scheme
+/// @author Matthew O'Meara
+
+#ifndef INCLUDED_protocols_features_UnrecognizedAtomFeaturesCreator_hh
+#define INCLUDED_protocols_features_UnrecognizedAtomFeaturesCreator_hh
+
+// Unit Headers
+// AUTO-REMOVED #include <protocols/features/UnrecognizedAtomFeatures.hh>
+#include <protocols/features/FeaturesReporterCreator.hh>
+
+// c++ headers
+// AUTO-REMOVED #include <string>
+
+#include <core/types.hh>
+#include <utility/vector1.hh>
+
+
+namespace protocols {
+namespace features {
+
+/// @brief creator for the UnrecognizedAtomFeatures class
+class UnrecognizedAtomFeaturesCreator : public FeaturesReporterCreator
+{
+public:
+	UnrecognizedAtomFeaturesCreator();
+	virtual ~UnrecognizedAtomFeaturesCreator();
+
+	virtual FeaturesReporterOP create_features_reporter() const;
+	virtual std::string type_name() const;
+};
+
+} //namespace features
+} //namespace protocols
+
+#endif

rosetta_source/src/protocols/init/init.FeaturesReporterCreators.ihh
@@ -51,6 +51,7 @@
 #include <protocols/features/StructureFeaturesCreator.hh>
 #include <protocols/features/StructureScoresFeaturesCreator.hh>
 #include <protocols/features/ScoreFunctionFeaturesCreator.hh>
+#include <protocols/features/UnrecognizedAtomFeaturesCreator.hh>
 #include <protocols/features/BatchFeaturesCreator.hh>
 #include <protocols/features/helixAssembly/HelixBundleFeaturesCreator.hh>
 #include <protocols/features/helixAssembly/ConcurrencyTestCreator.hh>

rosetta_source/src/protocols/init/init.FeaturesReporterRegistrators.ihh
@@ -54,6 +54,7 @@ static FeaturesReporterRegistrator< ScoreTypeFeaturesCreator > ScoreTypeFeatures
 static FeaturesReporterRegistrator< StructureFeaturesCreator > StructureFeaturesCreator_registrator;
 static FeaturesReporterRegistrator< StructureScoresFeaturesCreator > StructureScoresFeaturesCreator_registrator;
 static FeaturesReporterRegistrator< ScoreFunctionFeaturesCreator > ScoreFunctionFeaturesCreator_registrator;
+static FeaturesReporterRegistrator< UnrecognizedAtomFeaturesCreator > UnrecognizedAtomFeaturesCreator_registrator;
 static FeaturesReporterRegistrator< helixAssembly::HelixBundleFeaturesCreator > HelixBundleFeaturesCreator_registrator;
 static FeaturesReporterRegistrator< helixAssembly::ConcurrencyTestCreator > ConcurrencyTestCreator_registrator;
 static FeaturesReporterRegistrator< BatchFeaturesCreator > BatchFeaturesCreator_registrator;

rosetta_source/src/protocols_c.4.src.settings
@@ -96,6 +96,8 @@ sources = {
 		"StructureFeaturesCreator",
 		"StructureScoresFeatures",
 		"StructureScoresFeaturesCreator",
+		"UnrecognizedAtomFeatures",
+		"UnrecognizedAtomFeaturesCreator",
 		"util",
 	],
 	"protocols/features/helixAssembly": [

rosetta_source/test/protocols/features/FeaturesReporterTests.cxxtest.hh
@@ -52,6 +52,7 @@
 #include <protocols/features/SaltBridgeFeatures.hh>
 #include <protocols/features/StructureFeatures.hh>
 #include <protocols/features/StructureScoresFeatures.hh>
+#include <protocols/features/UnrecognizedAtomFeatures.hh>
 
 
 // Project Headers
@@ -132,6 +133,7 @@ public:
 		features_reporters_.push_back(new SaltBridgeFeatures());
 		features_reporters_.push_back(new StructureFeatures());
 		features_reporters_.push_back(new StructureScoresFeatures(score_function_));
+		features_reporters_.push_back(new UnrecognizedAtomFeatures());
 
 		utility::file::file_delete(database_filename);
 