c2c1ee0ca02fa37787dd9192bf6c5f52dc959ffc
Andrew Leaver-Fay <aleaverfay@gmail.com>
2012-08-28 17:41:49 +0000

    first commit to owningptr_to_sharedptr branch; changing conformation::Residue
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50840 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 041df83b4ddcdd03258ff048b7bafacd282b4d89

--- rosetta_source/src/core/conformation/Residue.fwd.hh
+++ rosetta_source/src/core/conformation/Residue.fwd.hh
@@ -16,8 +16,8 @@
 
 // Utility headers
 #include <utility/vector1.fwd.hh>
-#include <utility/pointer/access_ptr.fwd.hh>
-#include <utility/pointer/owning_ptr.fwd.hh>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
 
 // C++ headers
 
@@ -26,10 +26,10 @@ namespace conformation {
 
 class Residue;
 
-typedef  utility::pointer::access_ptr< Residue >  ResidueAP;
-typedef  utility::pointer::access_ptr< Residue const >  ResidueCAP;
-typedef  utility::pointer::owning_ptr< Residue >  ResidueOP;
-typedef  utility::pointer::owning_ptr< Residue const >  ResidueCOP;
+typedef  boost::weak_ptr< Residue >  ResidueAP;
+typedef  boost::weak_ptr< Residue const >  ResidueCAP;
+typedef  boost::shared_ptr< Residue >  ResidueOP;
+typedef  boost::shared_ptr< Residue const >  ResidueCOP;
 
 typedef  utility::vector1< ResidueOP >  ResidueOPs;
 typedef  utility::vector1< ResidueCOP >  ResidueCOPs;
