RosettaCommons/rosetta

commit 4d71fd534467fce9ba2b9b71da96bbcbeae25fcc

PyRosetta: added PyAsserts() to fix bug #0000093; no test changes expected
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50958 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: b04f90cf716b3b8527749e9d14ecf9878dd520fc

rosetta_source/src/core/chemical/ResidueType.cc
@@ -73,6 +73,7 @@
 // AUTO-REMOVED #include <utility/io/ozstream.hh>
 // AUTO-REMOVED #include <utility/io/izstream.hh>
 #include <basic/Tracer.hh>
+#include <utility/PyAssert.hh>
 
 // Options and Option key includes (needed for protonated versions of the residues - pH mode)
 #include <basic/options/option.hh>
@@ -269,50 +270,47 @@ ResidueType::set_upper_connect_atom( std::string const & atm_name )
 ///////////////////////////////////////////////////////////////////////////////
 //
 
-/// @brief Get the chemical atom_type for this atom by it indexs number in this residue
+/// @brief Get the chemical atom_type for this atom by it index number in this residue
 ///
-/// @detais If we want the atom_type index (integer), we get this from
+/// @details If we want the atom_type index (integer), we get this from
 /// the conformation::Atom itself, as seen in the code below
 AtomType const &
 ResidueType::atom_type( Size const atomno ) const
 {
+	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::atom_type( Size const atomno ): atomno is not in this ResidueType!");
 	return ( *atom_types_ )[ atom_type_index_[ atomno ] ];
-}
-
-orbitals::OrbitalType const &
-ResidueType::orbital_type(int const orbital_index)const
-{
-	return ( *orbital_types_ )[ orbital_type_index_[ orbital_index ] ];
-
-}
-
-core::Size
-ResidueType::orbital_type_index(Size const orb_index) const
-{
-	return orbital_type_index_[orb_index];
 }
 
 /// @brief Get the chemical atom_type index number for this atom by its index number in this residue
 int
 ResidueType::atom_type_index( Size const atmno ) const
 {
+	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::atom_type_index( Size const atmno ): atomno is not in this ResidueType!");
 	return atom_type_index_[ atmno ];
 }
 
-/// @brief Get the MM atom_type for this atom by its index number in this residue
-MMAtomType const &
-ResidueType::mm_atom_type( Size const atomno ) const
+/// @brief Get the atom name by index
+std::string const &
+ResidueType::atom_name( Size const index ) const
 {
-	return ( *mm_atom_types_ )[ mm_atom_type_index_[ atomno ] ];
+	PyAssert((index > 0) && (index <= natoms_), "ResidueType::atom_name( Size const index ): index is not in this ResidueType!");
+	return atom_name_[ index ];
 }
 
-
-
-/// @brief Get the MM atom_type index number for this atom by its index number in this residue
-int
-ResidueType::mm_atom_type_index( Size const atomno ) const
+/// @brief get index of an atom's base atom
+Size
+ResidueType::atom_base( Size const atomno ) const
 {
-	return mm_atom_type_index_[ atomno ];
+	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::atom_base( Size const atomno ): atomno is not in this ResidueType!");
+	return atom_base_[ atomno ];
+}
+
+/// @brief get index of an atom's second base atom
+Size
+ResidueType::abase2( Size const atomno ) const
+{
+	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::abase2( Size const atomno ): atomno is not in this ResidueType!");
+	return abase2_[ atomno ];
 }
 
 
@@ -406,6 +404,55 @@ ResidueType::add_atom(
 
 }
 
+/// @brief set atom type
+void
+ResidueType::set_atom_type(
+	std::string const & atom_name,
+	std::string const & atom_type_name
+)
+{
+	atom_type_index_[ atom_index( atom_name ) ] = atom_types_->atom_type_index( atom_type_name );
+}
+
+
+/// @brief set mm atom type
+void
+ResidueType::set_mm_atom_type(
+	std::string const & atom_name,
+	std::string const & mm_atom_type_name
+)
+{
+	mm_atom_type_index_[ atom_index( atom_name ) ] = mm_atom_types_->atom_type_index( mm_atom_type_name );
+	mm_atom_name_[ atom_index( atom_name ) ] = mm_atom_type_name;
+}
+
+/// @brief Get the MM atom_type for this atom by its index number in this residue
+MMAtomType const &
+ResidueType::mm_atom_type( Size const atomno ) const
+{
+	return ( *mm_atom_types_ )[ mm_atom_type_index_[ atomno ] ];
+}
+
+/// @brief Get the MM atom_type index number for this atom by its index number in this residue
+int
+ResidueType::mm_atom_type_index( Size const atomno ) const
+{
+	return mm_atom_type_index_[ atomno ];
+}
+
+
+orbitals::OrbitalType const &
+ResidueType::orbital_type(int const orbital_index)const
+{
+	return ( *orbital_types_ )[ orbital_type_index_[ orbital_index ] ];
+
+}
+
+core::Size
+ResidueType::orbital_type_index(Size const orb_index) const
+{
+	return orbital_type_index_[orb_index];
+}
 
 /// @note this does not set xyz coordiates for the added orbital but sets the index of the orbital and maps
 /// it to the type of orbital.
@@ -463,27 +510,6 @@ ResidueType::add_orbital(
 	//orbital_icoor_.push_back( orbitals::OrbitalICoor( 0.0, 0.0, 0.0, orbital_name, orbital_name, orbital_name, *this ) );
 }
 
-/// @brief set atom type
-void
-ResidueType::set_atom_type(
-	std::string const & atom_name,
-	std::string const & atom_type_name
-)
-{
-	atom_type_index_[ atom_index( atom_name ) ] = atom_types_->atom_type_index( atom_type_name );
-}
-
-/// @brief set mm atom type
-void
-ResidueType::set_mm_atom_type(
-	std::string const & atom_name,
-	std::string const & mm_atom_type_name
-)
-{
-	mm_atom_type_index_[ atom_index( atom_name ) ] = mm_atom_types_->atom_type_index( mm_atom_type_name );
-	mm_atom_name_[ atom_index( atom_name ) ] = mm_atom_type_name;
-}
-
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -525,11 +551,6 @@ ResidueType::add_bond(
 	//bondType_vector_[i1].push_back(BondType(i1,i2,SingleBond));
 
 }
-
-
-
-
-
 
 /// @details add a bond between atom1 and atom2 and add a BondType object referencing the bond using the specified bondName
 void ResidueType::add_bond(std::string const & atom_name1, std::string const & atom_name2, BondName bondLabel)
@@ -595,11 +616,6 @@ ResidueType::add_orbital_bond(
 	orbital_bonded_neighbor_[i1].push_back(i2);
 
 }
-
-
-
-
-
 
 /// @details add a cut_bond between atom1 and atom2, which disallows an atom-tree connection,
 ///            though the atoms are really bonded.

rosetta_source/src/core/chemical/ResidueType.hh
@@ -207,9 +207,6 @@ public:
 	}
 
 	/// @brief Get the chemical atom_type for this atom by it index number in this residue
-	///
-	/// @detais If we want the atom_type index (integer), we get this from
-	/// the conformation::Atom itself, as seen in the code below
 	AtomType const &
 	atom_type( Size const atomno ) const;
 
@@ -467,24 +464,15 @@ public:
 
 	/// @brief get index of an atom's base atom
 	Size
-	atom_base( Size const atomno ) const
-	{
-		return atom_base_[ atomno ];
-	}
+	atom_base( Size const atomno ) const;
 
 	/// @brief get index of an atom's second base atom
 	Size
-	abase2( Size const atomno ) const
-	{
-		return abase2_[ atomno ];
-	}
+	abase2( Size const atomno ) const;
 
 	/// @brief get atom name by index
 	std::string const &
-	atom_name( Size const index ) const
-	{
-		return atom_name_[ index ];
-	}
+	atom_name( Size const index ) const;
 
 
 	/// @brief Check if residue type has an atom by a given atomname
@@ -530,10 +518,10 @@ public:
 	atom_is_backbone( Size const atomno ) const
 	{
 		assert( finalized_ );
+		assert( atomno <= natoms_ );
  		return ( ( atomno <= n_backbone_heavyatoms_ ) ||
  						 ( atomno > nheavyatoms_ && atomno < first_sidechain_hydrogen_ ) );
 	}
-
 
 	/// @brief quick lookup: is the atom with the given index a hydrogen or not?
 	/// Atoms are sorted so that heavy atoms come first and hydrogen atoms come last.

rosetta_source/src/apps/pilot/labonte/tester.cc
@@ -17,19 +17,20 @@
 
 // includes
 #include <iostream>
-#include <algorithm>
+//#include <algorithm>
 
 #include <basic/Tracer.hh>
 #include <devel/init.hh>
 
 #include <core/pose/Pose.hh>
-#include <core/pose/PDBInfo.hh>
-#include <core/pose/util.hh>
+#include <core/conformation/Residue.hh>
+//#include <core/pose/PDBInfo.hh>
+//#include <core/pose/util.hh>
 #include <core/import_pose/import_pose.hh>
 //#include <core/kinematics/MoveMap.hh>
 
-#include <utility/vector1.hh>
-#include <utility/vector0.hh>
+//#include <utility/vector1.hh>
+//#include <utility/vector0.hh>
 
 int main(int argc, char *argv[])
 {
@@ -37,7 +38,7 @@ int main(int argc, char *argv[])
 	using namespace core;
 	using namespace import_pose;
 	using namespace pose;
-	using namespace utility;
+	//using namespace utility;
 	//using namespace kinematics;
 
 	// initialize core
@@ -47,14 +48,14 @@ int main(int argc, char *argv[])
 	Pose test_pose;
 
 	// import a test pose
-	pose_from_pdb(test_pose, "/home/labonte/Code/test.pdb");
+	pose_from_pdb(test_pose, "/home/labonte/Workspace/test_input/test.pdb");
 
 
 	// test new set_chi_true_range() method
-	//MoveMap mm = MoveMap();
-	//mm.set_chi_true_range(5, 10);
+	/*MoveMap mm = MoveMap();
+	mm.set_chi_true_range(5, 10);
 
-	//mm.show(20);
+	mm.show(20);*/
 
 
 	// test new vectorL methods
@@ -85,7 +86,6 @@ int main(int argc, char *argv[])
 	}
 	cout << "D was found at " << my_map.index_of('D') << endl;
 
-
 	my_other_map.push_back('E');
 	my_other_map.push_back('F');
 	my_other_map.push_back('G');
@@ -112,9 +112,9 @@ int main(int argc, char *argv[])
 
 	cout << "##########" << endl;*/
 
-	// test new update_pose_chains_from_pdb_chains() method
 
-	for (Size i = 50; i <= 100; ++i) {
+	// test new update_pose_chains_from_pdb_chains() method
+	/*for (Size i = 50; i <= 100; ++i) {
 		test_pose.pdb_info()->chain(i, 'X');
 	}
 
@@ -130,7 +130,16 @@ int main(int argc, char *argv[])
 	new_pose = *pose_collection[2];
 	new_pose.dump_pdb("/home/labonte/Code/split.pdb", "");
 
-	test_pose.dump_pdb("/home/labonte/Code/output.pdb", "");
+	test_pose.dump_pdb("/home/labonte/Code/output.pdb", "");*/
+
+
+	// test atom_name()
+
+	Pose::Residue res2 = test_pose.residue(2);
+
+	cout << res2.atom_name(0) << endl;
+	//cout << res2.atom_type(0) << endl;
+	cout << res2.atom_type_index(0) << endl;
 }
 
 

commit 6d355ad85315228b051d46128c439c5e88703deb

Fixing build broken by me in r50958; spelling error in what I cut-and-pasted
git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@50960 6189c7af-7406-0410-a2ce-f22f51069c23

Old repository SHA1: 695828893c9be1178790a4c2f78709dcb9c151f6

rosetta_source/src/core/chemical/ResidueType.cc
@@ -283,10 +283,10 @@ ResidueType::atom_type( Size const atomno ) const
 
 /// @brief Get the chemical atom_type index number for this atom by its index number in this residue
 int
-ResidueType::atom_type_index( Size const atmno ) const
+ResidueType::atom_type_index( Size const atomno ) const
 {
-	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::atom_type_index( Size const atmno ): atomno is not in this ResidueType!");
-	return atom_type_index_[ atmno ];
+	PyAssert((atomno > 0) && (atomno <= natoms_), "ResidueType::atom_type_index( Size const atomno ): atomno is not in this ResidueType!");
+	return atom_type_index_[ atomno ];
 }
 
 /// @brief Get the atom name by index

rosetta_source/src/core/chemical/ResidueType.hh
@@ -212,7 +212,7 @@ public:
 
 	/// @brief Get the chemical atom_type index number for this atom by its index number in this residue
 	int
-	atom_type_index( Size const atmno ) const;
+	atom_type_index( Size const atomno ) const;
 
 	/// @brief number of atoms
 	Size