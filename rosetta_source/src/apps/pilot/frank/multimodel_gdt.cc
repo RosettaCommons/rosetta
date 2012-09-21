addd10e040da29b2d230caa9430ebe5ae9aaaf55
Frank Dimaio <dimaio@u.washington.edu>
2012-09-21 01:32:30 +0000

    updates to a pilot app
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@51281 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: bf3285043a44224bedd09a66b93d8d1d6133c2d4

--- rosetta_source/src/apps/pilot/frank/multimodel_gdt.cc
+++ rosetta_source/src/apps/pilot/frank/multimodel_gdt.cc
@@ -23,20 +23,32 @@
 
 void
 superimpose_tmalign( core::pose::Pose const & ref_pose, core::pose::Pose & pose) {
-	protocols::comparative_modeling::hybridize::TMalign tm_align;
-	int reval = tm_align.apply(pose, ref_pose);
-	if (reval != 0) {
-		std::cerr << "ERROR!" << std::endl;
-		return;
-	}
+	//protocols::comparative_modeling::hybridize::TMalign tm_align;
+	//int reval = tm_align.apply(pose, ref_pose);
+	//if (reval != 0) {
+	//	std::cerr << "ERROR!" << std::endl;
+	//	return;
+	//}
 
 	core::id::AtomID_Map< core::id::AtomID > atom_map;
 	core::pose::initialize_atomid_map( atom_map, pose, core::id::BOGUS_ATOM_ID );
-	std::string seq_pose, seq_ref, aligned;
+	//std::string seq_pose, seq_ref, aligned;
 	core::Size n_mapped_residues = 0;
+	//tm_align.alignment2AtomMap(pose, ref_pose, n_mapped_residues, atom_map);
+	//tm_align.alignment2strings(seq_pose, seq_ref, aligned);
 
-	tm_align.alignment2AtomMap(pose, ref_pose, n_mapped_residues, atom_map);
-	tm_align.alignment2strings(seq_pose, seq_ref, aligned);
+	core::Size nres = ref_pose.total_residue();
+	for (int i=1; i<=nres; ++i) {
+		core::Size n = ref_pose.pdb_info()->number(i);
+		char chainID = pose.pdb_info()->chain(1);
+		core::Size n_idx = pose.pdb_info()->pdb2pose( chainID, n );
+		if (n_idx != 0) {
+			core::id::AtomID const id1( pose.residue_type(n_idx).atom_index("CA"), n_idx );
+			core::id::AtomID const id2( ref_pose.residue_type(i).atom_index("CA"), i );
+			atom_map[ id1 ] = id2;
+			n_mapped_residues++;
+		}
+	}
 
 	if (n_mapped_residues >= 6) {
 		utility::vector1< core::Real > aln_cutoffs;
@@ -49,6 +61,7 @@ superimpose_tmalign( core::pose::Pose const & ref_pose, core::pose::Pose & pose)
 		core::Real min_coverage = 0.2;
 		protocols::comparative_modeling::hybridize::partial_align(pose, ref_pose, atom_map, true, aln_cutoffs, min_coverage); // iterate_convergence = true
 	}
+	pose.dump_pdb("aligned.pdb");
 }
 
 void
@@ -65,9 +78,13 @@ multimodel_gdt( core::pose::Pose const & ref_pose, utility::vector1<core::pose::
 			core::Size n = ref_pose.pdb_info()->number(i);
 			numeric::xyzVector<core::Real> x_n = ref_pose.residue(i).xyz(2);
 
-			core::Size n_idx = models[j].pdb_info()->pdb2pose( 'A', n );
-			if (n_idx == 0) 
-				n_idx = models[j].pdb_info()->pdb2pose( ' ', n );
+			//core::Size n_idx = models[j].pdb_info()->pdb2pose( 'A', n );
+			//if (n_idx == 0) 
+			//	n_idx = models[j].pdb_info()->pdb2pose( ' ', n );
+
+			// assumes single chain
+			char chainID = models[j].pdb_info()->chain(1);
+			core::Size n_idx = models[j].pdb_info()->pdb2pose( chainID, n );
 
 			if (n_idx != 0) {
 				numeric::xyzVector<core::Real> y_n = models[j].residue(n_idx).xyz(2);
