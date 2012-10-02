77061c80726790e2134b2e477a488bfcc315a859
TJ Brunette <tjbrunette@gmail.com>
2012-10-02 18:19:24 +0000

    added a method to evaluate repeat pose for remodel and added external requirements such as boost serialization which were required for Ken's Lua
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@51451 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 9a5dc4f9de071ec7e91a0ec894bb7ae37faee924

---
+++ rosetta_source/src/apps/pilot/brunette/evalRepeats.cc
@@ -0,0 +1,229 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet;
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.AlignmentCluster
+
+/// @file   apps/pilot/brunette/evalRepeats
+///
+/// @brief  analyzes helix oritentation in repeat proteins
+
+/// @usage:
+
+/// @author TJ Brunette
+
+
+// Utility Headers
+#include <basic/Tracer.hh>
+#include <numeric/conversions.hh>
+#include <numeric/trig.functions.hh>
+#include <numeric/xyz.functions.hh>
+#include <numeric/xyzVector.hh>
+
+// Core Headers
+#include <core/chemical/util.hh>
+#include <core/chemical/ResidueTypeSet.fwd.hh>
+#include <core/conformation/Residue.hh>
+
+#include <core/id/NamedAtomID.hh>
+
+#include <core/import_pose/import_pose.hh>
+#include <core/import_pose/pose_stream/util.hh>
+#include <core/import_pose/pose_stream/MetaPoseInputStream.hh>
+
+#include <core/pose/Pose.hh>
+#include <core/pose/util.hh>
+
+#include <core/scoring/ScoreType.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <core/scoring/packing/compute_holes_score.hh>
+#include <core/scoring/packing/HolesParams.hh>
+#include <core/scoring/packstat/compute_sasa.hh>
+
+#include <core/types.hh>
+
+#include <core/util/ABEGOManager.hh>
+
+#include <devel/init.hh>
+
+#include <utility/vector1.hh>
+
+//protocols
+#include <protocols/jumping/util.hh>
+#include <protocols/loops/Loop.hh>
+#include <protocols/loops/Loops.hh>
+
+//basic & utility
+#include <basic/database/open.hh>
+#include <basic/options/option.hh>
+#include <basic/options/keys/out.OptionKeys.gen.hh>
+#include <basic/options/keys/holes.OptionKeys.gen.hh>
+#include <utility/io/ozstream.hh>
+#include <iostream>
+#include <ObjexxFCL/format.hh>
+
+using namespace ObjexxFCL::fmt;
+using utility::vector1;
+using core::Size;
+using core::Real;
+
+basic::Tracer tr( "evalRepeats" );
+
+void avg_ca_position(
+	const core::pose::Pose& pose,
+	const protocols::loops::Loop& region,
+	numeric::xyzVector<double>* point
+) {
+	assert(point);
+
+	point->zero();
+	for (unsigned i = region.start(); i <= region.stop(); ++i) {
+		(*point) += pose.xyz(core::id::NamedAtomID("CA", i));
+	}
+
+	(*point) /= region.length();
+}
+
+void get_helices(core::pose::Pose& pose, protocols::loops::Loops* helices){
+	using protocols::loops::Loop;
+	using numeric::xyzVector;
+	protocols::jumping::assign_ss_dssp( pose );
+	char lastSecStruct = pose.secstruct(1);
+	Size startHelix = 0;
+	Size endHelix = 0;
+	if(pose.secstruct(1) == 'H')
+		startHelix = 1;
+	for ( core::Size ii = 2; ii <= pose.total_residue(); ++ii ) {
+		if(pose.secstruct(ii) == 'H' && lastSecStruct != 'H')
+			startHelix = ii;
+		if(pose.secstruct(ii) != 'H' && lastSecStruct == 'H'){
+			endHelix = ii-1;
+			helices->add_loop(Loop(startHelix,endHelix));
+		}
+		lastSecStruct = pose.secstruct(ii);
+	}
+}
+
+Real get_distance(const core::pose::Pose& pose, const protocols::loops::Loop helix1, const protocols::loops::Loop helix2){
+	using protocols::loops::Loop;
+	using numeric::xyzVector;
+	//std::cout <<"helix length" << helix1.length() << "," << helix2.length() << std::endl;
+	if((helix1.length() < 3) || (helix2.length() < 3)){
+		tr.Warning << "distance invoked with helix shorter than 3 residues" << std::endl;
+		return(-1);
+	}
+	xyzVector<double> a, b;
+	avg_ca_position(pose, Loop(helix1.stop()-2, helix1.stop()),&a);
+	avg_ca_position(pose, Loop(helix2.start(), helix2.start()+2),&b);
+	return(a.distance(b));
+}
+Real get_distance_endpoint(const core::pose::Pose& pose, const protocols::loops::Loop helix1, const protocols::loops::Loop helix2){
+	using protocols::loops::Loop;
+	using numeric::xyzVector;
+	//std::cout <<"helix length" << helix1.length() << "," << helix2.length() << std::endl;
+	if((helix1.length() < 3) || (helix2.length() < 3)){
+		tr.Warning << "distance invoked with helix shorter than 3 residues" << std::endl;
+		return(-1);
+	}
+	xyzVector<double> a, b;
+	avg_ca_position(pose, Loop(helix2.stop()-2, helix2.stop()),&a);
+	avg_ca_position(pose, Loop(helix1.start(), helix1.start()+2),&b);
+	return(a.distance(b));
+}
+
+Real get_holes_score(const core::pose::Pose& pose){
+	core::scoring::packing::HolesParams hp_resl,hp_dec,hp_dec15;
+	hp_dec15.read_data_file(basic::database::full_name("scoring/rosettaholes/decoy15.params"));
+	Real  holes_result = core::scoring::packing::compute_dec15_score(pose);
+	return holes_result;
+}
+
+void get_angles(const core::pose::Pose& pose, const protocols::loops::Loop helix1, const protocols::loops::Loop helix2, Real& theta, Real& sigma, Real& phi){
+	using protocols::loops::Loop;
+	using numeric::xyzVector;
+	using numeric::conversions::degrees;
+	if((helix1.length() < 3) || (helix2.length() < 3)){
+		tr.Warning << "distance invoked with helix shorter than 3 residues" << std::endl;
+	}
+	xyzVector<double> h1_start, h1_stop, h2_start, h2_stop;
+	avg_ca_position(pose, Loop(helix1.start(), helix1.start()+2),&h1_start); //you want the two helices to start at the same turn.
+	avg_ca_position(pose, Loop(helix1.stop()-2, helix1.stop()),&h1_stop);
+	avg_ca_position(pose, Loop(helix2.start(), helix2.start()+2),&h2_start);
+	avg_ca_position(pose, Loop(helix2.stop()-2, helix2.stop()),&h2_stop);
+	numeric::xyzVector<Real> v1(h1_stop - h1_start);
+	numeric::xyzVector<Real> v2(h2_stop - h2_start);
+	numeric::xyzVector<Real> u1 = v1.normalize();
+	numeric::xyzVector<Real> u2 = v2.normalize();
+	numeric::xyzVector<Real> r = h1_start-h2_start;
+	numeric::xyzVector<Real> ur = r.normalize();
+	theta = degrees(numeric::arccos(u1.dot_product(u2)));
+	sigma = degrees(numeric::arccos(u1.dot_product(ur)));
+	Real phi_top = u2.dot_product((u1.cross_product(ur)));
+	Real phi_bottom = u2.dot_product(u1.cross_product(u1.cross_product(ur)));
+	phi = degrees(std::atan(phi_top/phi_bottom));
+}
+
+Real res_type_score(const core::pose::Pose& pose){
+  Real score = 0;
+	for ( core::Size ii = 1; ii <= pose.total_residue(); ++ii ) {
+		if((pose.residue(ii).name3() == "VAL") || (pose.residue(ii).name3() == "ILE") || (pose.residue(ii).name3() == "LEU"))
+			score++;
+		else
+			if((pose.residue(ii).name3() == "ALA") || (pose.residue(ii).name3() == "TRP") || (pose.residue(ii).name3() == "MET"))
+				score=score-1;
+	}
+	std::cout << "score: " << score << std::endl;
+	return score;
+}
+
+
+int main( int argc, char * argv [] ) {
+	using namespace core::chemical;
+	using namespace core::import_pose::pose_stream;
+	using core::import_pose::pose_from_pdb;
+	using protocols::loops::Loops;
+	using namespace core::scoring;
+	using namespace basic::options;
+	using namespace basic::options::OptionKeys;
+	devel::init(argc, argv);
+	std::string out_nametag = option[ out::file::o ];
+	std::string out_file_name_str( out_nametag + ".scores");
+	utility::io::ozstream output(out_file_name_str);
+	ResidueTypeSetCAP rsd_set = rsd_set_from_cmd_line();
+	//create vector of input poses.
+	MetaPoseInputStream input = streams_from_cmd_line();
+	vector1<core::pose::PoseOP> poses;
+	output << "score" <<" " << "holes" << " " << "distance" << " " << "distance_endpoint" <<" " << "theta" << " " << "sigma" << " " << "phi" << " " << "tag" <<" " <<"resTypeScore" << " " << "abego" << std::endl;
+	while(input.has_another_pose()){
+		core::pose::PoseOP input_poseOP;
+		input_poseOP = new core::pose::Pose();
+		input.fill_pose(*input_poseOP,*rsd_set);
+		std::string tag = core::pose::tag_from_pose(*input_poseOP);
+		Loops helices;
+		get_helices(*input_poseOP,&helices);
+		if(helices.num_loop() > 1)
+			 if((helices[1].length() > 3)&&(helices[2].length() > 3)){//assumes repeat proteins with > 1 loops
+				 Real distance = get_distance(*input_poseOP,helices[1],helices[2]);
+				 Real distance_endpoint = get_distance_endpoint(*input_poseOP,helices[1],helices[2]);
+				 Real theta;
+				 Real sigma;
+				 Real phi;
+				 get_angles(*input_poseOP,helices[1],helices[2],theta,sigma,phi);
+				 Real holesScore = get_holes_score(*input_poseOP);
+				 core::scoring::ScoreFunctionOP scorefxn( ScoreFunctionFactory::create_score_function(STANDARD_WTS, SCORE12_PATCH) );
+				 Real fa_score = scorefxn->score(*input_poseOP);
+				 Real resTypeScore = res_type_score(*input_poseOP);
+				 utility::vector1< std::string >  abego_vector = core::util::get_abego(*input_poseOP,1);
+				 output << F(8,3,fa_score) << " " <<F(8,3,holesScore) <<" "<< F(8,3,distance) << " " << F(8,3,distance_endpoint) <<" " << F(8,3,theta) << " "<< F(8,3,sigma) << " " << F(8,3,phi) << " " << I(6,tag) <<" " << I(4,resTypeScore) << " ";
+				 for (int ii=1; ii<25; ++ii){
+					 output << abego_vector[ii];
+				 }
+				 output << std::endl;
+			 }
+	}
+}
+
