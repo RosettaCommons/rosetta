d19d73d5427fbcb9eaa54cc71faa682fd61891a7
Rhiju Das <rhiju@stanford.edu>
2012-10-02 20:23:03 +0000

    enable manipulation of hack_elec min_dis, max_dis, and no_dis_dep_die parameters from lines in weights files.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@51455 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 961ef1b78f9b32b5cd4582dab349fa7b9a44479e

--- rosetta_source/src/core/scoring/ScoreFunction.cc
+++ rosetta_source/src/core/scoring/ScoreFunction.cc
@@ -286,6 +286,16 @@ ScoreFunction::_add_weights_from_file( std::string const & filename, bool patch/
 			std::string type;
 			l >> type;
 			energy_method_options_->unfolded_energies_type( type );
+		} else if ( tag == "HACK_ELEC_MIN_DIS" ) {
+			Real value;
+			l >> value;
+			energy_method_options_->hackelec_min_dis( value );
+		} else if ( tag == "HACK_ELEC_MAX_DIS" ) {
+			Real value;
+			l >> value;
+			energy_method_options_->hackelec_max_dis( value );
+		} else if ( tag == "HACK_ELEC_NO_DIS_DEP_DIE" ) {
+			energy_method_options_->hackelec_no_dis_dep_die( true );
 		} else {
 
 	// //////////// Regular Weights ///////////////////////
