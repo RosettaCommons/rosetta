8e0ffd14398f050eae3501e8a176ae3f369464ee
Sergey Lyskov <sergey.lyskov@jhu.edu>
2012-10-04 23:46:45 +0000

    Updating SetPyRosettaEnvironment.sh script again.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@51528 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 6823cd0713330ea3a295710cca0c94e3c9f4ae96

--- rosetta_source/src/python/bindings/SetPyRosettaEnvironment.sh
+++ rosetta_source/src/python/bindings/SetPyRosettaEnvironment.sh
@@ -3,17 +3,26 @@
 # This script intended to set Pyrosetta environment variables so user can execute 'import rosetta' from
 # any file system location. Use 'source SetPyRosettaEnvironment.sh' before starting to work with PyRosetta.
 
-
-if [[ "$0" == "bash"  ||  "$0" == "-bash" ]]; then
-    #echo "bash like shell..."
-    OLD_PATH=`pwd`
-    PYROSETTA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-else
+if [[ "${BASH_SOURCE[0]}" == "" ]]; then
     #echo "zsh like shell..."
     OLD_PATH=`pwd`
     PYROSETTA="$( cd "$( dirname "$0" )" && pwd )"
+else
+    #echo "bash like shell..."
+    OLD_PATH=`pwd`
+    PYROSETTA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 fi
+
+
+#if [[ "$0" == "bash"  ||  "$0" == "-bash" ]]; then
+#    #echo "bash like shell..."
+#    OLD_PATH=`pwd`
+#    PYROSETTA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#else
+#    #echo "zsh like shell..."
+#    OLD_PATH=`pwd`
+#    PYROSETTA="$( cd "$( dirname "$0" )" && pwd )"
+#fi
 
 cd $PYROSETTA
 
