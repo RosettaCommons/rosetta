92b8d7e6885051b47326bbc5c9963a408d620302
Daisuke Kuroda <dkuroda1981@gmail.com>
2012-11-24 23:01:45 +0000

    Forgot to do "svn add".
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@52315 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 5881546fd4c8e41137303f2ba90ad9968a197e40

---
+++ rosetta_source/src/protocols/docking/DockingLowResEnsemble.cc
@@ -0,0 +1,130 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file DockingLowResEnsemble
+/// @brief protocols that are specific to low resolution ensemble docking
+/// @detailed
+/// @author Daisuke Kuroda
+
+#include <protocols/docking/DockingLowRes.hh>
+#include <protocols/docking/DockingLowResEnsemble.hh>
+#include <protocols/docking/DockingEnsemble.hh>
+
+// Rosetta Headers
+#include <core/kinematics/MoveMap.hh>
+
+// AUTO-REMOVED #include <basic/options/option.hh>
+// AUTO-REMOVED #include <basic/options/keys/OptionKeys.hh>
+
+// AUTO-REMOVED #include <core/kinematics/FoldTree.hh>
+
+#include <core/pose/Pose.hh>
+
+#include <core/scoring/ScoreFunction.hh>
+
+#include <protocols/docking/ConformerSwitchMover.hh>
+#include <protocols/moves/MonteCarlo.hh>
+#include <protocols/moves/Mover.hh>
+#include <protocols/rigid/RigidBodyMover.hh>
+#include <protocols/moves/TrialMover.hh>
+#include <protocols/moves/RepeatMover.hh>
+#include <protocols/moves/MoverContainer.hh>
+
+// Utility Headers
+#include <utility/tools/make_vector1.hh>
+
+// ObjexxFCL Headers
+#include <ObjexxFCL/string.functions.hh>
+#include <ObjexxFCL/format.hh>
+
+// C++ Headers
+#include <string>
+
+//Utility Headers
+// AUTO-REMOVED #include <numeric/conversions.hh>
+
+#include <numeric/trig.functions.hh>
+#include <numeric/xyzMatrix.fwd.hh>
+
+#include <basic/Tracer.hh>
+
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <utility/vector0.hh>
+#include <utility/vector1.hh>
+
+using basic::T;
+using basic::Error;
+using basic::Warning;
+
+static basic::Tracer TR("protocols.docking.DockingLowResEnsemble");
+
+using namespace core;
+
+namespace protocols {
+namespace docking {
+
+// default constructor
+DockingLowResEnsemble::DockingLowResEnsemble() : DockingLowRes()
+{
+	type( "DockingLowResEnsemble" );
+}
+    
+// constructor with arguments    
+DockingLowResEnsemble::DockingLowResEnsemble(
+    core::scoring::ScoreFunctionCOP scorefxn,
+    DockJumps const movable_jumps
+) : DockingLowRes( scorefxn, movable_jumps )
+{
+    type( "DockingLowResEnsemble" );
+}
+
+//destructor
+DockingLowResEnsemble::~DockingLowResEnsemble() {}
+
+    
+protocols::moves::MoverOP DockingLowResEnsemble::clone() const {
+	return new DockingLowResEnsemble(*this);
+}
+
+ 
+void DockingLowResEnsemble::set_ensemble1( DockingEnsembleOP ensemble1 )
+{
+    ensemble1_mover_ = new protocols::docking::ConformerSwitchMover( ensemble1 );
+}
+
+void DockingLowResEnsemble::set_ensemble2( DockingEnsembleOP ensemble2 )
+{
+    ensemble2_mover_ = new protocols::docking::ConformerSwitchMover( ensemble2 );
+}
+
+void DockingLowResEnsemble::finalize_setup( core::pose::Pose & pose){
+	using namespace moves;
+
+    DockingLowRes::finalize_setup( pose );
+    
+    // add if ens_mover1 ...
+    // inner_move_sequence
+	docking_lowres_protocol_->add_mover( ensemble1_mover_ );
+	docking_lowres_protocol_->add_mover( ensemble2_mover_ );        
+}
+
+void DockingLowResEnsemble::show( std::ostream & out ) const
+{
+    DockingLowRes::show( out );
+    
+    using namespace ObjexxFCL::fmt;
+    std::string line_marker = "///";
+    out << line_marker << " Ensemble 1: " << ( ( ensemble1_mover_ ) ? ( "on" ) : ( "off " ) );
+    out << space( 59 ) << line_marker << std::endl;
+    out << line_marker << " Ensemble 2: " << ( ( ensemble2_mover_ ) ? ( "on" ) : ( "off " ) );
+    out << space( 59 ) << line_marker << std::endl;
+}
+    
+} // namespace docking
+} // namespace protocols
---
+++ rosetta_source/src/protocols/docking/DockingLowResEnsemble.hh
@@ -0,0 +1,88 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file protocols/docking/DockinLowResEnsemble
+/// @brief low resolution mode for ensemble docking
+/// @detailed
+/// @author Daisuke Kuroda
+///
+
+
+#ifndef INCLUDED_protocols_docking_DockingLowRes_Ensemble_hh
+#define INCLUDED_protocols_docking_DockingLowRes_Ensemble_hh
+
+#include <protocols/docking/types.hh>
+#include <protocols/docking/DockingLowRes.fwd.hh>
+#include <protocols/docking/DockingLowResEnsemble.fwd.hh>
+#include <protocols/docking/DockingEnsemble.fwd.hh>
+
+// Package headers
+#include <core/kinematics/MoveMap.fwd.hh>
+#include <core/pose/Pose.fwd.hh>
+#include <core/scoring/ScoreFunction.fwd.hh>
+
+#include <protocols/docking/ConformerSwitchMover.fwd.hh>
+#include <protocols/moves/MoverContainer.fwd.hh>
+#include <protocols/moves/MonteCarlo.fwd.hh>
+#include <protocols/moves/Mover.hh>
+#include <protocols/rigid/RigidBodyMover.fwd.hh>
+// AUTO-REMOVED #include <protocols/filters/Filter.hh>
+// AUTO-REMOVED #include <protocols/simple_filters/ScoreCutoffFilter.hh>
+
+#include <string>
+
+// option key includes
+
+// AUTO-REMOVED #include <basic/options/keys/docking.OptionKeys.gen.hh>
+
+#include <utility/vector1.hh>
+#include <iostream>
+
+namespace protocols {
+namespace docking {
+
+class DockingLowResEnsemble : public DockingLowRes
+{
+	//typedef core::Real Real;
+public:
+
+	/// @brief Default constructor
+	DockingLowResEnsemble();
+
+	// destructor
+	~DockingLowResEnsemble();
+
+	/// @brief Constructor with two arguments.  The first is scorefunction to be used for docking, the second is the
+	///		DockJumps.
+	DockingLowResEnsemble(
+        core::scoring::ScoreFunctionCOP scorefxn,
+        DockJumps const movable_jumps
+    );
+	
+    virtual protocols::moves::MoverOP clone() const;
+    
+    virtual void show( std::ostream & out=std::cout ) const;
+    
+	// option setters
+	void set_ensemble1( DockingEnsembleOP ensemble1 );
+	void set_ensemble2( DockingEnsembleOP ensemble2 );
+    
+protected:
+	/// @brief Performs the portion of setup of non-primitive members that requires a pose - called on apply
+	virtual void finalize_setup( core::pose::Pose & pose); // Comment out by DK
+
+private:
+    protocols::docking::ConformerSwitchMoverOP ensemble1_mover_;
+	protocols::docking::ConformerSwitchMoverOP ensemble2_mover_;
+};
+
+} // docking
+} // protocols
+
+#endif
