a701a096392010c520b420557be73bc2d5587461
Gideon Lapidoth <glapidoth@gmail.com>
2012-12-02 12:08:13 +0000

    Added a mover that splits a pose fold tree into two by distance criteria
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@52432 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 044b57804c72b5be1a25fbfef35725b228074766

---
+++ rosetta_source/src/protocols/simple_moves/CutChainMover.cc
@@ -0,0 +1,196 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet;
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   ForceDisulfidesMover.cc
+///
+/// @brief
+/// @author Sarel Fleishman
+
+// unit headers
+#include <protocols/simple_moves/CutChainMover.hh>
+#include <protocols/simple_moves/CutChainMoverCreator.hh>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+// type headers
+#include <core/types.hh>
+#include <core/id/types.hh>
+
+// project headers
+#include <protocols/moves/Mover.hh>
+#include <core/pose/util.hh>
+#include <core/conformation/Conformation.hh>
+#include <core/pose/Pose.hh>
+#include <core/pose/selection.hh>
+
+// package headers
+#include <core/pose/PDBInfo.hh>
+#include <core/pose/carbohydrates/util.hh>
+#include <core/pose/datacache/CacheableDataType.hh>
+#include <core/pose/datacache/CacheableObserverType.hh>
+#include <core/pose/datacache/ObserverCache.hh>
+
+// Project headers
+#include <core/chemical/ResidueType.hh>
+#include <core/conformation/Residue.hh>
+#include <core/conformation/Conformation.hh>
+
+#include <core/id/TorsionID.hh>
+#include <core/id/types.hh>
+#include <core/scoring/Energies.hh>
+
+// utility header
+#include <basic/Tracer.hh>
+#include <utility/vector1.hh>
+#include <utility/exit.hh>
+#include <utility/tag/Tag.hh>
+#include <utility/string_util.hh>
+#include <protocols/rosetta_scripts/util.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <core/pack/task/operation/TaskOperations.hh>
+#include <protocols/toolbox/task_operations/DesignAroundOperation.hh>
+#include <core/pack/task/PackerTask.hh>
+#include <protocols/simple_moves/PackRotamersMover.hh>
+#include <core/scoring/ScoreFunction.hh>
+
+namespace protocols {
+namespace simple_moves {
+
+static basic::Tracer TR("protocols.simple_moves.CutChainMover");
+
+std::string
+CutChainMoverCreator::keyname() const
+{
+	return CutChainMoverCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+CutChainMoverCreator::create_mover() const {
+	return new CutChainMover;
+}
+
+std::string
+CutChainMoverCreator::mover_name()
+{
+	return "CutChain";
+}
+
+//Default Constructor
+CutChainMover::CutChainMover() :
+	protocols::moves::Mover("CutChain"),
+	bond_length_(4.0), //define covalent bond length cut-off
+	chain_id_(1)//define default main chain
+{
+}
+
+
+CutChainMover::~CutChainMover() {}
+
+protocols::moves::MoverOP
+CutChainMover::clone() const
+{
+	return new CutChainMover( *this );
+}
+
+protocols::moves::MoverOP
+CutChainMover::fresh_instance() const
+{
+	return new CutChainMover();
+}
+
+//getters
+std::string
+CutChainMover::get_name() const {
+	return "CutChain";
+}
+
+core::Real
+CutChainMover::bond() const {
+	return bond_length_;
+}
+
+core::Size
+CutChainMover::chainId() const {
+	return chain_id_;
+}
+
+//setters
+void
+CutChainMover::bond(core::Real length){
+	bond_length_ = length;
+}
+void
+CutChainMover::chainId(core::Size ID){
+	chain_id_ = ID;
+}
+void
+CutChainMover::parse_my_tag( utility::tag::TagPtr const tag, protocols::moves::DataMap &data, protocols::filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const & pose )
+{
+  bond( tag->getOption< core::Real >( "bond_length", 4.0 ) );
+  chainId( tag->getOption< core::Size >( "chain_id", 1 ) );
+	TR<<" bond_length: "<<bond();
+	TR<<"Chain id: "<<chainId();
+	TR<<std::endl;
+}
+
+void CutChainMover::apply( core::pose::Pose & pose )
+{
+		create_subpose(pose);
+		foldTree(pose);
+		
+}
+
+core::Size
+CutChainMover::chain_cut( core::pose::Pose & pose) 
+{
+	core::Size cut_pos = -1;
+	for( core::Size resj = pose.conformation().chain_begin( chain_id_ ); resj <= pose.conformation().chain_end( chain_id_ )-1; ++resj ){
+		core::Real const distance = pose.residue( resj+1 ).xyz( "N" ).distance(pose.residue( resj ).xyz( "C" ));
+			TR<<"distance is: "<<distance<<std::endl;
+			TR<<"residue name is : "<<pose.residue(resj).name1()<<std::endl;
+		if( distance > bond()){
+			cut_pos = resj;
+			TR<<"Found cut at: "<<resj<<std::endl;
+			break;
+		}
+	}
+	return( cut_pos );//cut_pos is the amino acid position BEFORE the cut
+}
+
+void
+CutChainMover::create_subpose(core::pose::Pose & pose)
+{
+	core::pose::Pose copy_pose( pose );
+	pose.clear();
+
+	//add to pose only residues from main chain
+	for (core::Size resj = copy_pose.conformation().chain_begin( chain_id_); resj <= copy_pose.conformation().chain_end( chain_id_); ++resj) {
+		core::conformation::Residue const & rsd( copy_pose.residue( resj) );
+		pose.append_residue_by_bond( rsd );
+	}
+
+}
+
+void
+CutChainMover::foldTree (core::pose::Pose & pose){
+core::Size const s1 = chain_cut(pose);
+core::kinematics::FoldTree ft;
+	ft.clear();
+	ft.add_edge( 1, s1, -1 );
+	ft.add_edge( s1, s1+1, 1 );	
+	ft.add_edge( s1+1, pose.conformation().chain_end( chainId()), -1 );
+TR<<"old foldtree: "<<pose.fold_tree()<<std::endl;
+pose.fold_tree(ft);
+TR<<"new_foldtree: "<<pose.fold_tree()<<std::endl;
+pose.conformation().detect_disulfides();
+}
+
+
+} // moves
+} // protocols
---
+++ rosetta_source/src/protocols/simple_moves/CutChainMover.hh
@@ -0,0 +1,70 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet;
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   ForceDisulfidesMover.hh
+/// @brief
+/// @author Sarel Fleishman
+
+#ifndef INCLUDED_protocols_simple_moves_CutChainMover_hh
+#define INCLUDED_protocols_simple_moves_CutChainMover_hh
+
+// Unit headers
+#include <protocols/simple_moves/CutChainMover.fwd.hh>
+#include <protocols/moves/Mover.hh>
+#include <core/pose/Pose.fwd.hh>
+
+#include <utility/vector1.hh>
+
+
+namespace protocols {
+namespace simple_moves {
+
+
+/// @brief simple mover that sperates Fv from Fl into two seperate chains
+class CutChainMover : public moves::Mover {
+
+public:
+	CutChainMover();
+	~CutChainMover();
+
+	virtual void apply( core::pose::Pose & pose );
+	virtual std::string get_name() const;
+
+	void parse_my_tag( utility::tag::TagPtr const tag, protocols::moves::DataMap & data_map, protocols::filters::Filters_map const &, protocols::moves::Movers_map const &, core::pose::Pose const & );
+
+	protocols::moves::MoverOP clone() const;
+	protocols::moves::MoverOP fresh_instance() const;
+
+
+core::Size
+chain_cut( core::pose::Pose & pose);
+
+void
+create_subpose(core::pose::Pose & pose );
+
+void
+foldTree (core::pose::Pose & pose);
+
+core::Real bond() const;
+core::Size chainId() const;
+
+void bond(core::Real);
+void chainId(core::Size);
+
+private:
+	core::Real bond_length_;
+	core::Size chain_id_;
+
+};
+
+
+} // moves
+} // protocols
+
+#endif //INCLUDED_protocols_simple_moves_CutChainMover_HH
