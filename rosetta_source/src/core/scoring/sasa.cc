87d8f945d22d0701591b775f2f6e0c3d217085c8
Sergey Lyskov <sergey.lyskov@jhu.edu>
2012-12-11 20:59:31 +0000

    Fixing PyRosetta build.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@52615 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 47cf601f43846d6a540af6683b9c1c210cb973df

--- rosetta_source/src/core/scoring/sasa.cc
+++ rosetta_source/src/core/scoring/sasa.cc
@@ -958,7 +958,6 @@ calc_per_res_hydrophobic_sasa( pose::Pose const & pose,
 	return total_hydrophobic_sasa;
 }
 
-#ifdef FILE_DEBUG
 ///
 /// @begin sasa.cc::print_dot_bit_string
 ///
@@ -985,7 +984,7 @@ void print_dot_bit_string( utility::vector1< ObjexxFCL::ubyte > & values ) {
 	std::cout << std::endl;
 //#endif
 }
-#endif
+
 
 } // namespace scoring
 } // namespace core
