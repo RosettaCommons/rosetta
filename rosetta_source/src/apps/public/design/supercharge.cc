409ff70e2495d75d8cd8fc82a1ce898ac3c72c29
Bryan Der <bder@email.unc.edu>
2013-01-23 17:20:06 +0000

    Moving supercharge from apps/public to apps/public/design.  Reorganizing the demo and integration test.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@53296 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 99735a7e1293a68a038d1ea6ed5ed472dcd77982

--- rosetta_source/src/apps/public/design/supercharge.cc
+++ rosetta_source/src/apps/public/design/supercharge.cc
@@ -98,7 +98,7 @@
 using basic::Error;
 using basic::Warning;
 using basic::T;
-static basic::Tracer TR("apps.pilot.bder.supercharge");
+static basic::Tracer TR("apps.public.design.supercharge");
 
 using namespace core;
 typedef core::pose::Pose Pose;
