a7d2d91f8dda42548a39e9ccabce7d930cc9584c
William Sheffler <willsheffler@gmail.com>
2013-02-14 22:26:04 +0000

    added utilities for uniform sampling of orientations with various precision
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@53732 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 5289ae2c14db17568c97f8d77e45e2565ca545fa

---
+++ rosetta_source/src/basic/sampling/orientations/QuaternionGrid.fwd.hh
@@ -0,0 +1,34 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet;
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   basic/sampling/orientations/QuaternionGrid.fwd.hh
+/// @brief  basic::sampling::orientations::QuaternionGrid forward declaration
+/// @author Will Sheffler <willsheffler@gmail.com>
+
+#ifndef INCLUDED_basic_sampling_orientations_QuaternionGrid_fwd_hh
+#define INCLUDED_basic_sampling_orientations_QuaternionGrid_fwd_hh
+
+#include <utility/pointer/access_ptr.hh>
+
+namespace basic {
+namespace sampling {
+namespace orientations {
+
+class QuaternionGrid;
+typedef utility::pointer::access_ptr<QuaternionGrid const> QuaternionGridCAP;
+
+class QuaternionGridManager;
+typedef utility::pointer::access_ptr<QuaternionGridManager const> QuaternionGridManagerCAP;
+
+}
+}
+}
+
+
+#endif /*INCLUDED_basic_sampling_orientations_QuaternionGrid_FWD_HH*/
---
+++ rosetta_source/src/basic/sampling/orientations/QuaternionGrid.hh
@@ -0,0 +1,206 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet;
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   basic/sampling/orientations/QuaternionGrid.fwd.hh
+/// @brief  provide an evenly spaced set of rotations
+/// @author Will Sheffler <willsheffler@gmail.com>
+
+// much of this comes from http://charles.karney.info/orientation/
+
+#ifndef INCLUDED_basic_sampling_orientations_QuaternionGrid_hh
+#define INCLUDED_basic_sampling_orientations_QuaternionGrid_hh
+
+
+// unit headers
+#include <basic/sampling/orientations/QuaternionGrid.fwd.hh>
+#include <iosfwd>
+#include <utility/vector1.hh>
+#include <utility/pointer/ReferenceCount.hh>
+#include <numeric/xyzVector.hh>
+#include <numeric/xyzMatrix.hh>
+#include <numeric/types.hh>
+#include <map>
+
+namespace basic {
+namespace sampling {
+namespace orientations {
+
+
+
+
+// Minimal quaternion class
+class Quaternion {
+ public:
+    numeric::Real w, x, y, z;
+    Quaternion(numeric::Real ww = 1, numeric::Real xx = 0, numeric::Real yy = 0, numeric::Real zz = 0)
+        : w(ww)    , x(xx)    , y(yy)    , z(zz) {}
+    void Normalize();
+    void Canonicalize() {
+        Normalize();
+        // Make first biggest element positive
+        numeric::Real mag = w;
+        if (abs(x) > abs(mag))      mag = x;
+        if (abs(y) > abs(mag))      mag = y;
+        if (abs(z) > abs(mag))      mag = z;
+        if (mag < 0) {      w *= -1;      x *= -1;      y *= -1;      z *= -1;    }
+        return;
+    }
+    // a.Times(b) returns a * b
+    Quaternion Times(const Quaternion& q) const {
+        numeric::Real
+            mw = w*q.w - x*q.x - y*q.y - z*q.z,
+            mx = w*q.x + x*q.w + y*q.z - z*q.y,
+            my = w*q.y + y*q.w + z*q.x - x*q.z,
+            mz = w*q.z + z*q.w + x*q.y - y*q.x;
+        return Quaternion(mw, mx, my, mz);
+    }
+    void Print(std::ostream& s) const;
+    numeric::xyzVector<numeric::Real> euler() const;
+    void PrintEuler(std::ostream& s) const;
+    numeric::xyzMatrix<numeric::Real> rotation_matrix() const ;
+};
+
+// Class to hold a set of orientations and weights
+class QuatSet {
+ public:
+    Quaternion Orientation(size_t i) const {
+        return m_v[i];
+    }
+    numeric::Real Weight(size_t i) const {
+        return m_w[i];
+    }
+    size_t Number() const {
+        return m_v.size();
+    }
+    void Add(const Quaternion& q, numeric::Real w = 1) {
+        Quaternion v(q);
+        v.Canonicalize();
+        m_v.push_back(v);
+        m_w.push_back(w);
+    }
+    void Clear() {
+        m_v.clear();
+        m_w.clear();
+    }
+    void Print(std::ostream& s, bool euler = false, size_t prec = 6) const;
+ private:
+    std::vector<Quaternion> m_v;
+    std::vector<numeric::Real> m_w;
+};
+
+// The triple of grid indices
+class Triple {
+ public:
+    long a, b, c;
+    Triple(long aa, long bb, long cc)
+        : a(aa)
+        , b(bb)
+        , c(cc) {}
+};
+
+// Generate the permutations and sign changes for a Triple.
+class Permute {
+ public:
+    Permute(Triple x) {
+        assert(x.a >= x.b && x.b >= x.c && x.c >= 0);
+        m_arr.push_back(x);
+        size_t n = 1;
+        // Do the sign changes
+        if (x.a != 0) {
+            for (size_t i = 0; i < n; ++i)
+                m_arr.push_back(Triple(-m_arr[i].a, m_arr[i].b, m_arr[i].c));
+            n *= 2;
+        }
+        if (x.b != 0) {
+            for (size_t i = 0; i < n; ++i)
+                m_arr.push_back(Triple(m_arr[i].a, -m_arr[i].b, m_arr[i].c));
+            n *= 2;
+        }
+        if (x.c != 0) {
+            for (size_t i = 0; i < n; ++i)
+                m_arr.push_back(Triple(m_arr[i].a, m_arr[i].b, -m_arr[i].c));
+            n *= 2;
+        }
+        if (x.a == x.b && x.b == x.c)
+            return;
+        // With at least two distinct indices we can rotate the set thru 3
+        // permuations.
+        for (size_t i = 0; i < n; ++i) {
+            m_arr.push_back(Triple(m_arr[i].b, m_arr[i].c, m_arr[i].a));
+            m_arr.push_back(Triple(m_arr[i].c, m_arr[i].a, m_arr[i].b));
+        }
+        n *= 3;
+        if (x.a == x.b || x.b == x.c)
+            return;
+        // With three distinct indices we can in addition interchange the
+        // first two indices (to yield all 6 permutations of 3 indices).
+        for (size_t i = 0; i < n; ++i) {
+            m_arr.push_back(Triple(m_arr[i].b, m_arr[i].a, m_arr[i].c));
+        }
+        n *= 2;
+    }
+    size_t Number() const {
+        return m_arr.size();
+    }
+    Triple Member(size_t i) const {
+        return m_arr[i];
+    }
+ private:
+    std::vector<Triple> m_arr;
+};
+
+
+class QuaternionGrid : public utility::pointer::ReferenceCount {
+ public: // types
+ 	QuaternionGrid(std::string const & name, std::istream & in);
+ 	void print() const;
+ 	long num_samples() const { return ntot; }
+ 	Quaternion quaternion(long i) const { return s.Orientation(i-1); }
+ 	numeric::xyzVector<numeric::Real> euler(long i) const { return s.Orientation(i-1).euler(); }
+ 	numeric::Real weight(long i) const { return s.Weight(i-1); }
+ private:
+ 	std::string name_;
+    numeric::Real delta, sigma, maxrad, coverage;
+    size_t ncell, ntot, nent;
+    QuatSet s;
+    friend std::ostream & operator<<(std::ostream & out, QuaternionGrid const & q);
+};
+std::ostream & operator<<(std::ostream & out, QuaternionGrid const & q){
+	out << "QuaternionGrid " << q.name_ << ", nsamp: " << q.ntot << ", covering radius: " << q.maxrad << " degrees";
+	return out;
+}
+
+class QuatDBMetadata {
+public:
+	QuatDBMetadata(std::string _name, long _N, numeric::Real _radius, numeric::Real _cover, numeric::Real _delta ) : name(_name),N(_N),radius(_radius),cover(_cover),delta(_delta) {}
+	std::string name;
+	long N;
+	numeric::Real radius,cover,delta;
+};
+
+class QuaternionGridManager : public utility::pointer::ReferenceCount {
+public:
+	static QuaternionGridManager * get_instance();
+	void fill_metadata();
+	QuaternionGridCAP request_by_name(std::string const & name);
+	QuaternionGridCAP request_by_size(long target_size);
+	QuaternionGridCAP request_by_radius(numeric::Real target_radius);
+private:
+	QuaternionGridManager();
+	static QuaternionGridManager * instance_;
+	utility::vector1<QuatDBMetadata> by_size_,by_radius_,by_cover_;
+	std::map<std::string,QuaternionGridCAP> grids_;
+};
+
+}
+}
+}
+
+
+#endif /*INCLUDED_basic_sampling_QuaternionGrid_HH*/
