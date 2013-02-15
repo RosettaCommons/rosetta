093e694da74b04216a57c68e1eab7433f552920b
Gordon Lemmon <glemmon@gmail.com>
2013-02-15 17:11:05 +0000

    Expanding the ResidueTypeTests unit test.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@53750 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: f56ba53e513fdcb914f96608b706be553dd56e96

--- rosetta_source/test/core/chemical/ResidueTypeTests.cxxtest.hh
+++ rosetta_source/test/core/chemical/ResidueTypeTests.cxxtest.hh
@@ -20,7 +20,8 @@
 #include <core/chemical/ResidueType.hh>
 
 // Project Headers
-// AUTO-REMOVED #include <core/chemical/AtomTypeSet.hh>
+#include <core/chemical/AtomTypeSet.hh>
+#include <core/chemical/AtomType.hh>
 #include <core/chemical/ChemicalManager.hh>
 // AUTO-REMOVED #include <core/chemical/ElementSet.hh>
 // AUTO-REMOVED #include <core/chemical/MMAtomTypeSet.hh>
@@ -41,6 +42,7 @@ using std::endl;
 using std::string;
 using basic::Tracer;
 using core::chemical::AtomTypeSetCAP;
+using core::chemical::AtomType;
 using core::chemical::ChemicalManager;
 using core::chemical::ElementSetCAP;
 using core::chemical::MMAtomTypeSetCAP;
@@ -50,6 +52,57 @@ using core::chemical::FA_STANDARD;
 using utility::vector1;
 
 static Tracer TR("core.chemical.ResidueTypeTests.cxxtest");
+
+void add_atom(
+		ResidueType & rsd,
+		AtomTypeSetCAP & atom_types,
+		string const& name, 
+		string const& type, 
+		string const& mm_type, 
+		core::Real const& charge
+){
+	core::Size natoms = rsd.natoms();
+	core::Size nheavyatoms = rsd.nheavyatoms();
+	core::Size nhbond_acceptors = rsd.n_hbond_acceptors();
+	core::Size nhbond_donors = rsd.n_hbond_donors();
+	core::Size Haro_size = rsd.Haro_index().size();
+	core::Size Hpol_size = rsd.Hpol_index().size();
+	core::Size Hpos_polar = rsd.Hpos_polar().size();
+	core::Size Hpos_apolar = rsd.Hpos_apolar().size();
+	core::Size accpt_pos = rsd.accpt_pos().size();
+
+	TS_ASSERT(atom_types->has_atom(type));
+	AtomType const& at = (*atom_types)[ atom_types->atom_type_index(type)];
+	++natoms;
+	if(at.is_heavyatom()) ++nheavyatoms;
+	if(at.is_acceptor()){
+		++nhbond_acceptors;
+		++accpt_pos;
+	}
+	if(at.is_donor()) ++nhbond_donors;
+	if(at.is_haro()) ++Haro_size;
+	if(at.is_polar_hydrogen()){
+		++Hpol_size;
+		if(std::abs(charge > 1.0e-3)) ++Hpos_polar;
+	}else if(std::abs(charge > 1.0e-3)) ++Hpos_apolar;
+
+	rsd.add_atom( name, type, mm_type, charge);
+	
+	TS_ASSERT_EQUALS(rsd.natoms(), natoms);
+	//TS_ASSERT_EQUALS(rsd.nheavyatoms(), nheavyatoms); // Only updated with finalize
+	TS_ASSERT_EQUALS(rsd.n_hbond_acceptors(), nhbond_acceptors);
+	TS_ASSERT_EQUALS(rsd.n_hbond_donors(), nhbond_donors);
+	//TS_ASSERT_EQUALS(rsd.Haro_index().size(), Haro_size); // Only updated with finalize
+	//TS_ASSERT_EQUALS(rsd.Hpol_index().size(), Hpol_size); // Only updated with finalize
+	//TS_ASSERT_EQUALS(rsd.Hpos_polar().size(), Hpos_polar); // Only updated with finalize
+	//TS_ASSERT_EQUALS(rsd.Hpos_apolar().size(), Hpos_apolar); // Only updated with finalize
+	//TS_ASSERT_EQUALS(rsd.accpt_pos().size(), accpt_pos); // Only updated with finalize
+}
+
+void add_bond(ResidueType & rsd, std::string const& a1, std::string const& a2){
+	rsd.add_bond(a1,a2);
+	//TS_ASSERT_EQUALS( rsd.path_distance(rsd.atom_index(a1), rsd.atom_index(a2)), 1); // Only updated with finalize
+}
 
 class ResidueTypeTests : public CxxTest::TestSuite {
 
@@ -63,27 +116,133 @@ public:
 
 	void test_residue_type_initialization() {
 		ChemicalManager * cm(ChemicalManager::get_instance());
-
 		string const tag(FA_STANDARD);
-
 		// minirosett_database/chemical/atom_type_sets/<tag>
 		AtomTypeSetCAP atom_types = cm->atom_type_set(tag);
-
 		// minirosetta_database/chemical/element_sets/<tag>
 		ElementSetCAP element_types = cm->element_set(tag);
-
 		// minirosetta_database/chemical/mm_atom_type_sets/<tag>
 		MMAtomTypeSetCAP mm_atom_types = cm->mm_atom_type_set(tag);
-
 		// minirosetta_database/chemical/orbital_type_sets/<tag>
 		OrbitalTypeSetCAP orbital_types = cm->orbital_type_set(tag);
 
 		ResidueType rsd( atom_types, element_types, mm_atom_types, orbital_types);
 
-		string p("property1");
-		rsd.add_property(p);
-		vector1<string > const & properties(rsd.properties());
- 		TS_ASSERT(!properties[1].compare(p));
+		TS_ASSERT_EQUALS( &(*atom_types), &rsd.atom_type_set());
+		TS_ASSERT_EQUALS(atom_types, rsd.atom_type_set_ptr());
+		TS_ASSERT_EQUALS(&(*element_types), &rsd.element_set());
+
+		TS_ASSERT_EQUALS( rsd.natoms(), (core::Size) 0);
+		TS_ASSERT_EQUALS( rsd.nheavyatoms(), (core::Size) 0);
+
+		rsd.add_property("PROTEIN");
+		TS_ASSERT(rsd.has_property("PROTEIN"));
+
+/// Build an Alanine
+		add_atom( rsd, atom_types," N  ", "Nbb", "NH1", -0.47);
+		add_atom( rsd, atom_types," CA ", "CAbb", "CT1", 0.07);
+		add_atom( rsd, atom_types," C  ", "CObb", "C", 0.51);
+		add_atom( rsd, atom_types," O  ", "OCbb", "O", -0.51);
+		add_atom( rsd, atom_types," CB ", "CH3", "CT3", -0.27);
+		add_atom( rsd, atom_types," H  ", "HNbb", "H", 0.31);
+		add_atom( rsd, atom_types," HA ", "Hapo", "HB", 0.09);
+		add_atom( rsd, atom_types,"1HB ", "Hapo", "HA", 0.09);
+		add_atom( rsd, atom_types,"2HB ", "Hapo", "HA", 0.09);
+		add_atom( rsd, atom_types,"3HB ", "Hapo", "HA", 0.09);
+
+		rsd.set_lower_connect_atom("N");
+		rsd.set_upper_connect_atom("C");
+
+		TS_ASSERT_EQUALS( rsd.lower_connect_atom(), rsd.atom_index("N"));
+		TS_ASSERT_EQUALS( rsd.upper_connect_atom(), rsd.atom_index("C"));
+		
+		add_bond( rsd, "N", "CA");
+		add_bond( rsd, "N", "H");
+		add_bond( rsd, "CA", "C");
+		add_bond( rsd, "CA", "CB");
+		add_bond( rsd, "CA", "HA");
+		add_bond( rsd, "C", "O");
+		add_bond( rsd, "CB", "1HB");
+		add_bond( rsd, "CB", "2HB");
+		add_bond( rsd, "CB", "3HB");
+
+		
+		rsd.nbr_atom("CB");
+		TS_ASSERT_EQUALS(rsd.nbr_atom(), rsd.atom_index("CB"));
+		rsd.nbr_radius(3.4473);
+		TS_ASSERT_EQUALS(rsd.nbr_radius(), 3.4473);
+		rsd.add_actcoord_atom("CB");
+		TS_ASSERT_EQUALS(rsd.actcoord_atoms().size(), (core::Size) 1);
+
+		rsd.set_icoor("N", 0.000000, 0.000000, 0.000000, "N",  "CA", "C");
+		rsd.set_icoor("CA", 0.000000, 180.000000, 1.458001, "N",  "CA", "C");
+		rsd.set_icoor("C", 0.000000, 68.800003, 1.523258, "CA",  "N", "C");
+		rsd.set_icoor("UPPER",  149.999985,  63.800007, 1.328685, "C",  "CA", "N");
+		rsd.set_icoor("O", -180.000000, 59.200005, 1.231015, "C",  "CA", "UPPER");
+		rsd.set_icoor("CB", -123.028732, 69.625412, 1.521736, "CA",  "N", "C");
+		rsd.set_icoor("1HB",-179.994110, 70.562309, 1.090040, "CB",  "CA", "N");
+		rsd.set_icoor("2HB", 119.960403, 70.475021, 1.090069, "CB",  "CA", "1HB");
+		rsd.set_icoor("3HB", 120.089012, 70.493805, 1.088803, "CB",  "CA", "2HB");
+		rsd.set_icoor("HA", -119.013138, 71.295197, 1.090078, "CA",  "N", "CB");
+		rsd.set_icoor("LOWER", -150.000000,  58.300003, 1.328685, "N",  "CA", "C");
+		rsd.set_icoor("H", -180.000000,  60.849998,   1.010000, "N",  "CA", "LOWER");
+
+		rsd.finalize();
+
+		//TS_ASSERT_EQUALS( rsd.chi_atoms( rsd.atom_index("CA")).size(), (core::Size) 3); // we have to "add_chi" first and ALA has no chi listed
+		
+		TS_ASSERT_EQUALS( rsd.path_distance(rsd.atom_index("N"), rsd.atom_index("C")), 2);
+		TS_ASSERT_EQUALS( rsd.path_distance(rsd.atom_index("N"), rsd.atom_index("O")), 3);
+
+		TS_ASSERT_EQUALS( rsd.attached_H_begin( rsd.atom_index("N")), (core::Size) 6);
+		TS_ASSERT_EQUALS( rsd.attached_H_end( rsd.atom_index("N")), (core::Size) 6);
+		TS_ASSERT_EQUALS( rsd.attached_H_begin( rsd.atom_index("CB")), (core::Size) 8);
+		TS_ASSERT_EQUALS( rsd.attached_H_end( rsd.atom_index("CB")), (core::Size) 10);
+		TS_ASSERT_EQUALS( rsd.number_bonded_hydrogens( rsd.atom_index("N")), (core::Size) 1);
+		TS_ASSERT_EQUALS( rsd.number_bonded_hydrogens( rsd.atom_index("CB")), (core::Size) 3);
+		TS_ASSERT_EQUALS( rsd.bonded_neighbor( rsd.atom_index("N")).size(), (core::Size) 2);
+		TS_ASSERT_EQUALS( rsd.bonded_neighbor( rsd.atom_index("CA")).size(), (core::Size) 4);
+		TS_ASSERT_EQUALS( rsd.bonded_neighbor( rsd.atom_index("O")).size(), (core::Size) 1);
+		TS_ASSERT_EQUALS( rsd.bonded_neighbor( rsd.atom_index("CA")), rsd.nbrs( rsd.atom_index("CA")));
+		TS_ASSERT( ! rsd.heavyatom_has_polar_hydrogens( rsd.atom_index("CB"))); // should be > 0
+		TS_ASSERT( rsd.heavyatom_is_an_acceptor( rsd.atom_index("O")));
+		TS_ASSERT( ! rsd.atom_is_polar_hydrogen( rsd.atom_index("O")));
+		TS_ASSERT_EQUALS(rsd.mainchain_atoms().size(), (core::Size) 0); // Number of MC atoms should be 4? // Is this even used?
+		//TS_ASSERT_EQUALS(rsd.mainchain_atom( rsd.atom_index("N")), (core::Size) 1); // Index of MC atom should be 1
+		TS_ASSERT( ! rsd.has( "BURRITO" ));
+
+		//core::Size all_bb_atoms_size = rsd.all_bb_atoms().size();
+		//core::Size all_sc_atoms_size = rsd.all_sc_atoms().size();
+		//core::Size Hpos_polar_sc = rsd.Hpos_polar_sc().size();
+		//core::Size accpt_pos_sc = rsd.accpt_pos_sc().size();
+		TS_ASSERT_EQUALS(rsd.all_bb_atoms().size(), (core::Size) 0); // Why are all atoms being counted as side chain atoms?
+		TS_ASSERT_EQUALS(rsd.all_sc_atoms().size(), (core::Size) 10); // Why are all atoms being called side chain atoms?
+		TS_ASSERT_EQUALS(rsd.Hpos_polar_sc().size(), (core::Size) 1);
+		TS_ASSERT_EQUALS(rsd.accpt_pos_sc().size(), (core::Size) 1);
+		
+		//ResidueTypeOP rsd_copy(rsd.clone());
+		//ResidueType rsd_copy2(rsd);
+		//TS_ASSERT_EQUALS(rsd_copy, rsd);
+		//TS_ASSERT_EQUALS(rsd_copy2, rsd);
+
+		rsd.delete_atom("3HB");
+		rsd.delete_atom("2HB");
+		rsd.delete_atom("1HB");
+		rsd.delete_atom("HA");
+		rsd.delete_atom("H");
+		rsd.delete_atom("CB");
+
+		TS_ASSERT_EQUALS(rsd.natoms(), 4);
+
+		rsd.finalize();
+
+		rsd.delete_atom("O"); // can't delete these
+		rsd.delete_atom("C");
+		rsd.delete_atom("CA");
+		rsd.delete_atom("N");
+
+		TS_ASSERT_EQUALS(rsd.natoms(), 0);
+		//rsd.finalize(); // residue type doesn't support removing the backbone...
 
 	}
 
