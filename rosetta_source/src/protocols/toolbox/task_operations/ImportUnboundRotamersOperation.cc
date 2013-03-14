bbda592f332fa6f704ab47e549c6f94d50af9700
David La <davela@uw.edu>
2013-03-14 08:41:21 +0000

    Added UnboundRotamersOperation, to load in ubound rotamers ... to be used with the load_unbound_rot mover to favor native rotamers during designing/packing of sidechains.  See wiki for more information.
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@54257 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: d9e2de1dd488cb4c780b1f7033b4580e736024d2

---
+++ rosetta_source/src/protocols/toolbox/task_operations/ImportUnboundRotamersOperation.cc
@@ -0,0 +1,89 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/toolbox/task_operations/ImportUnboundRotamersOperation.cc
+/// @brief  Give user-defined native rotamers a bonus!
+/// @author Dave La ( davela@uw.edu )
+
+
+// Unit Headers
+#include <protocols/toolbox/task_operations/ImportUnboundRotamersOperation.hh>
+#include <protocols/toolbox/task_operations/ImportUnboundRotamersOperationCreator.hh>
+
+#include <core/chemical/AA.hh>
+#include <core/conformation/Residue.hh>
+// AUTO-REMOVED #include <core/pose/Pose.hh>
+// AUTO-REMOVED #include <core/pack/rotamer_set/RotamerSet_.hh>
+#include <core/pack/rotamer_set/RotamerSetOperation.hh>
+#include <core/pack/task/PackerTask.hh>
+#include <core/pack/task/operation/TaskOperation.hh>
+#include <utility/tag/Tag.hh>
+
+
+#include <core/pack/rotamer_set/RotamerSet.hh>
+#include <core/pack/rotamer_set/UnboundRotamersOperation.hh>
+#include <utility/vector0.hh>
+#include <utility/vector1.hh>
+
+#ifdef WIN32
+	#include <core/graph/Graph.hh>
+#endif
+
+namespace protocols {
+namespace toolbox {
+namespace task_operations {
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+core::pack::task::operation::TaskOperationOP
+ImportUnboundRotamersOperationCreator::create_task_operation() const
+{
+	return new ImportUnboundRotamersOperation;
+}
+
+/// @brief default constructor
+ImportUnboundRotamersOperation::ImportUnboundRotamersOperation():
+	TaskOperation()
+{}
+
+/// @brief destructor
+ImportUnboundRotamersOperation::~ImportUnboundRotamersOperation(){}
+
+/// @brief clone
+core::pack::task::operation::TaskOperationOP
+ImportUnboundRotamersOperation::clone() const {
+	return new ImportUnboundRotamersOperation( *this );
+}
+
+//mjo commenting out 'pose' because it is unused and causes a warning
+/// @brief
+void
+ImportUnboundRotamersOperation::apply( Pose const & /*pose*/, PackerTask & task ) const
+{
+	core::pack::rotamer_set::UnboundRotamersOperationOP unboundrot_ = new core::pack::rotamer_set::UnboundRotamersOperation;
+	unboundrot_->initialize_from_command_line();
+	task.append_rotamerset_operation( unboundrot_ );
+
+}
+
+void
+ImportUnboundRotamersOperation::parse_tag( TagPtr tag )
+{
+}
+
+void
+ImportUnboundRotamersOperation::parse_def( utility::lua::LuaObject const & def)
+{
+}                                                                              
+
+
+} // TaskOperations
+} // toolbox
+} // protocols
+
---
+++ rosetta_source/src/protocols/toolbox/task_operations/ImportUnboundRotamersOperation.hh
@@ -0,0 +1,100 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/toolbox/task_operations/ImportUnboundRotamersOperation.hh
+/// @brief  eliminate aromatic rotamers, of which chi2 are around 0, 180 degree.
+/// @detail Chi2=0, 180 rotamers of aromatic residues ( PHE, TYR, HIS ) are not observed in nature very much,
+/// however Rosetta really like them. This is really pathology. For design purpose, we don't need them actually.
+/// @author Nobuyasu Koga ( nobuyasu@uw.edu )
+
+
+#ifndef INCLUDED_protocols_toolbox_task_operations_ImportUnboundRotamersOperation_hh
+#define INCLUDED_protocols_toolbox_task_operations_ImportUnboundRotamersOperation_hh
+
+
+// Unit Headers
+#include <protocols/toolbox/task_operations/ImportUnboundRotamersOperation.fwd.hh>
+#include <core/pack/rotamer_set/RotamerSetOperation.hh>
+
+#include <core/pack/task/PackerTask.fwd.hh>
+#include <core/pack/task/operation/TaskOperation.hh>
+
+// Package Headers
+#include <core/pack/rotamer_set/RotamerSet.fwd.hh>
+#include <core/graph/Graph.fwd.hh>
+#include <core/pose/Pose.fwd.hh>
+#include <core/scoring/ScoreFunction.fwd.hh>
+#include <core/pack/dunbrack/ChiSet.fwd.hh>
+
+// Utility Headers
+#include <utility/pointer/ReferenceCount.hh>
+#include <utility/tag/Tag.fwd.hh>
+#include <utility/vector1.hh>
+
+#include <core/pack/rotamer_set/UnboundRotamersOperation.fwd.hh>
+
+
+namespace protocols {
+namespace toolbox {
+namespace task_operations {
+
+
+class ImportUnboundRotamersOperation : public core::pack::task::operation::TaskOperation {
+public:
+
+
+	typedef core::Real Real;
+	typedef core::pose::Pose Pose;
+	typedef core::pack::task::PackerTask PackerTask;
+	typedef core::pack::task::operation::TaskOperation TaskOperation;
+	typedef core::pack::task::operation::TaskOperationOP TaskOperationOP;
+	typedef TaskOperation parent;
+	typedef utility::tag::TagPtr TagPtr;
+
+
+public:
+
+
+	/// @brief default constructor
+	ImportUnboundRotamersOperation();
+
+	/// @brief destructor
+	virtual ~ImportUnboundRotamersOperation();
+
+	/// @brief make clone
+	virtual TaskOperationOP clone() const;
+	
+
+public:
+
+
+	/// @brief apply
+	virtual void apply( Pose const & pose, PackerTask & task ) const;
+
+
+public:
+
+
+	void parse_tag( TagPtr tag );
+	void parse_def( utility::lua::LuaObject const & def );
+
+
+private: // data
+
+	core::pack::rotamer_set::UnboundRotamersOperationOP unboundrot_;		
+
+};
+
+
+} // TaskOperations
+} // toolbox
+} // protocols
+
+
+#endif
