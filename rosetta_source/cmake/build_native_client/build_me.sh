b90c5b1baa67805f0a7f346202b384ffcb09bb41
Mike Tyka <mike.tyka@gmail.com>
2013-03-19 23:08:54 +0000

    Just updates to the way rosetta_native client is compiled (pilotapp)
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@54390 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 47df7d43caafb20c1f4eb8f603501fa2dd6e4769

--- rosetta_source/cmake/build_native_client/build_me.sh
+++ rosetta_source/cmake/build_native_client/build_me.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CC=../../external/native_client_sdk_0_2_803_0/toolchain/mac_x86/bin/nacl-gcc
-export CXX=../../external/native_client_sdk_0_2_803_0/toolchain/mac_x86/bin/nacl-g++
+export CC=/Users/mtyka/src/nacl_sdk/pepper_23/toolchain/mac_x86_newlib/bin/x86_64-nacl-gcc
+export CXX=/Users/mtyka/src/nacl_sdk/pepper_23/toolchain/mac_x86_newlib/bin/x86_64-nacl-g++
 cmake ./
 make -j 4
 
