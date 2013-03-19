80a9780a0cb514e40bdf9c4d4acf5e55d0f5b65f
Lei Shi <shileiustc@gmail.com>
2013-03-19 16:11:27 +0000

    Add DockWithHotspotMover for hotspot guided docking before PlaceSimultaneousMover
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@54383 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 4cd790dbc04ceae2cf395af1a72f573fcb7485fe

---
+++ rosetta_source/src/protocols/protein_interface_design/movers/DockWithHotspotMover.fwd.hh
@@ -0,0 +1,34 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file
+/// @brief
+/// @author Lei Shi <shilei@uw.edu>
+
+#ifndef INCLUDED_protocols_protein_interface_design_movers_DockWithHotspotMover_fwd_hh
+#define INCLUDED_protocols_protein_interface_design_movers_DockWithHotspotMover_fwd_hh
+
+#include <utility/pointer/owning_ptr.hh>
+
+
+// Package headers
+
+namespace protocols {
+namespace protein_interface_design {
+namespace movers {
+
+class DockWithHotspotMover;
+typedef utility::pointer::owning_ptr< DockWithHotspotMover > DockWithHotspotMoverOP;
+typedef utility::pointer::owning_ptr< DockWithHotspotMover const > DockWithHotspotMoverCOP;
+
+} //movers
+} //protein_interface_design
+} //protocols
+
+#endif //INCLUDED_protocols_protein_interface_design_movers_DockWithHotspotMover_FWD_HH
