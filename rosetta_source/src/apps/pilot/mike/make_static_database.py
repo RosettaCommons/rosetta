9f62d376964c8b72fff643288fffb12d4834acd8
Mike Tyka <mike.tyka@gmail.com>
2013-04-02 19:36:56 +0000

    Just adding a helper tool for my pilot apps
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta/@54641 6189c7af-7406-0410-a2ce-f22f51069c23
    
    Old repository SHA1: 5b6c9add90382733d929216b3ce5e2663db0b573

---
+++ rosetta_source/src/apps/pilot/mike/make_static_database.py
@@ -0,0 +1,56 @@
+import sys;
+import os
+import os.path
+import re
+
+output = "static_database.hh";
+path_prefix = "/Users/mtyka/rosetta_database/"
+
+outfile = open( output, "w" )
+
+outfile.write('#ifndef STATIC_DATABASE_HH\n')
+outfile.write('#define STATIC_DATABASE_HH\n')
+
+outfile.write('const char* static_database[ ][2] = {\n')
+
+# some regulat expressions we'll need
+
+escaped_chars = re.compile('(["\\%])');
+
+
+for file in sys.argv[1:] :
+
+  if( file[0:2] == "./" ): file = file[2:]
+
+  file = re.sub( '//', '/', file )
+
+
+
+  
+  print "Reading: ", file
+
+  fullfilename =  path_prefix + "/" +  file
+
+  try:
+    input = open( fullfilename, "r" );
+  except: 
+    print "ERROR Reading ", fullfilename 
+    outfile.write('{"'+file+'",""},\n')
+    continue
+
+  outfile.write('{\n')
+  outfile.write('"'+file+'",\n"\\\n')
+  inputdata = input.read().split("\n")
+  for line in inputdata:
+    if line == "": continue
+    #prefix these characters with backslashes
+    line = re.sub( '\\\\', '\\\\\\\\', line )  
+    line = re.sub( '"', '\\"', line )  
+#    line = re.sub( '%', '\\%', line )  
+    outfile.write( line + '\\n\\\n' )
+  outfile.write( '"},\n' )
+
+outfile.write('{"",""}};\n')
+outfile.write('const int static_database_size = %d;\n\n'%(len(sys.argv[1:])) )
+outfile.write('#endif\n\n')
+outfile.close()
