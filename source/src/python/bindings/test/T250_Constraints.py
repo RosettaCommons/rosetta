33e5660154655eeb4bdd2bf899592b17d47593bd
Kenny Kostenbader <kensteroo22@gmail.com>
2013-04-24 03:09:42 +0000

    reverted path to pdb, not sure which to use
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@54998 6189c7af-7406-0410-a2ce-f22f51069c23
    
    ----
    An additional 1 commit messages were squashed into this commit:
    --
    added set_weight to constraint test
    
    git-svn-id: https://svn.rosettacommons.org/source/trunk/rosetta@54996 6189c7af-7406-0410-a2ce-f22f51069c23
    --
    
    Old repository SHA1: 27a2fd7dbdb7a54003d836a5d972f02f9210fe8e

--- source/src/python/bindings/test/T250_Constraints.py
+++ source/src/python/bindings/test/T250_Constraints.py
@@ -6,14 +6,16 @@ rosetta.init()
 print 'constraints ----------------------------------------------'
 
 pose = pose_from_pdb("test/data/test_in.pdb")
+
 scorefxn = create_score_function('standard')
+scorefxn.set_weight( atom_pair_constraint, 10 )
 scorefxn(pose)
 
 print
 print 'Score before constraints applied:', scorefxn(pose)
 
-hterm2 = AtomID(1, 137)
-gterm2 = AtomID(1, 91)
+hterm2 = AtomID(1, 55)
+gterm2 = AtomID(1, 5)
 
 GF = constraints.GaussianFunc( 4.0, 2.0 )
 apc = constraints.AtomPairConstraint( hterm2, gterm2, GF )
