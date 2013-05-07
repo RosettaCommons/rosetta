beea549a77877553df74220a938e5521b5b423dc
Sergey's Benchmarking account <Sergey.Lyskov@jhu.edu>
2013-05-07 18:50:13 -0400

    Fixing build on GCC 4.1.
    Old repository SHA1: edf19a90c62e5c47e82d163dab2f075a3186a1aa

--- source/src/devel/sewing/SewingDesigner.cc
+++ source/src/devel/sewing/SewingDesigner.cc
@@ -90,7 +90,7 @@
 
 namespace devel {
 namespace sewing {
-	
+
 using namespace core;
 using namespace io;
 using namespace pdb;
@@ -132,7 +132,7 @@ Mover("SewingDesigner")
 {
 	init();
 }
-	
+
 SewingDesigner::SewingDesigner(
 	NativeRotamersMap native_helix_residues
 ):
@@ -163,7 +163,7 @@ protocols::moves::MoverOP
 SewingDesigner::fresh_instance() const {
 	return protocols::moves::MoverOP( new SewingDesigner );
 }
-	
+
 string
 SewingDesigner::get_name() const {
 	return "SewingDesigner";
@@ -195,56 +195,56 @@ SewingDesigner::apply( pose::Pose & pose )
 
 	repack_mover_ = new protocols::simple_moves::PackRotamersMover;
 	repack_mover_->score_function(scorefxn_);
-	
+
 	//Look for breaks in the pose and keep residues as anchors for loops
 	if(loop_anchors_.size()==0){
 		detect_loop_anchors(pose);
 	}
-	
+
 	//optimally rearrange helix segments to minimize loop distance
 	rearrange_pose(pose, native_helix_residues_, loop_anchors_);
-	
+
 	//Do an initial design of the entire protein
 	TR << "Statistic before design: " << std::endl;
 	std::map<core::Size, char> empty;
 	record_statistics(pose, native_helix_residues_, empty);
 	design(pose, native_helix_residues_);
 	pose.dump_pdb("initial_design.pdb");
-	
+
 	TR << "Statistic after initial design: " << std::endl;
 	record_statistics(pose, native_helix_residues_, empty);
-	
+
 	//Try to design a loop after each loop_anchor
 	std::map<Size,char> native_loop_residues;
 	for(core::Size loop_index=1; loop_index<=loop_anchors_.size(); ++loop_index)
 	{
 		core::Size cur_anchor = loop_anchors_[loop_index];
 		TR << "Trying to insert loop after residue: " << cur_anchor << std::endl;
-		
+
 		//update the loop_inserter's loop_anchor and apply
 		loop_creation_mover_->loop_inserter()->loop_anchor(cur_anchor);
 		loop_creation_mover_->apply(pose);
-		
+
 		protocols::loops::Loop new_loop=loop_creation_mover_->get_last_created_loop();
-		
+
 		std::set<Size> loop_residues;
 		for(core::Size i=new_loop.start(); i<=new_loop.stop(); ++i)
 		{
 			loop_residues.insert(i);
 		}
-		
+
 		std::string const nb_calc("loop_neighboorhood_calculator");
 		pose::metrics::CalculatorFactory::Instance().register_calculator( nb_calc,
 			new protocols::toolbox::pose_metric_calculators::NeighborhoodByDistanceCalculator( loop_residues ) );
-			
+
 		basic::MetricValue< std::set< Size > > neighbor_mv;
 		pose.metric( nb_calc, "neighbors", neighbor_mv);
 		std::set<Size> const neighbor_set ( neighbor_mv.value() );
-		
+
 		//Update native rotamers map for the given insert
 		update_native_helix_residues(native_helix_residues_, new_loop);
 		update_anchors(loop_anchors_, new_loop, loop_index);
-		
+
 		//record all the loop residues before design so we can generate recovery metrics
 		native_loop_residues.clear();
 		for(core::Size i=1; i<=pose.total_residue(); ++i)
@@ -255,7 +255,7 @@ SewingDesigner::apply( pose::Pose & pose )
 				native_loop_residues.insert(std::make_pair(i, pose.residue(i).type().name1()));
 			}
 		}
-		
+
 		asym_size_+=new_loop.size();
 		core::Size dup_anchor=cur_anchor+asym_size_;
 		TR << "asym size is: " << asym_size_ << std::endl;
@@ -267,7 +267,7 @@ SewingDesigner::apply( pose::Pose & pose )
 			loop_creation_mover_->copy_last_loop_to_new_anchor(pose, dup_anchor);
 			dup_anchor+=asym_size_;
 		}
-		
+
 		//Design/repack & score
 		design_neighborhood(pose, native_helix_residues_, neighbor_set);
 
@@ -285,7 +285,7 @@ SewingDesigner::apply( pose::Pose & pose )
 	}
 	std::map<std::string, core::Real> const & native_retention_map =
 		( static_cast< basic::datacache::DiagnosticData const &>( pose.data().get( core::pose::datacache::CacheableDataType::SCORE_MAP ))).data();
-		
+
 	protocols::jd2::JobOP const job_me = protocols::jd2::JobDistributor::get_instance()->current_job();
 	job_me->add_string_real_pair("native_helix_percent", native_retention_map.find("native_helix_percent")->second);
 	job_me->add_string_real_pair("native_helix_percent_buried", native_retention_map.find("native_helix_percent_buried")->second);
@@ -308,19 +308,19 @@ SewingDesigner::record_statistics(
 	std::string sequence = pose.sequence();
 	Size total_helix_exposed=0;
 	Size total_helix_buried=0;
-	
+
 	Size recovered_helix_total=0;
 	Size recovered_helix_exposed=0;
 	Size recovered_helix_buried=0;
-	
+
 	Size recovered_loop=0;
-	
+
 	Size total_loop_pro=0;
 	Size total_loop_gly=0;
-	
+
 	Size recovered_loop_pro=0;
 	Size recovered_loop_gly=0;
-	
+
 	std::set<core::Size> retained_helix_residues;
 	std::set<core::Size> retained_loop_residues;
 
@@ -380,7 +380,7 @@ SewingDesigner::record_statistics(
 				gly=true;
 				total_loop_gly++;
 			}
-			
+
 			if(sequence[cur_res-1]==native_loop_residues.find(cur_res)->second)
 			{
 				recovered_loop++;
@@ -391,52 +391,52 @@ SewingDesigner::record_statistics(
 		}
 	}
 	std::map<std::string, core::Real> native_retention_map;
-	
+
 	core::Real native_helix_percent = (core::Real)recovered_helix_total/native_helix_residues.size();
 	native_retention_map.insert(std::make_pair("native_helix_percent", native_helix_percent));
-	
+
 	core::Real native_helix_percent_buried = (core::Real)recovered_helix_buried/total_helix_buried;
 	native_retention_map.insert(std::make_pair("native_helix_percent_buried", native_helix_percent_buried));
-	
+
 	core::Real native_helix_percent_exposed = (core::Real)recovered_helix_exposed/total_helix_exposed;
 	native_retention_map.insert(std::make_pair("native_helix_percent_exposed", native_helix_percent_exposed));
-	
+
 	core::Real native_loop_percent = 0;
 	if(native_loop_residues.size() != 0)
 	{
 		native_loop_percent = (core::Real)recovered_loop/native_loop_residues.size();
 		native_retention_map.insert(std::make_pair("native_loop_percent", native_loop_percent));
 	}
-	
+
 	core::Real pro_loop_percent = (core::Real)recovered_loop_pro/total_loop_pro;
 	native_retention_map.insert(std::make_pair("native_loop_pro_percent", pro_loop_percent));
-	
+
 	core::Real gly_loop_percent = (core::Real)recovered_loop_gly/total_loop_gly;
 	native_retention_map.insert(std::make_pair("native_loop_gly_percent", gly_loop_percent));
-	
+
 	std::string pymol_helix_select = "select resi ";
 	for(std::set<Size>::const_iterator it=retained_helix_residues.begin(); it!= retained_helix_residues.end(); ++it)
 	{
 		pymol_helix_select += "+"+utility::to_string(*it);
 	}
 	pymol_helix_select += "/helix_residues";
-	
+
 	std::string pymol_loop_select = "select resi ";
 	for(std::set<Size>::const_iterator it=retained_loop_residues.begin(); it!= retained_loop_residues.end(); ++it)
 	{
 		pymol_loop_select += "+"+utility::to_string(*it);
 	}
 	pymol_loop_select += "/loop_residues";
-	
-	
+
+
 	protocols::jd2::JobOP const job_me ( protocols::jd2::JobDistributor::get_instance()->current_job() );
 	std::string const job_name ( protocols::jd2::JobDistributor::get_instance()->job_outputter()->output_name(job_me) );
-	
+
 //	dump_native_residue_file(native_helix_residues, job_name+".rots");
 
 	TR << "Helix residue select: " << pymol_helix_select << std::endl;
 	TR << "Loop residue select: " << pymol_loop_select << std::endl;
-	
+
 	pose.data().set(core::pose::datacache::CacheableDataType::SCORE_MAP,
 					new basic::datacache::DiagnosticData(native_retention_map));
 	TR << "Percent of native helix residues (buried): " << native_helix_percent_buried << std::endl;
@@ -468,28 +468,28 @@ SewingDesigner::design_neighborhood(
 	//Update loops for each of the new structures and continue closing
 	pack::task::TaskFactoryOP task_factory = new pack::task::TaskFactory;
 	//	task_factory->push_back( new pack::task::operation::InitializeFromCommandline );
-	
+
 	//Add native rotamers for each position in the updated native rotamers map
 	for(NativeRotamersMap::const_iterator map_it = native_helix_residues.begin();
 		map_it != native_helix_residues.end(); ++map_it)
 	{
 		//Create rotamer set operation from a list of residues
 		pack::rotamer_set::AddResiduesRotamerSetOperation const & nat_ro_set(map_it->second);
-		
+
 //		TR.Debug << "Adding the following native rotamers for position: " << map_it->first << std::endl;
 		for(Size temp=1; temp<=map_it->second.size(); ++temp)
 		{
 			conformation::ResidueOP cur_res = map_it->second[temp];
 //			TR.Debug << "Residue Type: " << cur_res->type().name() << std::endl;
 		}
-		
+
 		//Add AppendResidueRotamerSet task operation to the task factory. This task operation
 		//adds the rotamer set to the residue-level task for the given residue
 		task_factory->push_back(
 			new core::pack::task::operation::AppendResidueRotamerSet(map_it->first, nat_ro_set.clone()) );
 	}
-	
-	
+
+
 	//Add constraints for each of the native residues at each position that is a neighbor
 	//and just repack at non-neighbor residues
 	core::Real native_bonus=0.5;
@@ -510,9 +510,9 @@ SewingDesigner::design_neighborhood(
 					core::scoring::constraints::ResidueTypeConstraintOP nat_res_constraint =
 						new core::scoring::constraints::ResidueTypeConstraint(pose, resnum,
 							position_residues[pos_index]->name3(), native_bonus);
-							
+
 					pose.add_constraint(nat_res_constraint);
-					
+
 //					TR.Debug << "Favored residue for helix position " << resnum << ": "
 //						<< position_residues[pos_index]->name() << std::endl;
 				}
@@ -533,7 +533,7 @@ SewingDesigner::design_neighborhood(
 			repack_res->include_residue(resnum);
 		}
 	}
-	
+
 	task_factory->push_back(repack_res);
 	repack_mover_->score_function(scorefxn_);
 	repack_mover_->task_factory(task_factory);
@@ -549,7 +549,7 @@ SewingDesigner::update_native_helix_residues(
 ){
 	core::Size start_of_residue_change=new_loop.stop();
 	core::Size num_residues_inserted=new_loop.size();
-	
+
 	NativeRotamersMap new_native_helix_residues;
 	for(NativeRotamersMap::const_iterator map_it = native_helix_residues.begin();
 		map_it != native_helix_residues.end(); ++map_it)
@@ -584,7 +584,7 @@ struct protein_segment
 {
 	Size start;
 	Size end;
-	
+
 	bool operator<(const protein_segment & rhs) const
 	{
 		return start < rhs.start;
@@ -620,7 +620,7 @@ SewingDesigner::rearrange_pose(
 		}
 		protein_segments.push_back(segment);
 	}
-	
+
 	utility::vector1<core::Size> dim_sizes;
 //	if(num_helices_in_repeat_ > 0)
 //	{
@@ -631,18 +631,18 @@ SewingDesigner::rearrange_pose(
 		dim_sizes = utility::vector1<core::Size>(protein_segments.size(), protein_segments.size());
 //	}
 	core::Size n_dims = dim_sizes.size();
-	Real min_distance_sum = 1000000000000;
+	Real min_distance_sum = 1000000000;
 	utility::vector1<core::Size> rearrangement_order;
 	for ( utility::LexicographicalIterator lex( dim_sizes ); ! lex.at_end(); ++lex )
 	{
 		bool valid=true;
-		
+
 		//Force either the first or second helix to be the first in the new bundle
 		if(lex[1] != 1 && lex[1] != 2)
 		{
 			valid=false;
 		}
-		
+
 		//Don't allow duplicate helix segments
 		for(Size i=1; i<=n_dims; ++i)
 		{
@@ -654,7 +654,7 @@ SewingDesigner::rearrange_pose(
 				}
 			}
 		}
-		
+
 		if(valid)
 		{
 			Real dist_sq=0;
@@ -678,7 +678,7 @@ SewingDesigner::rearrange_pose(
 			}
 		}
 	}
-	
+
 	//Use the rearrangment order of the unique (non-repeat) helices to create order for the entire pose
 	utility::vector1<protein_segment> best_order;
 //	if(num_helices_in_repeat_ > 0)
@@ -701,13 +701,13 @@ SewingDesigner::rearrange_pose(
 		best_order.push_back(protein_segments[rearrangement_order[j]]);
 	}
 //	}
-	
+
 	TR << "Best order for pose:\n " << std::endl;
 	for(Size i=1; i<=best_order.size(); ++i)
 	{
 		TR << best_order[i].start << " " << best_order[i].end << std::endl;
 	}
-	
+
 	core::pose::Pose reordered_pose;
 	NativeRotamersMap reordered_native_helix_residues;
 //	protocols::loops::Loops reordered_loop;
@@ -726,12 +726,12 @@ SewingDesigner::rearrange_pose(
 				conformation::remove_upper_terminus_type_from_conformation_residue(pose.conformation(), j);
 				reordered_pose.conformation().append_residue_by_bond(pose.residue(j));
 			}
-			
+
 //			if(i!=1 && j==cur_frag.start)
 //			{
 //				reordered_loops.add_loop(reordered_pose.total_residue()-1, reordered_pose.total_residue());
 //			}
-			
+
 			reordered_native_helix_residues[residue_counter] = native_helix_residues[j];
 			++residue_counter;
 		}
@@ -742,7 +742,7 @@ SewingDesigner::rearrange_pose(
 	pose = reordered_pose;
 	native_helix_residues = reordered_native_helix_residues;
 //	loops = reordered_loops;
-	
+
 	protocols::jd2::JobOP const job_me ( protocols::jd2::JobDistributor::get_instance()->current_job() );
 	std::string const job_name ( protocols::jd2::JobDistributor::get_instance()->job_outputter()->output_name(job_me) );
 	reordered_pose.dump_pdb(job_name+"_reordered.pdb");
@@ -771,7 +771,7 @@ SewingDesigner::parse_my_tag(
 	{
 		utility_exit_with_message("You must provide a 'loop_creation_mover' option to the SewingDesigner tag");
 	}
-	
+
 	if(tag->hasOption("loop_anchors"))
 	{
 		string const loop_anchors_string = tag->getOption<string>("loop_anchors");
@@ -790,14 +790,14 @@ SewingDesigner::parse_my_tag(
 		utility_exit_with_message("You must provide a asym_size option to the SewingDesigner tag");
 	}
 }
-	
+
 void
 SewingDesigner::detect_loop_anchors(
 	Pose const & pose
 ){
 	// squared distance at which bond is considered discontinuous
 	Real const chain_break_cutoff = { 4.0 };
-	
+
 	for ( Size i = 1; i < pose.total_residue(); ++i )
 	{
 		Size j = i+1;
@@ -813,6 +813,6 @@ SewingDesigner::detect_loop_anchors(
 		}
 	}
 }
-	
+
 } //sewing
 } //devel
