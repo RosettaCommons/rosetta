ad1dfcacacebb0f1f5c355a44716358d68218809
rmoretti <rmoretti@u.washington.edu>
2013-07-12 18:30:41 -0700

    Add a RosettaScriptable mover to add/remove virtual root residues.
    Old repository SHA1: f658730d49fdbc7b4f12a5e4392a757ece079bc0

---
+++ source/src/protocols/simple_moves/VirtualRootMover.cc
@@ -0,0 +1,119 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/simple_moves/VirtualRootMover.cc
+/// @brief  Manipulate virtual roots on poses. This is a seperate mover mainly to be RosettaScriptable
+/// @author Rocco Moretti (rmoretti@u.washington.edu)
+
+// unit headers
+#include <protocols/simple_moves/VirtualRootMover.hh>
+#include <protocols/simple_moves/VirtualRootMoverCreator.hh>
+
+// type headers
+#include <core/types.hh>
+
+// project headers
+#include <core/pose/Pose.hh>
+#include <core/pose/util.hh>
+
+#include <core/conformation/Conformation.hh>
+#include <core/kinematics/FoldTree.hh>
+#include <core/conformation/Residue.hh>
+#include <core/chemical/AA.hh>
+
+#include <utility/tag/Tag.hh>
+#include <basic/Tracer.hh>
+
+namespace protocols {
+namespace simple_moves {
+
+static basic::Tracer TR("protocols.simple_moves.VirtualRootMover");
+
+std::string
+VirtualRootMoverCreator::keyname() const
+{
+	return VirtualRootMoverCreator::mover_name();
+}
+
+protocols::moves::MoverOP
+VirtualRootMoverCreator::create_mover() const {
+	return new VirtualRootMover;
+}
+
+std::string
+VirtualRootMoverCreator::mover_name()
+{
+	return "VirtualRoot";
+}
+
+VirtualRootMover::VirtualRootMover() :
+	moves::Mover( VirtualRootMoverCreator::mover_name() ),
+	remove_(false),
+	removable_(false)
+{}
+
+VirtualRootMover::~VirtualRootMover() {}
+
+moves::MoverOP VirtualRootMover::clone() const {
+	return new VirtualRootMover( *this );
+}
+moves::MoverOP VirtualRootMover::fresh_instance() const {
+	return new VirtualRootMover;
+}
+
+void
+VirtualRootMover::apply( core::pose::Pose & pose ) {
+	if( remove_ ) {
+		if( pose.residue( pose.fold_tree().root() ).aa() != core::chemical::aa_vrt ) {
+			TR << "Can't remove virtual root - pose is not rooted on virtual residue." << std::endl;
+			return;
+		}
+		core::Real virtroot;
+		if( ! core::pose::getPoseExtraScores( pose, "VirtualRootMover_root", virtroot ) ) {
+			TR << "Virtual root not added as removable by VirtualRootMover - not removing root." << std::endl;
+			return;
+		}
+		if( pose.residue( virtroot ).aa() != core::chemical::aa_vrt || virtroot != pose.fold_tree().root() ) {
+			TR.Warning << "Residue added by VirtualRootMover no longer virtual root - not removing root." << std::endl;
+		} else {
+			pose.conformation().delete_residue_slow(virtroot);
+		}
+		core::pose::clearPoseExtraScore( pose, "VirtualRootMover_root");
+	} else {
+		if ( pose.residue( pose.fold_tree().root() ).aa() != core::chemical::aa_vrt ) {
+			core::pose::addVirtualResAsRoot( pose );
+			if ( removable_ ) {
+				core::Size virtroot = pose.fold_tree().root();
+				assert( virtroot == pose.n_residue() && pose.residue(virtroot).aa() == core::chemical::aa_vrt );
+				core::pose::setPoseExtraScores( pose, "VirtualRootMover_root", virtroot );
+			}
+		} else {
+			TR << "Pose already virtually rooted. Not changing root." << std::endl;
+		}
+	}
+}
+
+std::string
+VirtualRootMover::get_name() const {
+	return VirtualRootMoverCreator::mover_name();
+}
+
+void VirtualRootMover::parse_my_tag(
+	TagPtr const tag,
+	moves::DataMap &,
+	protocols::filters::Filters_map const &,
+	moves::Movers_map const &,
+	Pose const &
+) {
+	remove_ =  tag->getOption<bool>("remove", false);
+	removable_ = tag->getOption<bool>("removable", false);
+}
+
+} // simple_moves
+} // protocols
---
+++ source/src/protocols/simple_moves/VirtualRootMover.hh
@@ -0,0 +1,54 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/simple_moves/VirtualRootMover.hh
+/// @brief  Manipulate virtual roots on poses.
+/// @author Rocco Moretti (rmoretti@u.washington.edu)
+
+#ifndef INCLUDED_protocols_simple_moves_VirtualRootMover_hh
+#define INCLUDED_protocols_simple_moves_VirtualRootMover_hh
+
+// Unit headers
+#include <protocols/simple_moves/VirtualRootMover.fwd.hh>
+#include <protocols/moves/Mover.hh>
+
+#include <core/types.hh>
+
+namespace protocols {
+namespace simple_moves {
+
+class VirtualRootMover : public moves::Mover {
+
+public:
+	VirtualRootMover();
+	~VirtualRootMover();
+	virtual moves::MoverOP clone() const;
+	virtual moves::MoverOP fresh_instance() const;
+
+	virtual void apply( Pose & pose );
+	virtual std::string get_name() const;
+
+	virtual
+	void parse_my_tag(
+		TagPtr const,
+		moves::DataMap &,
+		protocols::filters::Filters_map const &,
+		moves::Movers_map const &,
+		Pose const & );
+
+private:
+	bool remove_;
+	bool removable_;
+
+};
+
+} // simple_moves
+} // protocols
+
+#endif //INCLUDED_protocols_simple_moves_VirtualRootMover_HH
