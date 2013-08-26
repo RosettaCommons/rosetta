8e99b7bdeec62bf83a2b2b694f51f1f0b9caaafc
Sergey Lyskov <sergey.lyskov@jhu.edu>
2013-08-26 13:57:01 -0600

    PyRosetta. Adding doxygen docs extraction for 'details', 'param' and 'return' doxygen keywords.
    Old repository SHA1: 3dc19ccce1b2c5fb23527e7e4dcac36d99d24f03

--- source/src/python/bindings/tools/dox_extract.py
+++ source/src/python/bindings/tools/dox_extract.py
@@ -22,10 +22,10 @@ class dox_extract:
         self.contains_end_block_dox_comment = re.compile( '\*\*/\s*' )
         self.contains_start_block_dox_comment = re.compile( '\s*/\*\*' )
         self.contains_three_slash_dox_comment = re.compile( '^\s*///' )
-        
+
         # add doxygen symbols here as needed
         self.dox_symbols = [
-            'brief',
+            'brief', 'details', 'param', 'return',
             'fn',
             'file',
             'author',
@@ -37,7 +37,7 @@ class dox_extract:
         line = re.sub( self.contains_three_slash_dox_comment, '', line )
         line = re.sub( self.contains_start_block_dox_comment, '', line )
         return line.rstrip() + '\n'
-        
+
     def remove_unnecessary_whitespace( self, lines ):
         there_is_whitespace = False
         begins_with_white_space = self.line_begins_with_white_space.search( lines[ 0 ] )
@@ -52,41 +52,41 @@ class dox_extract:
             else:
                 there_is_whitespace = False
         return lines
-        
+
     def remove_dox_symbols( self, lines ):
         for dox_symbol in self.dox_symbols:
             for i in range( len( lines ) ):
                 lines[ i ] = re.sub( re.compile( '@' + dox_symbol + '\s*' ), '', lines[ i ] )
         return lines
-    
+
     def getDoxygenComment( self, file_name, line_number ):
         ''' Search and return doxygen comments at given file/line position and return them
             Note: - line is a number in string form here.
                   - if no appropriate comments could be found (or result is empty) function should return  file_ + ':' + line  string
         '''
-            
+
         line_number = int( line_number )
-    
+
         i_say_so = True
         block_comment = False
-    
+
         file = open( file_name, 'r' )
         lines = file.readlines()
         file.close()
-        
+
         comment_block = []
         current_line_number = line_number - 1
-    
+
         while i_say_so:
             current_line_number -= 1
-    
+
             three_slashes = self.contains_three_slash_dox_comment.search( lines[ current_line_number ] )
-    
+
             end_block = self.contains_end_block_dox_comment.search( lines[ current_line_number ] )
             start_block = self.contains_start_block_dox_comment.search( lines[ current_line_number ] )
-    
+
             blank_line = self.is_a_blank_line.search( lines[ current_line_number ] )
-    
+
             # if we decide to keep blank lines, remove the last condition below and then uncomment the first elif
             if three_slashes is not None or block_comment:
                 comment_block.append( self.clean_line( lines[ current_line_number ] ) )
@@ -95,16 +95,15 @@ class dox_extract:
             elif end_block is not None:
                 block_comment = True
             else:
-                function = self.is_a_function.search( lines[ current_line_number ] )     
+                function = self.is_a_function.search( lines[ current_line_number ] )
                 if function is not None:
                      i_say_so = False
-                     
+
             if block_comment and start_block is not None:
-                block_comment = False    
-           
+                block_comment = False
+
         comment_block.reverse()
         if len( comment_block ):
             comment_block = self.remove_unnecessary_whitespace( comment_block )
             comment_block = self.remove_dox_symbols( comment_block )
         return ''.join(comment_block).__repr__()[1:-1].replace('"', '\\"') or (file_name + ':' + str(line_number))
-        
