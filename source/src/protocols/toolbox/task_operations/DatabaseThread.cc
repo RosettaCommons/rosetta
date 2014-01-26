bf205a9d5f358acf9f5159af48d4c052a0ac0205
Assaf Alon <assafalon@gmail.com>
2014-01-26 16:25:51 +0200

    added the task operation DatabaseThread
    ----
    An additional 25 commit messages were squashed into this commit:
    --
    added TR for final sequence before threading
    --
    housekeeping and debug TR messages  removal after the task operation works
    --
    more bug fixes
    --
    fixes
    --
    troubleshooting
    --
    Merge branch 'assaf' of github.com:RosettaCommons/main into assaf
    pull before push
    --
    add ifs to the apply
    --
    deleting unneeded stuff
    --
    edited TR massages and default values
    --
    removed "design_residues"
    --
    changed the name in the creator file
    --
    start and end res function defs
    --
    removing target sequence
    --
    linking problem
    --
    daily
    --
    author name
    --
    working copy - compiling
    --
    wrote the apply
    --
    compile fixes
    --
    designable and leave as is functions
    --
    daily update. changed the delta function to incorporate an exit
    --
    added the creator and fwd files
    --
    added the decleration files
    --
    not me
    --
    added new databasethread.cc and .hh
    --
    
    Old repository SHA1: 1fa8c78d7a40249bbe87ed1ebae68d89120144ff

---
+++ source/src/protocols/toolbox/task_operations/DatabaseThread.cc
@@ -0,0 +1,174 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/toolbox/task_operations/DatabaseThread.cc
+/// @brief  picks a sequence from database by start and end position on the pose
+/// @author Assaf Alon assafalon@gmail.com
+
+// Unit Headers
+#include <protocols/toolbox/task_operations/DatabaseThread.hh>
+#include <protocols/toolbox/task_operations/DatabaseThreadCreator.hh>
+#include <protocols/toolbox/task_operations/ThreadSequenceOperation.hh>
+
+//Package headers
+#include <utility/io/izstream.hh>
+#include <core/pose/Pose.hh>
+#include <utility/string_util.hh>
+
+// AUTO-REMOVED #include <core/pack/task/PackerTask.hh>
+#include <core/pack/task/operation/TaskOperations.hh>
+
+// Utility Headers
+#include <core/types.hh>
+#include <utility/exit.hh>
+#include <basic/Tracer.hh>
+#include <utility/vector1.hh>
+#include <utility/tag/Tag.hh>
+#include <protocols/rosetta_scripts/util.hh>
+#include <core/import_pose/import_pose.hh>
+#include <numeric/random/random.hh>
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+#include <utility/vector0.hh>
+
+static numeric::random::RandomGenerator RG( 21051979 );
+
+
+using basic::Error;
+using basic::Warning;
+static basic::Tracer TR( "protocols.toolbox.TaskOperations.DatabaseThread" );
+
+namespace protocols {
+namespace toolbox {
+namespace task_operations {
+
+using namespace core::pack::task::operation;
+using namespace std;
+
+DatabaseThread::DatabaseThread() : parent(),
+    template_file_(""),
+    database_fname_( "" ),
+    template_pose_(NULL),
+    start_res_(0),
+    end_res_(0 ),
+    allow_design_around_(true )
+{
+    design_.clear(); revert_to_template_.clear(); full_database_.clear(); designable_.clear(); leave_as_is_.clear();
+
+}
+
+DatabaseThread::~DatabaseThread() {}
+
+core::pack::task::operation::TaskOperationOP
+DatabaseThreadCreator::create_task_operation() const
+{
+	return new DatabaseThread;
+}
+
+core::pack::task::operation::TaskOperationOP DatabaseThread::clone() const
+{
+	return new DatabaseThread( *this );
+}
+
+    
+void
+DatabaseThread::apply( core::pose::Pose const & pose, core::pack::task::PackerTask & task ) const
+{
+    protocols::toolbox::task_operations::ThreadSequenceOperation thread;
+    std::string sequence(pick_sequence_from_database(pose));
+		if (!designable_.empty()) //if user entered positions to mark for design, label those with 'X'
+		    mark_designable(sequence,pose);
+        if (!leave_as_is_.empty()) //if user entered positions to avoid threading mark those with '_'
+        	mark_leave_as_is(sequence, pose);
+    thread.start_res(start_res_);
+    thread.allow_design_around(allow_design_around_);
+    TR<<"Threading the following sequence:"<<std::endl;
+    TR<<sequence<<std::endl;
+    thread.target_sequence(sequence);
+    thread.apply(pose,task);
+}
+    
+core::Size
+DatabaseThread::find_length(const core::pose::Pose &pose) const // find the lenght of the pose according to template positions - if there was an insertion or deletion along the way.
+{
+    core::Size const user_start(start_res()); //get the user defined start residue on the template
+    core::Size const nearest_to_start_on_pose( rosetta_scripts::find_nearest_res( pose, *template_pose_, user_start, 1/*chain*/ ) );
+    core::Size const user_end(end_res()); //get the user defined end residue on the template
+    core::Size const nearest_to_end_on_pose( rosetta_scripts::find_nearest_res( pose, *template_pose_, user_end, 1/*chain*/ ) );
+    int const start_end_delta( nearest_to_end_on_pose - nearest_to_start_on_pose + 1);
+    if(start_end_delta<0)
+        utility_exit_with_message("Difference between start and end residues is negative - Aborting!!!");
+    else
+        return start_end_delta;
+}
+    
+ 
+std::string
+DatabaseThread::pick_sequence_from_database( core::pose::Pose const & pose ) const{
+    core::Size const segment_length(find_length( pose ));
+    std::string line;
+    utility::vector1< std::string > sized_database;
+    foreach( std::string const line, full_database_ ) {
+        if (line.length()==segment_length){ // if length of line is the same as segment length, incorporate into vector of strings.
+            sized_database.push_back( line );
+        }
+    }
+    if (sized_database.size()==0){
+        utility_exit_with_message("no entries with correct length were found in the database" + database_fname_ );
+            }
+    TR<<"Finished reading database with "<<sized_database.size()<<" entries of length "<<segment_length<<std::endl;
+    core::Size const entry=RG.uniform() * sized_database.size() + 1;
+		TR<<"Picked the sequence:"<<std::endl;
+		TR<<sized_database[entry]<<std::endl;
+    return sized_database[entry];
+}
+
+void //mark residue positions on the pose (not positions in the sequence!) as designlable
+DatabaseThread::mark_designable(std::string & sequence, core::pose::Pose const & pose ) const{
+    foreach(core::Size const i, designable_ ){
+        core::Size const near_to_i(rosetta_scripts::find_nearest_res(pose,*template_pose_,i,1/*chain*/));
+        core::Size const position(near_to_i - start_res_ +1);
+        sequence[position]='X';
+    }
+}
+
+
+void //mark residue positions on the pose (not positions in the sequence!) to leave as in template
+    DatabaseThread::mark_leave_as_is(std::string &sequence, core::pose::Pose const & pose ) const{
+    foreach(core::Size const i,leave_as_is_){
+        core::Size const near_to_i(rosetta_scripts::find_nearest_res(pose,*template_pose_,i,1/*chain*/));
+        core::Size const position(near_to_i - start_res_ +1);
+        sequence[position]='_';
+    }
+}
+
+void
+DatabaseThread::parse_tag(TagCOP tag, DataMap &)
+{
+    template_file( tag->getOption< std::string >( "template_file") );
+   	template_pose_ = new core::pose::Pose;
+	core::import_pose::pose_from_pdb( *template_pose_, template_file_ );
+	database_fname( tag->getOption< std::string >( "database" ) );
+    utility::io::izstream database( database_fname_ );
+    if( !database )
+        utility_exit_with_message("cannot open database " + database_fname_ +"\n");
+    std::string line;
+    while( getline( database, line ) ) { // if length of line is the same as segment length, incorporate into vector of strings.
+            full_database_.push_back( line );
+    }
+	start_res( tag->getOption< core::Size >( "start_res" ) );
+	end_res( tag->getOption< core::Size >( "end_res" ) );
+	allow_design_around( tag->getOption< bool >( "allow_design_around", true ) );
+    designable(utility::string_split(tag->getOption<std::string>("design_residues",""),',',core::Size()));
+    leave_as_is(utility::string_split(tag->getOption<std::string>("keep_original_identity",""),',',core::Size()));
+}
+
+} //namespace protocols
+} //namespace toolbox
+} //namespace task_operations
---
+++ source/src/protocols/toolbox/task_operations/DatabaseThread.hh
@@ -0,0 +1,102 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file   protocols/toolbox/task_operations/DatabaseThread.hh
+/// @brief  TaskOperation class that restricts a chain to repacking
+/// @author Assaf Alon assafalon@gmail.com
+
+#ifndef INCLUDED_protocols_toolbox_task_operations_DatabaseThread_hh
+#define INCLUDED_protocols_toolbox_task_operations_DatabaseThread_hh
+
+// Unit Headers
+#include <protocols/toolbox/task_operations/DatabaseThread.fwd.hh>
+#include <protocols/toolbox/task_operations/RestrictOperationsBase.hh>
+
+// Project Headers
+#include <core/pose/Pose.fwd.hh>
+#include <core/pack/task/PackerTask.fwd.hh>
+#include <utility/tag/Tag.fwd.hh>
+
+// Utility Headers
+#include <core/types.hh>
+
+// C++ Headers
+#include <string>
+// AUTO-REMOVED #include <set>
+
+#include <utility/vector1.hh>
+
+
+namespace protocols {
+namespace toolbox {
+namespace task_operations {
+
+///@details this class is a TaskOperation to prevent repacking of residues not near an interface.
+class DatabaseThread : public RestrictOperationsBase
+{
+public:
+	typedef RestrictOperationsBase parent;
+
+	DatabaseThread();
+	DatabaseThread( std::string const seq );
+
+	virtual ~DatabaseThread();
+
+	virtual TaskOperationOP clone() const;
+
+	virtual	void apply( core::pose::Pose const &, core::pack::task::PackerTask & ) const;
+    virtual void parse_tag( TagCOP, DataMap & );
+    
+    //function declerations:
+    core::Size find_length( core::pose::Pose const & pose ) const;
+    std::string pick_sequence_from_database( core::pose::Pose const & pose ) const;
+    void mark_designable(std::string & sequence, core::pose::Pose const &) const;
+    void mark_leave_as_is(std::string & sequence, core::pose::Pose const &) const;
+    std::string pick_sequence_from_database();
+    
+    
+    //setters and getters for functions in parse my tag:
+    
+    std::string template_file() const{ return template_file_; } //getter for the template file name
+	void template_file( std::string const s ){ template_file_ = s; } // setter for the template file name
+	core::Size start_res() const {return start_res_;};
+	void start_res( core::Size const s ) { start_res_ = s; };
+	core::Size end_res() const { return end_res_; }; //getter
+	void end_res( core::Size const s ) { end_res_ = s; }; //setter
+	bool allow_design_around() const{ return allow_design_around_;}
+	void allow_design_around( bool const b ){ allow_design_around_ = b ; }
+    std::string database_fname() const{ return database_fname_; };//getter
+	void database_fname( std::string const d ){ database_fname_ = d; };//setter
+    utility::vector1<core::Size> designable() const {return designable_; };//getter
+    void designable(utility::vector1<core::Size> const vector) {designable_=vector; };//setter
+    utility::vector1<core::Size> leave_as_is() const {return leave_as_is_; };//getter
+    void leave_as_is(utility::vector1<core::Size> const vector) {leave_as_is_=vector; };//setter
+    
+private:
+    std::string template_file_;
+    std::string database_fname_;
+    core::pose::PoseOP template_pose_;
+	core::Size start_res_;
+	core::Size end_res_;
+	bool allow_design_around_; //dflt true; if false restricts rest of the pose to repakcing
+//    std::string design_residues_; //get from parse my tag a list of residues to design
+	utility::vector1<core::Size> design_;
+	utility::vector1<core::Size> revert_to_template_;
+	utility::vector1<std::string> full_database_;
+    utility::vector1<core::Size> designable_,leave_as_is_;
+
+
+
+};
+
+} //namespace protocols
+} //namespace toolbox
+} //namespace task_operations
+
+#endif // INCLUDED_protocols_toolbox_TaskOperations_DatabaseThread_HH
