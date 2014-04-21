1d7d0b165028c4265777c892739d68e06507a010
jadolfbr <jadolfbr@gmail.com>
2014-04-21 14:02:39 -0400

    fix another warning, remove options files that were re-added
    Old repository SHA1: e5ae0051d8dca6da27763a495da58c9eeeb306a2

--- source/src/protocols/antibody/AntibodyInfo.cc
+++ source/src/protocols/antibody/AntibodyInfo.cc
@@ -434,7 +434,7 @@ void AntibodyInfo::setup_CDRsInfo( pose::Pose const & pose ) {
 	int loop_start_in_pose, loop_stop_in_pose, cut_position ;
 	loopsop_having_allcdrs_ = new loops::Loops();
 
-	for (Size i=start_cdr_loop; i<=total_cdr_loops_; ++i ) {
+	for (Size i=start_cdr_loop; i<= core::Size(total_cdr_loops_); ++i ) {
 		
 		CDRNameEnum cdr = static_cast<CDRNameEnum>(i);
 		loop_start_in_pose = pose.pdb_info()->pdb2pose( chains_for_cdrs_[i], numbering_info_.cdr_numbering[i][cdr_start]->resnum());
