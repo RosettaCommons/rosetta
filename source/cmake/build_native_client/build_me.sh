ecf439a91c4353a4a914393029d5ec8703e470db
ch.norn <ch.norn@gmail.com>
2014-05-07 20:48:58 +0300

    <?xml version="1.0" encoding="utf-8"?> <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> <html>   <head>     <title>503 Connection timed out</title>   </head>   <body>     <h1>Error 503 Connection timed out</h1>     <p>Connection timed out</p>     <h3>Guru Meditation:</h3>     <p>XID: 3686270381</p>     <hr>     <p>Varnish cache server</p>   </body> </html>
    Old repository SHA1: 39ec6e58fca104c2e0009f507baddd2dfd6527c5

--- source/cmake/build_native_client/build_me.sh
+++ source/cmake/build_native_client/build_me.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# First setup nacl_sdk
+# Find instructions for that here: https://developer.chrome.com/native-client/sdk/download
+
+make clean
+
+cd ../
+python make_project.py all
+cd build_native_client 
+
+cd ../../../source/external/zlib-1.2.8/
+~/nacl_sdk/pepper_33/toolchain/mac_x86_newlib/bin/x86_64-nacl-gcc -c *.c
+~/nacl_sdk/pepper_33/toolchain/mac_x86_newlib/bin/x86_64-nacl-ar -rsc libz-static.a *.o
+mv libz-static.a ../../cmake/build_native_client/
+cd ../../cmake/build_native_client
+
 export CC=/Users/christoffernorn/nacl_sdk/pepper_33/toolchain/mac_x86_newlib/bin/x86_64-nacl-gcc
 export CXX=/Users/christoffernorn/nacl_sdk/pepper_33/toolchain/mac_x86_newlib/bin/x86_64-nacl-g++
 cmake ./
