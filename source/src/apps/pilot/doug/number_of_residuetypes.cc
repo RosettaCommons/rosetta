11c32b1582780e5bdb4f61361e91b0b67cfeb32a
P. Douglas Renfrew <renfrew@nyu.edu>
2014-06-12 18:55:16 -0400

    Adding new app and integration test that prints the number of ResidueTypes in the default FA_STANDARD and CENTROID ResidueTypeSets.
    Integration test status: [1]
    
    The new number_of_residuetypes test will "fail".
    
    Old repository SHA1: f48563fd8dcb73f3aa29446d6a6e754312cf820c

---
+++ source/src/apps/pilot/doug/number_of_residuetypes.cc
@@ -0,0 +1,55 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @file src/apps/pilot/doug/number_of_residuetypes.cc
+/// @brief Simply print the number of ResidueTypes in the default ResidueTypeSet
+/// @author P. Douglas Renfrew (renfrew@nyu.edu)
+
+// devel headers
+#include <devel/init.hh>
+
+// core headers
+#include <core/chemical/ChemicalManager.hh>
+#include <core/chemical/ResidueTypeSet.hh>
+
+// utility headers
+#include <utility/excn/Exceptions.hh>
+
+// basic headers
+#include <basic/Tracer.hh>
+
+// c++ headers
+#include <iostream>
+
+static basic::Tracer TR("apps.pilot.doug.number_of_residuetypes");
+
+int
+main( int argc, char * argv [] )
+{
+	using namespace core;
+	using namespace chemical;
+
+	try {
+
+		devel::init( argc, argv );
+
+		ResidueTypeSetCAP rts_fa_std( ChemicalManager::get_instance()->residue_type_set( FA_STANDARD ) );
+		ResidueTypeSetCAP rts_centroid( ChemicalManager::get_instance()->residue_type_set( CENTROID ) );
+
+		TR << "The FA_STANDARD ResidueTypeSet contains " << rts_fa_std->residue_types().size() << " ResidueTypes" << std::endl;
+		TR << "The CENTROID ResidueTypeSet contains " << rts_centroid->residue_types().size() << " ResidueTypes" << std::endl;
+
+		TR << "************************************d**o**n**e***********************************" << std::endl;
+
+	} catch ( utility::excn::EXCN_Base const & e ) {
+		std::cout << "caught exception " << e.msg() << std::endl;
+	}
+
+	return 0;
+}
