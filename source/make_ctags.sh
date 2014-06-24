14e3640b639b25928a517de2802f4e294bf2d327
Kevin Houlihan <khouli@gmail.com>
2014-06-24 15:36:45 -0400

    Old repository SHA1: ee2354bfc04423e07472aa3a0155e7e8172cbe08

--- source/make_ctags.sh
+++ source/make_ctags.sh
@@ -5,17 +5,7 @@
 # (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 # (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
-#First make sure that all the options files are properly built.
-#./update_options.sh
-
-# This doesn't work, passes too many arguments and errors out
-#ctags `find src/core -name "*.cc" -or -name "*.hh"`
-#ctags --append `find src/protocols -name "*.cc" -or -name "*.hh"`
-#ctags --append `find src/devel -name "*.cc" -or -name "*.hh"`
-#ctags --append `find src/utility -name "*.cc" -or -name "*.hh"`
-#ctags --append `find src/numeric -name "*.cc" -or -name "*.hh"`
-
+find ./src/{apps,core,devel,numeric,protocols,utility} ./test -name '*.cc' -o -name '*.hh' -print > tagged_files.txt
 # The -L flag requires Exuberant Ctags
-find ./src/{apps,core,devel,numeric,protocols,utility} test -name "*.cc" -o -name "*.hh" > tagged_files.txt
 ctags -L tagged_files.txt
 cscope -bqk -i tagged_files.txt
