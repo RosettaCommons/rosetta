67da3e3a4b5ac46d70b7a8c4b1eb2c9ef998230e
Sergey Lyskov <sergey.lyskov@jhu.edu>
2014-07-25 19:58:32 -0400

    Fixing Windows build
    Old repository SHA1: ba9714e8d8c9ec9132911e10759dbdc57e47b396

--- source/src/core/membrane/geometry/Embedding.cc
+++ source/src/core/membrane/geometry/Embedding.cc
@@ -65,32 +65,32 @@ Embedding::Embedding() :
 
 /// @brief	Constructs bogus object from topology
 Embedding::Embedding( SpanningTopologyOP topology ){
-    
+
     TR << "Create a bogus embedding from topology" << std::endl;
-    
+
     // initialize embedding vectors
     utility::vector1< EmbeddingDefOP > span_embeddings;
-    
+
     // initializations
     core::Vector chain_com(0, 0, 0);
     core::Real radius(8);
     core::Real increment = 360 / topology->total_spans();
-    
+
     // get position around a circle
     for ( Size i = 1; i <= topology->total_spans(); ++i ) {
         core::Real alpha = increment * i;
         core::Real x = radius * cos( alpha );
         core::Real y = radius * sin( alpha );
-        
+
         // For each TMspan, get center and normal
         core::Vector center( x, y, 0 );
         core::Vector normal(0, 0, 1);
-        
+
         // create object and push back into vector
         EmbeddingDefOP span_embedding = new EmbeddingDef( normal, center );
         span_embeddings_.push_back( span_embedding );
     }
-    
+
     // chain embedding is sum of span embeddings
     chain_embedding_ = sum_of_parts( span_embeddings_ );
 }// object from topology
@@ -98,10 +98,10 @@ Embedding::Embedding( SpanningTopologyOP topology ){
 
 /// @brief	Constructs from topology and pose
 Embedding::Embedding( SpanningTopologyOP topology, PoseOP pose ){
-    
+
     // create span embedding from topology
     span_embeddings_ = from_spans( topology, pose );
-    
+
     // calculate chain embedding, push back
     chain_embedding_ = sum_of_parts( span_embeddings_ );
 }// from topology and pose
@@ -109,28 +109,28 @@ Embedding::Embedding( SpanningTopologyOP topology, PoseOP pose ){
 
 /// @brief	Constructs from user-defined embedding
 Embedding::Embedding( SpanningTopologyOP topology, PoseOP pose, EmbeddingDefOP user_embedding ){
-    
+
     // create span embeddings from topology and pose
     span_embeddings_ = from_spans( topology, pose );
-    
+
     // calculate chain embedding
     EmbeddingDefOP computed_embedding( sum_of_parts( span_embeddings_ ) );
-    
+
     // get translation vector from (user - computed)
     core::Vector const translation_center( user_embedding->center() - computed_embedding->center() );
     core::Vector const translation_normal( user_embedding->normal() - computed_embedding->normal() );
-    
+
     // create embedding object
     EmbeddingDefOP translation = new EmbeddingDef( translation_normal, translation_center );
-    
+
     // translate span embeddings
     for ( Size i = 1; i <= span_embeddings_.size(); ++i ){
         span_embeddings_[ i ]->translate_by( translation );
     }
-    
+
     // set chain embedding
     chain_embedding_ = user_embedding;
-    
+
 }// from user-defined embedding
 
 /// @brief	Destructor
@@ -145,7 +145,7 @@ Embedding::~Embedding(){}
 void Embedding::show(){
     TR << "Chain Embedding: " << std::endl;
     chain_embedding_->show();
-    
+
     TR << "Span Embedding: " << std::endl;
     for ( Size i = 1; i <= span_embeddings_.size(); ++i ){
         TR << "			";
@@ -173,11 +173,11 @@ EmbeddingDefOP Embedding::chain_embedding(){
 
 /// @brief Compute the total embedding from multiple embeddings
 EmbeddingDefOP Embedding::sum_of_parts( utility::vector1< EmbeddingDefOP > parts ){
-    
+
     // Initialize vars
     core::Vector center(0, 0, 0);
     core::Vector normal(0, 0, 0);
-    
+
     // Compute Resulting Center
     for ( Size i = 1 ; i <= parts.size(); ++i ) {
         center += parts[i]->center();
@@ -185,7 +185,7 @@ EmbeddingDefOP Embedding::sum_of_parts( utility::vector1< EmbeddingDefOP > parts
     }
     center /= parts.size();
     normal /= parts.size();
-    
+
     // Create new embedding setup and return it
     EmbeddingDefOP embedding = new EmbeddingDef( normal, center );
     return embedding;
@@ -194,20 +194,20 @@ EmbeddingDefOP Embedding::sum_of_parts( utility::vector1< EmbeddingDefOP > parts
 
 /// @brief Compute Embedding from transmembrane spans
 utility::vector1< EmbeddingDefOP > Embedding::from_spans( SpanningTopologyOP topology, PoseOP pose ){
-    
+
     TR << "Computing membrane embedding from TMspans: " << std::endl;
     topology->show();
-    
+
     // go through TMspans
     for ( Size i = 1; i <= topology->total_spans(); ++i ){
-        
+
         // residues
         Size start( topology->span( i )->start() );
         Size end( topology->span( i )->end() );
-        
+
         // create embedding object and fill it from span
         EmbeddingDefOP embedding = new EmbeddingDef( *from_span( start, end, pose ) );
-        
+
         // add embedding to vector
         span_embeddings_.push_back( embedding );
     }
@@ -218,34 +218,34 @@ utility::vector1< EmbeddingDefOP > Embedding::from_spans( SpanningTopologyOP top
 
 /// @brief Compute emmebrane embedding from a single transmembrane span
 EmbeddingDefOP Embedding::from_span( Size start, Size end, PoseOP pose ){
-    
+
     TR << "Computing membrane embedding from TMspan " << start << " to " << end << std::endl;
-    
+
     // get CA atom positions of anchor residues
     core::Vector pos1 = pose->residue( start ).atom( 2 ).xyz();
     core::Vector pos2 = pose->residue( end ).atom( 2 ).xyz();
-    
+
     // check TMspan
-    if ( pos1.z() < 0 and pos2.z() < 0 ){
+    if ( pos1.z() < 0 && pos2.z() < 0 ){
         throw new EXCN_Illegal_Arguments("Your TMspan does not span the membrane!");
     }
-    else if ( pos1.z() > 0 and pos2.z() > 0 ){
+    else if ( pos1.z() > 0 && pos2.z() > 0 ){
         throw new EXCN_Illegal_Arguments("Your TMspan does not span the membrane!");
     }
-    
+
     // compute center
     core::Vector center = 0.5 * ( pos1 + pos2 );
-    
+
     // compute normal in direction of increasing z-axis
     core::Vector normal;
-    if ( pos1.z() > 0 and pos2.z() < 0 ){
+    if ( pos1.z() > 0 && pos2.z() < 0 ){
         normal = pos2 - pos1;
     }
     else {
         normal = pos1 - pos2;
     }
     normal.normalize();
-    
+
     // Create new embedding and return it
     EmbeddingDefOP embedding = new EmbeddingDef( normal, center );
     return embedding;
@@ -254,4 +254,3 @@ EmbeddingDefOP Embedding::from_span( Size start, Size end, PoseOP pose ){
 } // geometry
 } // membrane
 } // core
-
