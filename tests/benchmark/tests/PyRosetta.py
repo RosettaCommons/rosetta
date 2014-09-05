6756b035f3ec7446730976ea9435f14b6b80d591
Sergey Lyskov <sergey.lyskov@jhu.edu>
2014-09-04 21:52:57 -0400

    Adding PyRosetta unit tests
    ----
    An additional 1 commit messages were squashed into this commit:
    --
    Renaming PyRosetta self-tests to PyRosetta.unit. Adding PyRosetta.build test.
    --
    
    Old repository SHA1: 08479a8cfe3f0600b26f20172eddc37994970972

--- tests/benchmark/tests/PyRosetta.py
+++ tests/benchmark/tests/PyRosetta.py
@@ -20,10 +20,7 @@ imp.load_source(__name__, '/'.join(__file__.split('/')[:-1]) +  '/__init__.py') 
 _api_version_ = '1.0'  # api version
 
 
-def run_test(test, rosetta_dir, working_dir, platform, config, hpc_driver=None, verbose=False, debug=False):
-    ''' Run single test.
-        Platform is a dict-like object, mandatory fields: {os='Mac', compiler='gcc'}
-    '''
+def run_build_test(rosetta_dir, working_dir, platform, config, hpc_driver=None, verbose=False, debug=False):
     memory = config['memory']
     jobs = config['cpu_count']
 
@@ -31,25 +28,54 @@ def run_test(test, rosetta_dir, working_dir, platform, config, hpc_driver=None, 
 
     TR = Tracer(verbose)
 
-    TR('Running test: "{test}" at working_dir={working_dir!r} with rosetta_dir={rosetta_dir}, platform={platform}, jobs={jobs}, memory={memory}GB, hpc_driver={hpc_driver}...'.format( **vars() ) )
+    TR('Running PyRosetta build test: at working_dir={working_dir!r} with rosetta_dir={rosetta_dir}, platform={platform}, jobs={jobs}, memory={memory}GB, hpc_driver={hpc_driver}...'.format( **vars() ) )
 
     compiler = platform['compiler']
     extras   = ','.join(platform['extras'])
 
     command_line = 'cd {rosetta_dir}/source && BuildPyRosetta.sh -u --monolith -j{jobs}'.format(rosetta_dir=rosetta_dir, compiler=compiler, jobs=jobs, extras=extras)
 
-    buildings_path_output = execute('Getting buindings build path...', command_line + ' --print-build-path', return_='tuple')
-    buildings_path = buildings_path_output[1].split()[-1]
-    if not (buildings_path  and  os.path.isdir(buildings_path) ): raise BenchmarkError('Could not retrieve valid PyRosetta bindings binary path!\nCommand line:{}\nResult:{}\n'.format(command_line, buildings_path_output))
+    res, output = execute('Compiling...', 'cd {}/source && {}'.format(rosetta_dir, command_line), return_='tuple')
 
-    print buildings_path
-    TR('Bindings build path is:{}'.format(buildings_path))
+    if res:  res, output = execute('Compiling...', 'cd {}/source && {}'.format(rosetta_dir, command_line.format(compiler=compiler, jobs=1, extras=extras)), return_='tuple')
+
+    file(working_dir+'/build-log.txt', 'w').write(output)
+
+    res_code = _S_failed_ if res else _S_finished_
+    if not res: output = '...\n'+'\n'.join( output.split('\n')[-32:] )  # truncating log for passed builds.
+    output = 'Running: {}\n'.format(command_line) + output  # Making sure that exact command line used is stored
+    r = {_StateKey_ : res_code,  _ResultsKey_ : {},  _LogKey_ : output }
+    # makeing sure that results could be serialize in to json, but ommiting logs because they could take too much space
+    json.dump({_ResultsKey_:r[_ResultsKey_], _StateKey_:r[_StateKey_]}, file(working_dir+'/output.json', 'w'), sort_keys=True, indent=2)
+
+    return r
+
+
+def run_unit_tests(rosetta_dir, working_dir, platform, config, hpc_driver=None, verbose=False, debug=False):
+    memory = config['memory']
+    jobs = config['cpu_count']
+
+    jobs = jobs if memory/jobs >= 1.0 else max(1, int(memory) )  # PyRosetta builds require at least 1Gb per memory per thread
+
+    TR = Tracer(verbose)
+
+    TR('Running PyRosetta unit tests: at working_dir={working_dir!r} with rosetta_dir={rosetta_dir}, platform={platform}, jobs={jobs}, memory={memory}GB, hpc_driver={hpc_driver}...'.format( **vars() ) )
+
+    compiler = platform['compiler']
+    extras   = ','.join(platform['extras'])
+
+    command_line = 'cd {rosetta_dir}/source && BuildPyRosetta.sh -u --monolith -j{jobs}'.format(rosetta_dir=rosetta_dir, compiler=compiler, jobs=jobs, extras=extras)
 
     res, output = execute('Compiling...', 'cd {}/source && {}'.format(rosetta_dir, command_line), return_='tuple')
 
     if res:  res, output = execute('Compiling...', 'cd {}/source && {}'.format(rosetta_dir, command_line.format(compiler=compiler, jobs=1, extras=extras)), return_='tuple')
 
     file(working_dir+'/build-log.txt', 'w').write(output)
+
+    buildings_path_output = execute('Getting buindings build path...', command_line + ' --print-build-path', return_='tuple')
+    buildings_path = buildings_path_output[1].split()[-1]
+    if not (buildings_path  and  os.path.isdir(buildings_path) ): raise BenchmarkError('Could not retrieve valid PyRosetta bindings binary path!\nCommand line:{}\nResult:{}\n'.format(command_line, buildings_path_output))
+    TR('Bindings build path is:{}'.format(buildings_path))
 
     if not res: res, output = execute('Running PyRosetta tests...', 'cd {buildings_path} && python TestBindings.py'.format(buildings_path=buildings_path), return_='tuple')
 
@@ -63,6 +89,11 @@ def run_test(test, rosetta_dir, working_dir, platform, config, hpc_driver=None, 
     return r
 
 
+
 def run(test, rosetta_dir, working_dir, platform, config, hpc_driver=None, verbose=False, debug=False):
-    if not test: return run_test('', rosetta_dir, working_dir, platform, config=config, hpc_driver=hpc_driver, verbose=verbose, debug=debug)
+    ''' Run single test.
+        Platform is a dict-like object, mandatory fields: {os='Mac', compiler='gcc'}
+    '''
+    if   test =='build': return run_build_test(rosetta_dir, working_dir, platform, config=config, hpc_driver=hpc_driver, verbose=verbose, debug=debug)
+    elif test =='unit':  return run_unit_tests(rosetta_dir, working_dir, platform, config=config, hpc_driver=hpc_driver, verbose=verbose, debug=debug)
     else: raise BenchmarkError('Unknow PyRosetta test: {}!'.format(test))
