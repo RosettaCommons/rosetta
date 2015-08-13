62cb37f08016ba2cf6c1f1eb9b2e6bd0090a56bb
Sergey Lyskov <sergey.lyskov@gmail.com>
2015-08-13 09:56:25 -0600

    Merge pull request #706 from RosettaCommons/sergey/f
    Adjusting CMake config: adding CXX11 build, making default Clang build on Mac to use -std=c++11 to match our scons settings.
    
    Old repository SHA1: 3b059b6f2b9bf66ba7a334b32c6e8bfbb09a0d52

--- source/cmake/build/build.settings.cmake
+++ source/cmake/build/build.settings.cmake
@@ -1,21 +1,21 @@
-# The settings below are combined together to create the final set of settings 
-# for CMake + Ninja builds. These settings attempt to match those found in 
-# tools/build/basic.settings, which are used for building with scons. 
+# The settings below are combined together to create the final set of settings
+# for CMake + Ninja builds. These settings attempt to match those found in
+# tools/build/basic.settings, which are used for building with scons.
 #
 # Currently, this file only includes settings for CMake + Ninja release, debug,
-# and graphics builds. This file is included in the CMakeLists.txt files found 
-# in the build_release, build_debug, and build_graphics directories. The 
-# CMakeLists.txt files only need to specify which mode (i.e., release, debug), 
+# and graphics builds. This file is included in the CMakeLists.txt files found
+# in the build_release, build_debug, and build_graphics directories. The
+# CMakeLists.txt files only need to specify which mode (i.e., release, debug),
 # extras (i.e., graphics), and compiler (i.e., gcc, clang) should be used. The
-# appropriate settings for each mode, extras, and compiler should be 
+# appropriate settings for each mode, extras, and compiler should be
 # implemented here.
 #
-# Ultimately, it may be more efficient to automate the generation of this 
-# build.settings.cmake file so that it does not have to be updated manually 
-# every time basic.settings is modified. 
+# Ultimately, it may be more efficient to automate the generation of this
+# build.settings.cmake file so that it does not have to be updated manually
+# every time basic.settings is modified.
 #
 # @file source/cmake/build/build.settings.cmake
-# @author Caleb W. Geniesse (calebgeniesse@stanford.edu) 
+# @author Caleb W. Geniesse (calebgeniesse@stanford.edu)
 
 
 ###########################################################################
@@ -149,6 +149,41 @@ if( ${COMPILER} STREQUAL "clang" )
 	)
 endif()
 
+
+if(APPLE AND ${COMPILER} STREQUAL "clang")
+    list( REMOVE_ITEM compile
+	-stdlib=libstdc++
+    )
+    list( REMOVE_ITEM shlink
+	-stdlib=libstdc++
+    )
+    list( REMOVE_ITEM link
+	-stdlib=libstdc++
+    )
+
+    REMOVE_DEFINITIONS(-DPTR_MODERN)
+    REMOVE_DEFINITIONS(-DPTR_BOOST)
+
+    ADD_DEFINITIONS(-DCXX11)
+    ADD_DEFINITIONS(-DPTR_STD)
+
+    set( cxx
+          -std=c++11
+          -isystem external/boost_1_55_0/
+          -isystem external/include/
+          -isystem external/dbio/
+	  -stdlib=libc++
+    )
+    set( shlink
+	  -stdlib=libc++
+    )
+    set( link
+	  -stdlib=libc++
+    )
+
+endif()
+
+
 # modes ###################################################################
 
 # "clang, debug"
@@ -181,6 +216,46 @@ endif()
 ###########################################################################
 
 if( EXTRAS )
+
+	# cxx11"
+	if( ${EXTRAS} STREQUAL "cxx11" )
+	    set( cc
+			    #-std=c99
+			    -isystem external/boost_1_55_0/
+			    -isystem external/include/
+			    -isystem external/dbio/
+	    )
+	    set( cxx
+			    -std=c++11
+			    -isystem external/boost_1_55_0/
+			    -isystem external/include/
+			    -isystem external/dbio/
+
+	    )
+	    set( compile
+			    -pipe
+			    -Qunused-arguments
+			    -DUNUSUAL_ALLOCATOR_DECLARATION
+			    -ftemplate-depth-256
+			    -stdlib=libstdc++
+    			    -fPIC
+			    -DBOOST_ERROR_CODE_HEADER_ONLY
+			    -DBOOST_SYSTEM_NO_DEPRECATED
+			    -I /usr/include
+			    -I /usr/local/include
+			    -I src
+			    -I src/platform/macos/64/clang/6.1
+			    -I src/platform/macos/64/clang
+			    -I src/platform/macos/64
+    	    )
+	    set( shlink
+			    -stdlib=libstdc++
+	    )
+	    set( link
+			    -stdlib=libstdc++
+	    )
+	endif()
+
 
 	# "macos, graphics"
 	if( APPLE AND ${EXTRAS} STREQUAL "graphics" )
@@ -223,7 +298,7 @@ if( EXTRAS )
 				-Werror=enum-compare
 		)
 	endif()
-	
+
 	# "linux, graphics"
 	if( UNIX AND ${EXTRAS} STREQUAL "graphics" )
 		list( APPEND defines
