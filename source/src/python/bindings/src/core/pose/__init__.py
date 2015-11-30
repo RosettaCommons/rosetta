32cda9104bed9924808bf2937aab2041e23722f3
Sergey Lyskov <sergey.lyskov@jhu.edu>
2015-11-30 18:18:20 -0500

    Fixing PyRosetta imports in namespace build
    Old repository SHA1: b1658077a18ea220b6b9d2e0e9a0b8d56c1239e3

--- source/src/python/bindings/src/core/pose/__init__.py
+++ source/src/python/bindings/src/core/pose/__init__.py
@@ -1,4 +1,5 @@
 from _core_pose_ import *
+import rosetta.core.pose.datacache
 
 from cStringIO import StringIO
 
@@ -42,7 +43,7 @@ def __pose_setstate__(self, state):
         self.pdb_info().name(state["name"])
 
 def __pose_reduce_ex__(self, protocol_level):
-    """Pose reduce implementation.""" 
+    """Pose reduce implementation."""
     return (Pose, tuple(), self.__getstate__())
 
 # Add pickle support to pose objects.
