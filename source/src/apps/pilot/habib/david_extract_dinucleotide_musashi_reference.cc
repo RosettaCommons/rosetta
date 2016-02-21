2ba0127fcfd3d342d692534a7604a5922642b2b9
David Johnson <habib@Davids-MacBook-Pro-2.local>
2016-02-21 12:03:58 -0600

    Added RRM dinucleotide pair utilities
    Old repository SHA1: 06a1671bf846d9d5efee31da1c161bfa9b7aacd4

---
+++ source/src/apps/pilot/habib/david_extract_dinucleotide_musashi_reference.cc
@@ -0,0 +1,367 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @brief
+/// @author jk + dj
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+#include <devel/init.hh>
+#include <core/pose/Pose.hh>
+#include <core/chemical/AA.hh>
+#include <core/chemical/ResidueTypeSet.hh>
+#include <core/select/residue_selector/ResidueSelector.hh>
+#include <core/conformation/Residue.hh>
+#include <core/conformation/ResidueFactory.hh>
+#include <basic/MetricValue.hh>
+#include <devel/init.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <core/pack/task/PackerTask.hh>
+#include <core/pack/pack_rotamers.hh>
+#include <core/scoring/TwelveANeighborGraph.hh>
+#include <core/pose/PDBInfo.hh>
+#include <core/scoring/rms_util.tmpl.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+#include <core/scoring/Energies.hh>
+#include <core/scoring/EnergyGraph.hh>
+
+#include <basic/options/util.hh>
+#include <basic/options/option.hh>
+#include <basic/options/after_opts.hh>
+#include <basic/options/option_macros.hh>
+#include <basic/Tracer.hh>
+#include <core/scoring/Energies.hh>
+
+#include <core/scoring/rms_util.hh>
+
+#include <core/pose/metrics/CalculatorFactory.hh>
+#include <numeric/random/random.hh>
+#include <core/pose/metrics/simple_calculators/SasaCalculatorLegacy.hh>
+
+
+// Utility Headers
+#include <utility/vector1.hh>
+#include <utility/io/ozstream.hh>
+#include <string>
+#include <ObjexxFCL/string.functions.hh>
+
+#include <basic/options/keys/constraints.OptionKeys.gen.hh>
+#include <basic/options/keys/out.OptionKeys.gen.hh>
+
+//Auto Headers
+#include <core/import_pose/import_pose.hh>
+
+#include <utility/excn/Exceptions.hh>
+
+
+using namespace core;
+using namespace core::scoring;
+using namespace basic::options;
+using namespace basic::options::OptionKeys;
+
+static THREAD_LOCAL basic::Tracer TR( "apps.pilot.david_recompute_score_and_rmsd.main" );
+
+
+OPT_KEY( String, input_TMalignment )
+
+  //set to store pdb info keys
+  std::vector<std::string> surface;
+  std::set <std::string> interface;
+
+
+  /// General testing code
+  int
+main( int argc, char * argv [] )
+{
+  try {
+
+    std::vector<std::string> surface;
+    std::set <std::string> interface;
+    NEW_OPT ( input_TMalignment, "File name specifying the alignment from the last 3 lines of output of TMalign","");
+
+    using namespace core;
+    using namespace core::scoring;
+
+    devel::init(argc, argv);
+    pose::Pose pose;
+    std::string const input_pdb_name ( basic::options::start_file() );
+    core::import_pose::pose_from_file( pose, input_pdb_name );
+
+    std::string const aln_file = option[ input_TMalignment ];
+    if (aln_file.length() == 0){
+      std::cerr<<"Error: No TMalignment file specified"<<std::endl;
+      return -102;
+    }
+    std::ifstream ifs(aln_file.c_str(), std::ifstream::in);
+    if (!ifs.is_open()){
+      std::cerr<< "Error opening TMalignment file "<<std::endl;
+      return -100;
+    }
+
+    std::string target_aln;
+    if (ifs.good()){
+      ifs >> target_aln;
+      std::cout<<target_aln<<std::endl;
+    }else{
+      std::cerr<< "Error reading TMalignment file"<<std::endl;
+      return -101;
+    }
+    std::string musashi_aln;
+    if (ifs.good()){
+      ifs.ignore(1000, '\n');
+      ifs.ignore(1000, '\n');
+    }else{
+      std::cerr<< "Error reading TMalignment file"<<std::endl;
+      return -101;
+    }
+    if (ifs.good()){
+      ifs >> musashi_aln;
+      std::cout<<musashi_aln<<std::endl;
+    }else{
+      std::cerr<< "Error reading TMalignment file"<<std::endl;
+      return -101;
+    }
+
+    char *target_caln = new char[target_aln.length()+1];
+    char *musashi_caln = new char[musashi_aln.length()+1];
+    std::strcpy(target_caln, target_aln.c_str());
+    std::strcpy(musashi_caln, musashi_aln.c_str());
+
+    int pos1=-1,pos2=-1,pos3=-1;
+    int pos=1;
+    int tpos=1;
+    for (int i=0; i< (int)musashi_aln.length(); ++i){
+      if (pos == 4 && pos1== -1){
+        if (musashi_caln[i]!='F'){
+          std::cerr<<"Error: alignment should be a F, not an "<<musashi_caln[i]<<std::endl;
+          return -105;
+        }
+        pos1=tpos;
+        if (target_caln[i]=='-'){
+          std::cerr<<"Error: alignment at B1-2 is a gap"<<std::endl;
+          return -108;
+        }
+
+      }
+
+      if (pos == 44 && pos2== -1){
+        if (musashi_caln[i]!='F'){
+          std::cerr<<"Error: alignment should be a F, not an "<<musashi_caln[i]<<std::endl;
+          return -106;
+        }
+        pos2=tpos;
+        if (target_caln[i]=='-'){
+          std::cerr<<"Error: alignment at B3-3 is a gap"<<std::endl;
+          return -109;
+        }
+      }
+
+      if (pos == 46 && pos3== -1){
+        if (musashi_caln[i]!='F'){
+          std::cerr<<"Error: alignment should be a F, not an "<<musashi_caln[i]<<std::endl;
+          return -107;
+        }
+        pos3=tpos;
+        if (target_caln[i]=='-'){
+          std::cerr<<"Error: alignment at B3-5 is a gap"<<std::endl;
+          return -110;
+        }
+      }
+
+      if (musashi_caln[i]!='-'){
+        pos++;
+      }
+      if (target_caln[i]!='-'){
+        tpos++;
+      }
+    }
+
+    int res1=-1,res2=-1,res3=-1;
+    pos=1;
+    for (int i=1; i <= (int)pose.total_residue(); i++){
+      if (pose.residue(i).is_protein()){
+        if (pos==pos1 && res1==-1){
+          res1=i;
+        }
+        if (pos==pos2 && res2==-1){
+          res2=i;
+        }
+        if (pos==pos3 && res3==-1){
+          res3=i;
+        }
+        pos++;
+      }
+    }
+
+    std::cout<<"Residue 1 "<<pose.pdb_info()->chain(res1) << pose.pdb_info()->number(res1)<<std::endl;
+    std::cout<<"Residue 2 "<<pose.pdb_info()->chain(res2) << pose.pdb_info()->number(res2)<<std::endl;
+    std::cout<<"Residue 3 "<<pose.pdb_info()->chain(res3) << pose.pdb_info()->number(res3)<<std::endl;
+
+    scoring::ScoreFunctionOP scorefxn( get_score_function() );
+    (*scorefxn)(pose);
+
+    Size rpos1=0, rpos2=0;
+    Real score1=1, score2=1;
+
+    EnergyGraph & energy_graph(pose.energies().energy_graph());
+    for (int i=1; i <= (int)pose.total_residue(); i++){
+
+      if (pose.residue(i).is_RNA()||pose.residue(i).is_DNA()){
+
+        Size cpos=0;
+        Real cscore=1;
+        for ( graph::Graph::EdgeListIter
+            iru  = energy_graph.get_node( i )->lower_edge_list_begin(),
+            irue = energy_graph.get_node( i )->lower_edge_list_end();
+            iru != irue; ++iru ) {
+          EnergyEdge * edge( static_cast< EnergyEdge *> (*iru) );
+          Size const j( edge->get_first_node_ind() );
+          if (!((int)j==res1 || (int)j==res2 || (int)j==res3)){
+            continue;
+          }
+
+          // the pair energies cached in the link
+          EnergyMap const & emap( edge->fill_energy_map());
+          Real const attr( emap[ fa_atr ] );
+          //TR<<"\n"<<j<<": "<<attr<<"\n";
+          if (attr < -1.){
+            if (pose.residue(j).is_protein()){
+              if (attr < cscore){
+                cpos=i;
+                cscore=attr;
+              }
+              std::cout<<"Score "<<i<<" "<<pose.pdb_info()->chain(i) << pose.pdb_info()->number(i)<<" "<<j<<" "<<pose.pdb_info()->chain(j) << pose.pdb_info()->number(j)<<" "<<attr<<std::endl;
+            }
+          }
+        }
+        if (cscore < score1){
+          rpos2=rpos1;
+          score2=score1;
+          rpos1=cpos;
+          score1=cscore;
+        }else if (cscore<score2){
+          rpos2=cpos;
+          score2=cscore;
+        }
+      }
+    }
+
+    for (int i=1; i <= (int)pose.total_residue(); i++){
+      Size cpos=0;
+      Real cscore=1;
+      //std::cout<<i<<" "<<res1<<" "<<res2<<" "<<res3<<std::endl;
+      if (pose.residue(i).is_protein()){
+        if (!(i == res1 || i == res2 ||i == res3)){
+          continue;
+        }
+
+        for ( graph::Graph::EdgeListIter
+            iru  = energy_graph.get_node( i )->lower_edge_list_begin(),
+            irue = energy_graph.get_node( i )->lower_edge_list_end();
+            iru != irue; ++iru ) {
+          EnergyEdge * edge( static_cast< EnergyEdge *> (*iru) );
+          Size const j( edge->get_first_node_ind() );
+          if (!(pose.residue(j).is_RNA() || pose.residue(j).is_DNA())){
+            continue;
+          }
+
+          // the pair energies cached in the link
+          EnergyMap const & emap( edge->fill_energy_map());
+          Real const attr( emap[ fa_atr ] );
+          //TR<<"\n"<<j<<": "<<attr<<"\n";
+          if (attr < -1.){
+            if (attr < cscore){
+              cpos=j;
+              cscore=attr;
+            }
+            std::cout<<"Score "<<i<<" "<<pose.pdb_info()->chain(i) << pose.pdb_info()->number(i)<<" "<<j<<" "<<pose.pdb_info()->chain(j) << pose.pdb_info()->number(j)<<" "<<attr<<std::endl;
+          }
+        }
+      }
+      if (cscore < score1){
+        rpos2=rpos1;
+        score2=score1;
+        rpos1=cpos;
+        score1=cscore;
+      }else if (cscore<score2){
+        rpos2=cpos;
+        score2=cscore;
+      }
+    }
+
+    if (rpos1>0 || rpos2>0){
+      if (rpos1<rpos2){
+        if (rpos1>1){
+          pose.conformation().delete_residue_range_slow(1,rpos1-1); 
+        }
+        if (rpos2>rpos1+1){
+          pose.conformation().delete_residue_range_slow(2, rpos2-rpos1);
+        }
+        if (pose.total_residue()>2){
+          pose.conformation().delete_residue_range_slow(3, pose.total_residue());
+        }
+      }else{
+        if (rpos2>1){
+          pose.conformation().delete_residue_range_slow(1,rpos2-1); 
+        }
+        if (rpos1>rpos2+1){
+          pose.conformation().delete_residue_range_slow(2, rpos1-rpos2);
+        }
+        if (pose.total_residue()>2){
+          pose.conformation().delete_residue_range_slow(3, pose.total_residue());
+        }
+
+      }
+      if (pose.total_residue()>0){
+        std::stringstream dnpfilename;
+        if (pose.total_residue()>1){
+          dnpfilename<<"dinucleotide_pair.pdb";
+        }else{
+          dnpfilename<<"single_nucleotide.pdb";
+        }
+        std::filebuf fb;
+        fb.open(dnpfilename.str().c_str(), std::ios::out);
+        std::ostream os(&fb);
+        //pose.dump_pdb("test.pdb");
+
+        Size acount=1;
+        for (Size i=1; i<= pose.total_residue();++i){
+          core::conformation::Residue const & rsd( pose.conformation().residue(i) );
+          for (Size j=1; j<= rsd.natoms(); ++j){
+            if (rsd.atom_name(j).compare(" P  ")&&rsd.atom_name(j).compare(" OP2")&&rsd.atom_name(j).compare(" OP1")&&rsd.atom_name(j).compare(" O5'")&&rsd.atom_name(j).compare(" C5'")&&rsd.atom_name(j).compare(" C4'")&&rsd.atom_name(j).compare(" O4'")&&rsd.atom_name(j).compare(" C3'")&&rsd.atom_name(j).compare(" O3'")&&rsd.atom_name(j).compare(" C2'")&&rsd.atom_name(j).compare(" H5'")&&rsd.atom_name(j).compare("H5''")&&rsd.atom_name(j).compare(" H4'")&&rsd.atom_name(j).compare(" H3'")&&rsd.atom_name(j).compare("H2''")&&rsd.atom_name(j).compare(" H2'")&&rsd.atom_name(j).compare("HO2'")&&rsd.atom_name(j).compare(" H1'")&&rsd.atom_name(j).compare(" O2'")){
+              os<<"ATOM  "<<std::setw(5)<<acount;
+              os<<" "<<rsd.atom_name(j)<<rsd.name3()<<"  A"<<std::setw(4)<<i;
+              os<<"    "<<std::setw(8)<<std::fixed<<std::setprecision(3)<<rsd.atom(j).xyz()(1)<<std::setw(8)<<std::fixed<<std::setprecision(3)<<rsd.atom(j).xyz()(2)<<std::setw(8)<<std::fixed<<std::setprecision(3)<<rsd.atom(j).xyz()(3);
+              os<<"  1.00  0.00           "<<rsd.atom_type(j).element()<<std::endl;
+              acount++;
+            }
+          }
+        }
+        fb.close();
+      }
+
+    }
+
+
+  } catch ( utility::excn::EXCN_Base const & e ) {
+    std::cout << "caught exception " << e.msg() << std::endl;
+    return -1;
+  }
+
+  return 0;
+
+}
+
+
+
---
+++ source/src/apps/pilot/habib/david_extract_domain.cc
@@ -0,0 +1,265 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
+
+/// @brief
+/// @author jk + dj
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+#include <devel/init.hh>
+#include <core/pose/Pose.hh>
+#include <core/chemical/AA.hh>
+#include <core/chemical/ResidueTypeSet.hh>
+#include <core/select/residue_selector/ResidueSelector.hh>
+#include <core/conformation/Residue.hh>
+#include <core/conformation/ResidueFactory.hh>
+#include <basic/MetricValue.hh>
+#include <devel/init.hh>
+#include <core/import_pose/import_pose.hh>
+#include <core/pack/task/TaskFactory.hh>
+#include <core/pack/task/PackerTask.hh>
+#include <core/pack/pack_rotamers.hh>
+#include <core/scoring/TwelveANeighborGraph.hh>
+#include <core/pose/PDBInfo.hh>
+#include <core/scoring/rms_util.tmpl.hh>
+#include <core/scoring/ScoreFunction.hh>
+#include <core/scoring/ScoreFunctionFactory.hh>
+
+#include <basic/options/util.hh>
+#include <basic/options/option.hh>
+#include <basic/options/after_opts.hh>
+#include <basic/options/option_macros.hh>
+#include <basic/Tracer.hh>
+#include <core/scoring/Energies.hh>
+
+#include <core/scoring/rms_util.hh>
+
+#include <core/pose/metrics/CalculatorFactory.hh>
+#include <numeric/random/random.hh>
+#include <core/pose/metrics/simple_calculators/SasaCalculatorLegacy.hh>
+
+
+// Utility Headers
+#include <utility/vector1.hh>
+#include <utility/io/ozstream.hh>
+#include <string>
+#include <ObjexxFCL/string.functions.hh>
+
+#include <basic/options/keys/constraints.OptionKeys.gen.hh>
+#include <basic/options/keys/out.OptionKeys.gen.hh>
+
+//Auto Headers
+#include <core/import_pose/import_pose.hh>
+
+#include <utility/excn/Exceptions.hh>
+
+
+using namespace core;
+using namespace core::scoring;
+using namespace basic::options;
+using namespace basic::options::OptionKeys;
+
+static THREAD_LOCAL basic::Tracer TR( "apps.pilot.david_recompute_score_and_rmsd.main" );
+
+
+OPT_KEY( Integer, startCut )
+OPT_KEY( Integer, endCut )
+OPT_KEY( Integer, domainNum )
+OPT_KEY( String, chain )
+OPT_KEY( String, input_complex )
+
+//set to store pdb info keys
+std::vector<std::string> surface;
+std::set <std::string> interface;
+
+
+/// General testing code
+int
+main( int argc, char * argv [] )
+{
+	try {
+
+std::vector<std::string> surface;
+std::set <std::string> interface;
+  NEW_OPT ( startCut, "Starting position of domain to be extracted",0);
+  NEW_OPT ( endCut, "Ending position of domain to be extracted",0);
+  NEW_OPT ( domainNum, "Number to append to output_tag for domain name",0);
+  NEW_OPT ( chain, "Chain to extract the domain from","");
+  NEW_OPT ( input_complex, "File name specifying the protein-RNA complex (the other PDB only has the specified chain","");
+
+  using namespace core;
+  using namespace core::scoring;
+
+  devel::init(argc, argv);
+  pose::Pose whole_pose;
+  pose::Pose whole_pose_complex;
+  pose::Pose chain_pose;
+  std::string const input_pdb_name ( basic::options::start_file() );
+  core::import_pose::pose_from_file( chain_pose, input_pdb_name );
+  std::string const complex_pdb_name ( option[input_complex] );
+  core::import_pose::pose_from_file( whole_pose, complex_pdb_name );
+  core::import_pose::pose_from_file( whole_pose_complex, complex_pdb_name );
+
+  core::Size const start = (core::Size)option[ startCut ];
+  core::Size const end = (core::Size)option[ endCut ];
+  core::Size const domain = (core::Size)option[ domainNum ];
+  std::string const chain_string = option[ chain ];
+  if ( start <= 0 ){
+    std::cerr<< "Error: startCut must be greater than 0"<<std::endl;
+    return -100;
+  }
+  if ( start >= end ){
+    std::cerr<< "Error: endCut must be greater than startCut"<<std::endl;
+    return -101;
+  }
+  if (chain_string.length() != 1){
+    std::cerr<<"Error: invalid chain specified"<<std::endl;
+    return -102;
+  }
+  char chain = chain_string.c_str()[0];
+  Size chain_count=0;
+  for( Size i = 1; i <= chain_pose.total_residue(); ++i){
+    if (chain_pose.residue(i).is_protein()){
+      chain_count++;
+    }
+  }
+  if (start>= chain_count){
+    std::cerr<< "Error: startCut must be less than the total residues in the chain"<<std::endl;
+    return -103;
+  }
+  if (end > chain_count){
+    std::cerr<< "Error: endCut must be less than the total residues in the chain"<<std::endl;
+    return -104;
+  }
+
+  bool found = false;
+  for( Size i = 1; i <= whole_pose.total_residue(); ++i){
+    std::cout<<i<<":"<<whole_pose.total_residue()<<" ";
+    if (!found && whole_pose.pdb_info()->chain(i) == chain){
+      found=true;
+      whole_pose_complex.conformation().delete_residue_range_slow(i+start-1, i+end-1);
+      std::cout<<i<<"\n";
+      for (Size j=1;j<start;j++){
+      std::cout<<j<<"\n";
+        whole_pose.conformation().delete_residue_slow(i);
+      }
+      std::cout<<i<<"\n";
+      i+=(end-start);
+      std::cout<<i<<"\n";
+      continue;
+    }
+    //if (whole_pose.pdb_info()->chain(i) != chain && !whole_pose.residue(i).is_RNA()){
+    if (!whole_pose.residue(i).is_RNA()){
+      whole_pose.conformation().delete_residue_slow(i);
+      i--;
+    }
+  }
+  std::cout<<"printing stuff\n";
+  std::stringstream domfilename;
+  domfilename<<option[ OptionKeys::out::output_tag ]()<<"_D"<<domain<<".pdb";
+  whole_pose.dump_pdb(domfilename.str()); 
+  
+  std::cout<<"deleting stuff\n";
+  if (end - start+1 == chain_pose.total_residue()){
+    chain_pose.clear();
+  }else{
+    chain_pose.conformation().delete_residue_range_slow(start, end);
+  }
+
+  std::cout<<"printing stuff\n";
+  std::stringstream leftfilename;
+  leftfilename<<option[ OptionKeys::out::output_tag ]()<<"_leftover.pdb";
+  chain_pose.dump_pdb(leftfilename.str()); 
+
+  std::stringstream leftcompfilename;
+  leftcompfilename<<option[ OptionKeys::out::output_tag ]()<<"_leftover_complex.pdb";
+  std::cout<<"printing stuff\n";
+  whole_pose_complex.dump_pdb(leftcompfilename.str()); 
+
+/*    while (ifs.good()){
+      std::string intres;
+      ifs >> intres;
+      if (intres.length() >0){
+        if (intres[0]!= '>'){
+          seq.append(intres);
+        }
+      }
+    }
+  }
+
+  if (seq.length()==0){
+      std::cout<< "Fasta file contains no sequence: "<<ffilename<<std::endl;
+      return -101;
+
+  }
+
+  int last = -100;
+  core::chemical::ResidueTypeSet const & rsd_set( pose.residue(1).residue_type_set() );
+
+  for( Size i = 1; i <= pose.total_residue(); ++i){
+    if (last == -100){
+      last = pose.pdb_info()->number(i);
+      std::cout<<i<<" "<< pose.pdb_info()->number(i)<<" "<<pose.residue(i).name1()<<"\n" ;
+      continue;
+    }
+    if (last > pose.pdb_info()->number(i)+1){
+      for (int j = last-1; j > pose.pdb_info()->number(i); j--){
+        std::cout<<seq[j-1]<<"\n";
+        core::chemical::ResidueTypeCOP new_rsd_type( core::chemical::ResidueSelector().set_name1( seq[j-1] ).exclude_variants().select( rsd_set )[1] );
+        core::conformation::ResidueOP new_rsd( core::conformation::ResidueFactory::create_residue( *new_rsd_type ) );
+        pose.append_polymer_residue_after_seqpos(*new_rsd, i, false );
+        pose.pdb_info()->number(i+2, pose.pdb_info()->number(i)-1);
+        pose.pdb_info()->chain(i+2, pose.pdb_info()->chain(i));
+      }
+
+    }else if (last < pose.pdb_info()->number(i)-1){
+      for (int j = last+1; j < pose.pdb_info()->number(i); j++){
+        std::cout<<seq[j-1]<<"\n";
+        core::chemical::ResidueTypeCOP new_rsd_type( core::chemical::ResidueSelector().set_name1( seq[j-1] ).exclude_variants().select( rsd_set )[1] );
+        core::conformation::ResidueOP new_rsd( core::conformation::ResidueFactory::create_residue( *new_rsd_type ) );
+        pose.append_polymer_residue_after_seqpos(*new_rsd, i-1, false );
+        pose.pdb_info()->number(i, pose.pdb_info()->number(i-1)+1);
+        pose.pdb_info()->chain(i, pose.pdb_info()->chain(i-1));
+        i++;
+      }
+
+
+    }
+    std::cout<<i<<" "<< pose.pdb_info()->number(i)<<" "<<pose.residue(i).name1()<<"\n" ;
+    last = pose.pdb_info()->number(i);
+  }
+
+  for( Size i = 1; i <= pose.total_residue(); ++i){
+    std::cout<<pose.pdb_info()->number(i)<<"\n";
+  }
+  for( int i = 1; i <pose.pdb_info()->number(1); ++i){
+    std::cout<<" ";
+  }
+  for( Size i = 1; i <= pose.total_residue(); ++i){
+    std::cout<<pose.residue(i).name1();
+	}
+  std::cout<<"\n"<<seq<<"\n";
+
+  pose.pdb_info()->obsolete(false);
+  pose.dump_pdb("test.pdb");
+*/
+	} catch ( utility::excn::EXCN_Base const & e ) {
+		std::cout << "caught exception " << e.msg() << std::endl;
+		return -1;
+	}
+
+  return 0;
+
+}
+
+
+
