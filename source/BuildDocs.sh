2de45c2c432549adac5a74fd44f4a1aa632dee34
Oriel Goldstein <oriel.goldstein@mail.huji.ac.il>
2016-02-29 13:50:49 +0200

    Added a flag for building the public docs.
    By default, doxygen is built with doxygen_devel.*
    
    Old repository SHA1: 9a2d27e0034f02760875cbb966ae97cbe04b152c

--- source/BuildDocs.sh
+++ source/BuildDocs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RELEASE_MODE='devel'
+
+[ "$1" == "--public" ] && RELEASE_MODE='release'
+
 echo 'Updating Rosetta options system...'
 
 ./update_options.sh
@@ -8,14 +12,15 @@ echo 'Updating ResidueType enum files...'
 
 ./update_ResidueType_enum_files.sh
 
-echo 'Building documentation for each src/doxygen.*...'
+echo "Building documentation for each src/doxyfile_${RELEASE_MODE}.*..."
 
 mkdir html
 cp src/index.html html/
 
 rm -rf html/core+protocols html/all_else
 
-for file in src/doxyfile_devel.* ; do
+for file in src/doxyfile_${RELEASE_MODE}.* ; do
     echo "Building doc: $file"
     doxygen $file
 done
+
