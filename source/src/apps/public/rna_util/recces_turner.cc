1d0ebc7098739592bd069d966dbbd939f4a3a6de
Andrew Watkins <andy.watkins2@gmail.com>
2016-10-21 19:56:24 -0700

    Merge pull request #1738 from RosettaCommons/everyday847/legacy_recces
    Restore legacy RECCES function
    
    Old repository SHA1: d86bb012bfabcde46f43d02b2ec518c1ceb05421

--- source/src/apps/public/rna_util/recces_turner.cc
+++ source/src/apps/public/rna_util/recces_turner.cc
@@ -8,7 +8,7 @@
 // (c) addressed to University of Washington CoMotion, email: license@uw.edu.
 
 /// @file
-/// @brief
+/// @brief Legacy version of recces app, to be merged with thermal_sampler by December 2016
 
 // libRosetta headers
 #include <core/init/init.hh>
@@ -27,8 +27,6 @@
 #include <protocols/stepwise/modeler/rna/helix/RNA_HelixAssembler.hh>
 #include <protocols/stepwise/sampler/rna/RNA_MC_Suite.hh>
 #include <protocols/stepwise/sampler/rna/RNA_MC_MultiSuite.hh>
-#include <protocols/farna/thermal_sampling/ThermalSamplingMover.hh>
-#include <protocols/farna/thermal_sampling/util.hh>
 
 #include <utility/io/ozstream.hh>
 
@@ -50,7 +48,6 @@ using namespace core::pose;
 using namespace protocols;
 using namespace protocols::stepwise;
 using namespace basic::options::OptionKeys;
-using namespace protocols::farna::thermal_sampling;
 using namespace basic::options;
 
 OPT_KEY( String, seq1 )
@@ -63,16 +60,140 @@ OPT_KEY( String, out_prefix )
 OPT_KEY( Boolean, save_score_terms )
 OPT_KEY( Boolean, dump_pdb )
 OPT_KEY( Integer, n_intermediate_dump )
+//////////////////////////////////////////////////////////////////////////////
+// Histogram class for accumulating samples
+class Histogram {
 
+	public:
+	Histogram( Real const min, Real const max, Real const spacing ):
+	min_( min ),
+		max_( max ),
+		spacing_( spacing )
+		{
+		runtime_assert( max > min );
+	n_elem_ = static_cast<Size>( ( max - min ) / spacing ) + 1;
+	for ( Size i = 1; i <= n_elem_; ++i ) hist_.push_back( 0 );
+}
 
+void add( float const value, Size const n_items ) {
+	Size bin_index;
+	if ( value <= min_ ) {
+		bin_index = 1;
+	} else if ( value >= max_ ) {
+		bin_index = n_elem_;
+	} else {
+		bin_index = static_cast<Size>( ( value - min_ ) / spacing_ ) + 1;
+	}
+	hist_[bin_index] += n_items;
+}
 
+void clear() {
+	for ( Size i = 0; i <= n_elem_; ++i ) hist_[i] = 0;
+}
+
+utility::vector1<Real> get_scores() const {
+	utility::vector1<Real> scores;
+	for ( Size i = 1; i <= n_elem_; ++i ) {
+		scores.push_back( min_ + spacing_ * ( i - 0.5 ) );
+	}
+	return scores;
+}
+
+utility::vector1<Size> get_hist() const { return hist_; }
+
+private:
+Real const min_, max_, spacing_;
+Size n_elem_;
+utility::vector1<Size> hist_;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+// score types to be recorded
+utility::vector1<scoring::ScoreType> const & get_scoretypes() {
+	using namespace scoring;
+	static utility::vector1<ScoreType> scoretypes;
+	if ( !scoretypes.empty() ) return scoretypes;
+
+	// List of score types to be cached
+	scoretypes.push_back( fa_atr );
+	scoretypes.push_back( fa_rep );
+	scoretypes.push_back( fa_intra_rep );
+	scoretypes.push_back( fa_stack );
+	scoretypes.push_back( rna_torsion );
+	scoretypes.push_back( hbond_sc );
+	scoretypes.push_back( lk_nonpolar );
+	scoretypes.push_back( geom_sol_fast );
+	scoretypes.push_back( stack_elec );
+	scoretypes.push_back( fa_elec_rna_phos_phos );
+	return scoretypes;
+}
+//////////////////////////////////////////////////////////////////////////////
+void update_scores(
+	utility::vector1<float> & scores,
+	Pose & pose,
+	scoring::ScoreFunctionOP const scorefxn
+) {
+	using namespace scoring;
+	scores.clear();
+	scores.push_back( ( *scorefxn )( pose ) );
+	utility::vector1<ScoreType> const & score_types( get_scoretypes() );
+	for ( Size i = 1; i<= score_types.size(); ++i ) {
+		scores.push_back( scorefxn->score_by_scoretype(
+			pose, score_types[i], false /*weighted*/ ) );
+	}
+}
+//////////////////////////////////////////////////////////////////////////////
+void fill_data(
+	utility::vector1<float> & data,
+	Size const count,
+	utility::vector1<float> & scores
+) {
+	using namespace scoring;
+	data.push_back( count );
+	data.insert( data.end(), scores.begin(), scores.end() );
+}
+//////////////////////////////////////////////////////////////////////////////
+// Simple heuristic for gaussian stdev
+Real gaussian_stdev( Real const n_rsd, Real const temp, bool const is_bp ) {
+	// Negative temp is infinite
+	if ( temp < 0 ) return -1;
+	if ( is_bp ) return 5 * temp / n_rsd;
+	return 6 * pow( temp / n_rsd, 0.75 );
+}
+//////////////////////////////////////////////////////////////////////////////
+template<typename T>
+void vector2disk_in1d(
+	std::string const & out_filename,
+	utility::vector1<T> const & out_vector
+) {
+	utility::io::ozstream out(
+		out_filename.c_str(), std::ios::out | std::ios::binary );
+	out.write( (const char*) &out_vector[1], sizeof(T) * out_vector.size() );
+	out.close();
+}
+//////////////////////////////////////////////////////////////////////////////
+template<typename T>
+void vector2disk_in2d(
+	std::string const & out_filename,
+	Size const dim1,
+	Size const dim2,
+	utility::vector1<T> const & out_vector
+) {
+	utility::io::ozstream out(
+		out_filename.c_str(), std::ios::out | std::ios::binary );
+	runtime_assert( dim1 * dim2 == out_vector.size() );
+	out.write( (const char*) &dim1, sizeof(Size) );
+	out.write( (const char*) &dim2, sizeof(Size) );
+	out.write( (const char*) &out_vector[1], sizeof(T) * out_vector.size() );
+	out.close();
+}
 //////////////////////////////////////////////////////////////////////////////
 
 void set_gaussian_stdev(
-sampler::rna::RNA_MC_MultiSuite & sampler,
-moves::SimulatedTempering const & tempering,
-utility::vector1<Size> const & bp_rsd,
-utility::vector1<Size> const & dangling_rsd
+	sampler::rna::RNA_MC_MultiSuite & sampler,
+	moves::SimulatedTempering const & tempering,
+	utility::vector1<Size> const & bp_rsd,
+	utility::vector1<Size> const & dangling_rsd
 ) {
 	Size const n_rsd( bp_rsd.size() + dangling_rsd.size() );
 	Real const temp( tempering.temperature() );
@@ -86,24 +207,67 @@ utility::vector1<Size> const & dangling_rsd
 	}
 }
 //////////////////////////////////////////////////////////////////////////////
-PoseOP pose_setup( utility::vector1< Size > & bp_rsd, utility::vector1< Size > & dangling_rsd, utility::vector1< Size > & all_rsd
+PoseOP pose_setup(
+	std::string const & seq1,
+	std::string const & seq2,
+	Size const len1
 ) {
+	protocols::stepwise::modeler::rna::helix::RNA_HelixAssembler assembler;
+	assembler.use_phenix_geo( true );
+	PoseOP pose( assembler.build_init_pose( seq1, seq2 ) );
+	add_variant_type_to_pose_residue( *pose, chemical::VIRTUAL_PHOSPHATE, 1 );
+	if ( seq1 != "" && seq2 != "" ) {
+		add_variant_type_to_pose_residue(
+			*pose, chemical::VIRTUAL_PHOSPHATE, len1 + 1 );
+	}
+	return pose;
+}
+////////////////////////////////////////////////////////////////////////////////
+Size data_dim() {
+	using namespace scoring;
+	utility::vector1<ScoreType> const & score_types( get_scoretypes() );
+	return score_types.size() + 2;
+}
+//////////////////////////////////////////////////////////////////////////////
+void
+MC_run() {
+	using namespace protocols::stepwise::sampler::rna;
+	using namespace protocols::moves;
+	using namespace scoring;
+
+	clock_t const time_start( clock() );
+
+	utility::vector1< Real > const & temps_( option[ temps ]() );
+	runtime_assert( temps_.size() != 0 );
+
+	utility::vector1< Real > weights_;
+	utility::vector1< Real > const & orig_weights( option[ st_weights ]() );
+	if ( temps_.size() != orig_weights.size() ) {
+		weights_.push_back( 0 );
+	}
+	weights_.insert( weights_.end(), orig_weights.begin(),
+		orig_weights.end() );
+	runtime_assert( temps_.size() == weights_.size() );
+
+	Size const n_cycle_( option[n_cycle]() );
 	std::string const & seq1_( option[seq1]() );
 	std::string const & seq2_( option[seq2]() );
 	Size const len1( get_sequence_len( seq1_ ) );
 	Size const len2( get_sequence_len( seq2_ ) );
 
-	protocols::stepwise::modeler::rna::helix::RNA_HelixAssembler assembler;
-	assembler.use_phenix_geo( true );
-	PoseOP pose( assembler.build_init_pose( seq1_, seq2_ ) );
-	add_variant_type_to_pose_residue( *pose, chemical::VIRTUAL_PHOSPHATE, 1 );
-	if ( seq1_ != "" && seq2_ != "" ) {
-		add_variant_type_to_pose_residue(
-			*pose, chemical::VIRTUAL_PHOSPHATE, len1 + 1 );
+	// Score function setup
+	ScoreFunctionOP scorefxn;
+	if ( option[ score::weights ].user() ) {
+		scorefxn = get_score_function();
+	} else {
+		scorefxn = ScoreFunctionFactory::create_score_function( RNA_HIRES_WTS );
 	}
 
+	// Pose setup
+	Pose pose( *pose_setup( seq1_, seq2_, len1 ) );
 
 	// Figure out bp and dangling residues
+	utility::vector1< Size > bp_rsd, dangling_rsd;
 	Size const n_bp( std::min( len1, len2 ) );
 	Size const total_len( len1 + len2 );
 	for ( Size i = 1; i <= total_len; ++i ) {
@@ -112,82 +276,29 @@ PoseOP pose_setup( utility::vector1< Size > & bp_rsd, utility::vector1< Size > &
 		} else {
 			bp_rsd.push_back( i );
 		}
-		all_rsd.push_back( i );
 	}
-
-	return pose;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-void
-MC_run() {
-	using namespace protocols::stepwise::sampler::rna;
-	using namespace protocols::moves;
-	using namespace scoring;
-
-	/*
-	clock_t const time_start( clock() );
-	utility::vector1< Real > const & temps_( option[ temps ]() );
-	runtime_assert( temps_.size() != 0 );
-
-	utility::vector1< Real > weights_;
-	utility::vector1< Real > const & orig_weights( option[ st_weights ]() );
-	if ( temps_.size() != orig_weights.size() ) {
-	weights_.push_back( 0 );
-	}
-	weights_.insert( weights_.end(), orig_weights.begin(),
-	orig_weights.end() );
-	runtime_assert( temps_.size() == weights_.size() );
-
-	Size const n_cycle_( option[n_cycle]() );
-
-	// Score function setup
-	ScoreFunctionOP scorefxn;
-	if ( option[ score::weights ].user() ) {
-	scorefxn = get_score_function();
-	} else {
-	scorefxn = ScoreFunctionFactory::create_score_function( RNA_HIRES_WTS );
-	}
-	*/
-
-
-	// Pose setup
-	// Figure out bp and dangling residues
-	utility::vector1< Size > bp_rsd, dangling_rsd, all_rsd;
-	Pose pose( *pose_setup( bp_rsd, dangling_rsd, all_rsd ) );
-
-	protocols::viewer::add_conformation_viewer( pose.conformation(), "current", 600, 600 );
-
-	using namespace protocols::farna::thermal_sampling;
-	ThermalSamplingMoverOP ts( new ThermalSamplingMover );
-	ts->set_residues( all_rsd );
-	ts->set_free_rsd( dangling_rsd );
-	ts->set_recces_turner_mode( true );
-	ts->set_dumping_app( true );
-	ts->apply( pose );
 
 	// Sampler setup
-	/*
 	RNA_MC_MultiSuite sampler;
 	for ( Size i = 1; i <= total_len; ++i ) {
-	bool const sample_near_a_form( bp_rsd.has_value( i ) );
-	if ( i == 1 || ( i > len1 && i != total_len ) ) {
-	RNA_MC_SuiteOP suite_sampler( new RNA_MC_Suite( i ) );
-	suite_sampler->set_sample_bb( i != 1 );
-	suite_sampler->set_sample_lower_nucleoside( true );
-	suite_sampler->set_sample_upper_nucleoside( false );
-	suite_sampler->set_sample_near_a_form( sample_near_a_form );
-	suite_sampler->set_a_form_range( option[a_form_range]() );
-	sampler.add_external_loop_rotamer( suite_sampler );
-	} else {
-	RNA_MC_SuiteOP suite_sampler( new RNA_MC_Suite( i - 1 ) );
-	suite_sampler->set_sample_bb( len1 == total_len || i != total_len );
-	suite_sampler->set_sample_lower_nucleoside( false );
-	suite_sampler->set_sample_upper_nucleoside( true );
-	suite_sampler->set_sample_near_a_form( sample_near_a_form );
-	suite_sampler->set_a_form_range( option[a_form_range]() );
-	sampler.add_external_loop_rotamer( suite_sampler );
-	}
+		bool const sample_near_a_form( bp_rsd.has_value( i ) );
+		if ( i == 1 || ( i > len1 && i != total_len ) ) {
+			RNA_MC_SuiteOP suite_sampler( new RNA_MC_Suite( i ) );
+			suite_sampler->set_sample_bb( i != 1 );
+			suite_sampler->set_sample_lower_nucleoside( true );
+			suite_sampler->set_sample_upper_nucleoside( false );
+			suite_sampler->set_sample_near_a_form( sample_near_a_form );
+			suite_sampler->set_a_form_range( option[a_form_range]() );
+			sampler.add_external_loop_rotamer( suite_sampler );
+		} else {
+			RNA_MC_SuiteOP suite_sampler( new RNA_MC_Suite( i - 1 ) );
+			suite_sampler->set_sample_bb( len1 == total_len || i != total_len );
+			suite_sampler->set_sample_lower_nucleoside( false );
+			suite_sampler->set_sample_upper_nucleoside( true );
+			suite_sampler->set_sample_near_a_form( sample_near_a_form );
+			suite_sampler->set_a_form_range( option[a_form_range]() );
+			sampler.add_external_loop_rotamer( suite_sampler );
+		}
 	}
 	sampler.init();
 	sampler.apply( pose );
@@ -203,7 +314,7 @@ MC_run() {
 	update_scores( scores, pose, scorefxn );
 	utility::vector1< float > const null_arr_;
 	utility::vector1<utility::vector1< float > > data(
-	temps_.size(), null_arr_ );
+		temps_.size(), null_arr_ );
 
 	Real const min( -100.05 ), max( 800.05 ), spacing( 0.1 );
 	Histogram null_hist( min, max, spacing);
@@ -230,91 +341,87 @@ MC_run() {
 	// Main sampling cycle
 	Size pct = 0;
 	for ( Size n = 1; n <= n_cycle_; ++n ) {
-	if ( n % ( n_cycle_/100 ) == 0 ) {
-	++pct;
-	std::cout << pct << "% complete." << std::endl;
-	}
-	++sampler;
-	sampler.apply( pose );
-	if ( tempering.boltzmann( pose ) || n == n_cycle_ ) {
-	if ( save_scores ) fill_data( data[ temp_id ], curr_counts, scores );
-	++n_accept_total;
-	hist_list[ temp_id ].add( scores[ 1 ], curr_counts );
-	update_scores( scores, pose, scorefxn );
-	if ( n == n_cycle_ ) break;
-	sampler.update();
-	curr_counts = 1;
-	if ( option[ dump_pdb ]() && scores[ 1 ] < min_score ) {
-	min_score = scores[ 1 ];
-	min_pose = pose;
-	}
-	if ( n_dump != 0 && n * (n_dump + 1) / double(n_cycle_) >= curr_dump ) {
-	std::ostringstream oss;
-	oss << "intermediate" << '_' << curr_dump << ".pdb";
-	pose.dump_pdb(oss.str());
-	++curr_dump;
-	}
-	} else {
-	++curr_counts;
-	}
+		if ( n % ( n_cycle_/100 ) == 0 ) {
+			++pct;
+			std::cout << pct << "% complete." << std::endl;
+		}
+		++sampler;
+		sampler.apply( pose );
+		if ( tempering.boltzmann( pose ) || n == n_cycle_ ) {
+			if ( save_scores ) fill_data( data[ temp_id ], curr_counts, scores );
+			++n_accept_total;
+			hist_list[ temp_id ].add( scores[ 1 ], curr_counts );
+			update_scores( scores, pose, scorefxn );
+			if ( n == n_cycle_ ) break;
+			sampler.update();
+			curr_counts = 1;
+			if ( option[ dump_pdb ]() && scores[ 1 ] < min_score ) {
+				min_score = scores[ 1 ];
+				min_pose = pose;
+			}
+			if ( n_dump != 0 && n * (n_dump + 1) / double(n_cycle_) >= curr_dump ) {
+				std::ostringstream oss;
+				oss << "intermediate" << '_' << curr_dump << ".pdb";
+				pose.dump_pdb(oss.str());
+				++curr_dump;
+			}
+		} else {
+			++curr_counts;
+		}
 
-	if ( n % t_jump_interval == 0 && tempering.t_jump() ) {
-	++n_t_jumps_accept;
-	if ( save_scores ) fill_data( data[ temp_id ], curr_counts, scores );
-	hist_list[ temp_id ].add( scores[ 1 ], curr_counts );
-	curr_counts = 1;
-	set_gaussian_stdev( sampler, tempering, bp_rsd, dangling_rsd );
-	temp_id = tempering.temp_id();
-	}
+		if ( n % t_jump_interval == 0 && tempering.t_jump() ) {
+			++n_t_jumps_accept;
+			if ( save_scores ) fill_data( data[ temp_id ], curr_counts, scores );
+			hist_list[ temp_id ].add( scores[ 1 ], curr_counts );
+			curr_counts = 1;
+			set_gaussian_stdev( sampler, tempering, bp_rsd, dangling_rsd );
+			temp_id = tempering.temp_id();
+		}
 	}
 	if ( option[dump_pdb]() ) {
-	pose.dump_pdb( "end.pdb" );
-	min_pose.dump_pdb( "min.pdb" );
-	scorefxn->show( min_pose );
+		pose.dump_pdb( "end.pdb" );
+		min_pose.dump_pdb( "min.pdb" );
+		scorefxn->show( min_pose );
 	}
-	*/
 
 	// Output simple statistics and the data
 	//pose.dump_pdb("final.pdb");
 
-	//std::cout << "n_cycles: " << n_cycle_ << std::endl;
-	//std::cout << "Accept rate: " << double( n_accept_total ) / n_cycle_
-	// << std::endl;
-	//std::cout << "T_jump accept rate: " << double( n_t_jumps_accept ) / n_t_jumps
-	// << std::endl;
-	//Real const time_in_test = static_cast<Real>( clock() - time_start )
-	// / CLOCKS_PER_SEC;
-	//std::cout << "Time in sampler: " <<  time_in_test << std::endl;
+	std::cout << "n_cycles: " << n_cycle_ << std::endl;
+	std::cout << "Accept rate: " << double( n_accept_total ) / n_cycle_
+		<< std::endl;
+	std::cout << "T_jump accept rate: " << double( n_t_jumps_accept ) / n_t_jumps
+		<< std::endl;
+	Real const time_in_test = static_cast<Real>( clock() - time_start )
+		/ CLOCKS_PER_SEC;
+	std::cout << "Time in sampler: " <<  time_in_test << std::endl;
 
-	/*
 	for ( Size i = 1; i <= temps_.size(); ++i ) {
-	if ( save_scores ) {
-	std::ostringstream oss;
-	oss << option[out_prefix]() << '_' << std::fixed << std::setprecision( 2 )
-	<< temps_[ i ] << ".bin.gz";
-	Size const data_dim2( scorefxn->get_nonzero_weighted_scoretypes().size() + 2 );
-	Size const data_dim1( data[ i ].size() / data_dim2 );
-	vector2disk_in2d( oss.str(), data_dim1, data_dim2, data[ i ] );
-	}
-	std::ostringstream oss;
-	oss << option[ out_prefix ]() << '_' << std::fixed << std::setprecision( 2 )
-	<< temps_[ i ] << ".hist.gz";
-	utility::vector1< Size > const & hist( hist_list[ i ].get_hist() );
-	utility::vector1< Real > const & scores( hist_list[ i ].get_scores() );
-	vector2disk_in1d( oss.str(), hist );
+		if ( save_scores ) {
+			std::ostringstream oss;
+			oss << option[out_prefix]() << '_' << std::fixed << std::setprecision( 2 )
+				<< temps_[ i ] << ".bin.gz";
+			Size const data_dim2( data_dim() );
+			Size const data_dim1( data[ i ].size() / data_dim2 );
+			vector2disk_in2d( oss.str(), data_dim1, data_dim2, data[ i ] );
+		}
+		std::ostringstream oss;
+		oss << option[ out_prefix ]() << '_' << std::fixed << std::setprecision( 2 )
+			<< temps_[ i ] << ".hist.gz";
+		utility::vector1< Size > const & hist( hist_list[ i ].get_hist() );
+		utility::vector1< Real > const & scores( hist_list[ i ].get_scores() );
+		vector2disk_in1d( oss.str(), hist );
 
-	std::ostringstream oss1;
-	oss1 << option[ out_prefix ]() << "_hist_scores.gz";
-	vector2disk_in1d( oss1.str(), scores );
+		std::ostringstream oss1;
+		oss1 << option[ out_prefix ]() << "_hist_scores.gz";
+		vector2disk_in1d( oss1.str(), scores );
 	}
-	*/
 }
 //////////////////////////////////////////////////////////////////////////////
 void*
 my_main( void* )
 {
 	MC_run();
-	protocols::viewer::clear_conformation_viewers();
 	exit( 0 );
 }
 //////////////////////////////////////////////////////////////////////////////
