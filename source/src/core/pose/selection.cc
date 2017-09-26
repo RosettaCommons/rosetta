ede956ca263bc44b6bbd37c98acd765d2a2eda17
Rocco Moretti <rmorettiase@gmail.com>
2017-09-26 10:27:02 -0500

    Beautifying forgotten file.
    Old repository SHA1: d6b45ae5514df4f6f109085a4ccc6dc770243381

--- source/src/core/pose/selection.cc
+++ source/src/core/pose/selection.cc
@@ -170,8 +170,8 @@ void
 attributes_for_parse_resnum( utility::tag::AttributeList & attlist, std::string const & att_name, std::string const & description ) {
 	using namespace utility::tag;
 	attlist
-			+ XMLSchemaAttribute( att_name, xsct_refpose_enabled_residue_number,
-			( description.empty() ?	"Residue number in PDB numbering (residue number + chain ID)" : description ) );
+		+ XMLSchemaAttribute( att_name, xsct_refpose_enabled_residue_number,
+		( description.empty() ? "Residue number in PDB numbering (residue number + chain ID)" : description ) );
 }
 
 /// @brief Extracts residue numbers from a 'selection'.
