51f9b616e19e846147c7b79ff2fd74d1248e6edd
Zibo Chen <zibochen@uw.edu>
2017-10-16 12:42:04 -0700

    adding try/catch block to the pilot app pad_helices
    Old repository SHA1: cd91c8a3f01ebe205c5b3994f71b8bd89902fc42

--- source/src/apps/pilot/zibochen/pad_helices.cc
+++ source/src/apps/pilot/zibochen/pad_helices.cc
@@ -7,9 +7,6 @@
 // (c) For more information, see http://www.rosettacommons.org. Questions about this can be
 // (c) addressed to University of Washington UW TechTransfer, email: license@u.washington.edu.
 
-//Part of the flatland docking protocol, symmetrizies sequence onto other modular parts of the building blocks. Rejects a building block if it is not modular.
-//Also generates the full lattice
-
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -55,10 +52,10 @@
 //#include <core/io/silent/BinaryProteinSilentStruct.hh>
 //#include <core/io/silent/SilentFileData.hh>
 
-#include <core/kinematics/FoldTree.hh>
-#include <core/kinematics/RT.hh>
-#include <core/kinematics/MoveMap.hh>
-#include <core/kinematics/Jump.hh>
+//#include <core/kinematics/FoldTree.hh>
+//#include <core/kinematics/RT.hh>
+//#include <core/kinematics/MoveMap.hh>
+//#include <core/kinematics/Jump.hh>
 
 //#include <core/optimization/MinimizerOptions.hh>
 //#include <core/optimization/symmetry/SymAtomTreeMinimizer.hh>
@@ -102,7 +99,7 @@
 //#include <protocols/cryst/TwoDLattice.hh>
 //#include <protocols/cryst/wallpaper.hh>
 
-#include <protocols/moves/Mover.hh>
+//#include <protocols/moves/Mover.hh>
 
 //#include <protocols/flxbb/LayerDesignOperation.hh>
 
@@ -149,76 +146,81 @@ static basic::Tracer TR("apps.pilot.zibochen.pad_helices");
 ////////////////////////////////////////////////
 int
 main( int argc, char ** argv ) {
+	try{
+		NEW_OPT( src_pose_1_start, "starting indecies of the source pose 1", "");
+		NEW_OPT( target_pose_start, "starting indecies of the target pose for src pose 2 ", "");
+		NEW_OPT( span, "how long are the replaced regions?", 7);
+		NEW_OPT( prefix, "prefix for the output file", "");
+		NEW_OPT( pad_3_heptads, "pad 2 or 3 heptads?", false);
+		NEW_OPT( target_pose_start_2, "starting indecies of the target pose for src pose 2 ", "");
+		NEW_OPT( align_bb, "should backbones be aligned during residue replacement?", false);
 
-	NEW_OPT( src_pose_1_start, "starting indecies of the source pose 1", "");
-	NEW_OPT( target_pose_start, "starting indecies of the target pose for src pose 2 ", "");
-	NEW_OPT( span, "how long are the replaced regions?", 7);
-	NEW_OPT( prefix, "prefix for the output file", "");
-	NEW_OPT( pad_3_heptads, "pad 2 or 3 heptads?", false);
-	NEW_OPT( target_pose_start_2, "starting indecies of the target pose for src pose 2 ", "");
-	NEW_OPT( align_bb, "should backbones be aligned during residue replacement?", false);
+		devel::init(argc, argv);
 
-	devel::init(argc, argv);
+		// option[ in::preserve_crystinfo ].value(true); //need this line to add remarks to pdb
+		// option[ basic::options::OptionKeys::symmetry::detect_bonds ].value(false); //force this option to be false so Rosetta would not try to connect bonds between adjacent jumps
 
-	// option[ in::preserve_crystinfo ].value(true); //need this line to add remarks to pdb
-	// option[ basic::options::OptionKeys::symmetry::detect_bonds ].value(false); //force this option to be false so Rosetta would not try to connect bonds between adjacent jumps
+		utility::vector1< std::string > design_filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ]();
+		core::pose::Pose src_pose;
+		core::import_pose::pose_from_file(src_pose, design_filenames[1] );
+		core::pose::Pose target_pose;
+		core::import_pose::pose_from_file(target_pose, design_filenames[2] );
 
-	utility::vector1< std::string > design_filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ]();
-	core::pose::Pose src_pose;
-	core::import_pose::pose_from_file(src_pose, design_filenames[1] );
-	core::pose::Pose target_pose;
-	core::import_pose::pose_from_file(target_pose, design_filenames[2] );
+		//get basename, used for outputing pose
+		std::string base_name=design_filenames[2].substr( design_filenames[2].find_last_of( '/' ) +1 );
+		const std::string ext(".pdb");
+		base_name = base_name.substr(0, base_name.size() - ext.size());
 
-	//get basename, used for outputing pose
-	std::string base_name=design_filenames[2].substr( design_filenames[2].find_last_of( '/' ) +1 );
-	const std::string ext(".pdb");
-	base_name = base_name.substr(0, base_name.size() - ext.size());
-
-	//string to size
-	utility::vector1< Size > src_pose_1_start_ind;
-	utility::vector1< Size > target_pose_start_ind;
-	utility::vector1<std::string> src_pose_1_start_in=utility::string_split_multi_delim(option[src_pose_1_start],",");
-	for ( Size i=1; i<=src_pose_1_start_in.size(); i++ ) {
-		src_pose_1_start_ind.push_back(utility::string2int(src_pose_1_start_in[i]));
-	}
-	utility::vector1<std::string> target_pose_start_in=utility::string_split_multi_delim(option[target_pose_start],",");
-	for ( Size i=1; i<=target_pose_start_in.size(); i++ ) {
-		target_pose_start_ind.push_back(utility::string2int(target_pose_start_in[i]));
-	}
-
-	//replace residues
-	for ( Size i=1; i<=src_pose_1_start_ind.size(); i++ ) {
-		for ( int j=0; j<=option[span]-1; j++ ) {
-			target_pose.replace_residue(target_pose_start_ind[i]+j, src_pose.residue(src_pose_1_start_ind[i]+j), option[align_bb]);
-			// target_pose.delete_polymer_residue(target_pose_start_ind[i]+j);
-			// //target_pose.append_residue_by_bond(src_pose.residue(src_pose_1_start_ind[i]+j));
-			// target_pose.append_residue_by_jump(src_pose.residue(src_pose_1_start_ind[i]+j),target_pose_start_ind[i]+j-1);
-		}
-		//target_pose.copy_segment(option[span],src_pose,target_pose_start_ind[i],src_pose_1_start_ind[i]);
-	}
-
-	//check if there is an additional heptad to be padded
-	if ( option[pad_3_heptads] ) {
 		//string to size
-		utility::vector1< Size > target_pose_start_2_ind;
-		utility::vector1<std::string> target_pose_start_2_in=utility::string_split_multi_delim(option[target_pose_start_2],",");
-		for ( Size i=1; i<=target_pose_start_2_in.size(); i++ ) {
-			target_pose_start_2_ind.push_back(utility::string2int(target_pose_start_2_in[i]));
+		utility::vector1< Size > src_pose_1_start_ind;
+		utility::vector1< Size > target_pose_start_ind;
+		utility::vector1<std::string> src_pose_1_start_in=utility::string_split_multi_delim(option[src_pose_1_start],",");
+		for ( Size i=1; i<=src_pose_1_start_in.size(); i++ ) {
+			src_pose_1_start_ind.push_back(utility::string2int(src_pose_1_start_in[i]));
 		}
-		core::pose::Pose src_pose_2;
-		core::import_pose::pose_from_file(src_pose_2, design_filenames[3] );
+		utility::vector1<std::string> target_pose_start_in=utility::string_split_multi_delim(option[target_pose_start],",");
+		for ( Size i=1; i<=target_pose_start_in.size(); i++ ) {
+			target_pose_start_ind.push_back(utility::string2int(target_pose_start_in[i]));
+		}
+
 		//replace residues
-		for ( Size i=1; i<=target_pose_start_2_ind.size(); i++ ) {
+		for ( Size i=1; i<=src_pose_1_start_ind.size(); i++ ) {
 			for ( int j=0; j<=option[span]-1; j++ ) {
-				target_pose.replace_residue(target_pose_start_2_ind[i]+j, src_pose_2.residue(src_pose_1_start_ind[i]+j), option[align_bb]);
+				target_pose.replace_residue(target_pose_start_ind[i]+j, src_pose.residue(src_pose_1_start_ind[i]+j), option[align_bb]);
 				// target_pose.delete_polymer_residue(target_pose_start_ind[i]+j);
 				// //target_pose.append_residue_by_bond(src_pose.residue(src_pose_1_start_ind[i]+j));
 				// target_pose.append_residue_by_jump(src_pose.residue(src_pose_1_start_ind[i]+j),target_pose_start_ind[i]+j-1);
 			}
 			//target_pose.copy_segment(option[span],src_pose,target_pose_start_ind[i],src_pose_1_start_ind[i]);
 		}
+
+		//check if there is an additional heptad to be padded
+		if ( option[pad_3_heptads] ) {
+			//string to size
+			utility::vector1< Size > target_pose_start_2_ind;
+			utility::vector1<std::string> target_pose_start_2_in=utility::string_split_multi_delim(option[target_pose_start_2],",");
+			for ( Size i=1; i<=target_pose_start_2_in.size(); i++ ) {
+				target_pose_start_2_ind.push_back(utility::string2int(target_pose_start_2_in[i]));
+			}
+			core::pose::Pose src_pose_2;
+			core::import_pose::pose_from_file(src_pose_2, design_filenames[3] );
+			//replace residues
+			for ( Size i=1; i<=target_pose_start_2_ind.size(); i++ ) {
+				for ( int j=0; j<=option[span]-1; j++ ) {
+					target_pose.replace_residue(target_pose_start_2_ind[i]+j, src_pose_2.residue(src_pose_1_start_ind[i]+j), option[align_bb]);
+					// target_pose.delete_polymer_residue(target_pose_start_ind[i]+j);
+					// //target_pose.append_residue_by_bond(src_pose.residue(src_pose_1_start_ind[i]+j));
+					// target_pose.append_residue_by_jump(src_pose.residue(src_pose_1_start_ind[i]+j),target_pose_start_ind[i]+j-1);
+				}
+				//target_pose.copy_segment(option[span],src_pose,target_pose_start_ind[i],src_pose_1_start_ind[i]);
+			}
+		}
+		//dump pdb
+		target_pose.dump_pdb(option[prefix]+base_name+"_padded.pdb");
+		return 0;
+		
+	} catch ( utility::excn::EXCN_Base const & e ) {
+		std::cout << "caught exception " << e.msg() << std::endl;
+		return -1;
 	}
-	//dump pdb
-	target_pose.dump_pdb(option[prefix]+base_name+"_padded.pdb");
-	return 0;
 }//main
