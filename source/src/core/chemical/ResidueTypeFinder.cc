a576b14b9f2823e5cf411608e6c503b204261a8d
Andy Watkins <andy.watkins2@gmail.com>
2018-03-22 00:10:59 -0700

    Merge pull request #3051 from RosettaCommons/everyday847/disable_vanity_metapatches
    Disable vanity metapatches
    
    Old repository SHA1: f73d86d0e0441daed9de7db9f2e811738e153554

--- source/src/core/chemical/ResidueTypeFinder.cc
+++ source/src/core/chemical/ResidueTypeFinder.cc
@@ -84,11 +84,11 @@ ResidueTypeFinder::get_representative_type() const
 	ResidueTypeCOPs rsd_types;
 	rsd_types = get_possible_base_residue_types( false /* include_unpatchable */ );
 	rsd_types = apply_patches_recursively( rsd_types, 1 /*start with this patch*/, true /*get_first_residue_found*/ );
-	rsd_types = apply_metapatches_recursively( rsd_types, 1 /*start with this patch*/ );
+	rsd_types = apply_metapatches_recursively( rsd_types, 1 /*start with this patch*/, true );
 	// We need to apply metapatches again just in case there are some double variants.
 	// Only needed for packing metapatched residues.
 	// TODO: this is atrocious.
-	rsd_types = apply_metapatches_recursively( rsd_types, 1 /*start with this patch*/ );
+	rsd_types = apply_metapatches_recursively( rsd_types, 1 /*start with this patch*/, true );
 	if ( rsd_types.size() == 0 ) rsd_types =  get_possible_unpatchable_residue_types();
 
 	rsd_types = apply_filters_after_patches( rsd_types, true /* allow_extra_variants */ );
