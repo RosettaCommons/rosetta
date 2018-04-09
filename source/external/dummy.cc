d149747c32d8d59700690dcf06e22496e5691832
Rocco Moretti <rmorettiase@gmail.com>
2018-04-09 19:15:52 -0400

    Merge pull request #3107 from RosettaCommons/roccomoretti/only_for_extras_dummy
    Add dummy cc file for 'only_for_extras' build.
    
    It appears the static build doesn't like an empty source list when an 'only_for_extras' library isn't active. Add a dummy source file which will be used for that purpose. Should fix the mac.clang.static.build.release test on the test server
    
    Old repository SHA1: f9e0cf84fc444a6c05ca5d840c0b54bbda2c03b6

---
+++ source/external/dummy.cc
@@ -0,0 +1,7 @@
+// This is a dummy (empty) file to be used with only_with_extras, to avoid issues with empty libraries.
+
+namespace { // An anonymous namespace to limit name clashes between multiple uses
+
+void _dummy_() {}
+
+}
