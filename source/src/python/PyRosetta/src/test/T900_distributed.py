6c3d670b0b621d63d1ef4444b62b2584316db7ab
Alex Ford <fordas@uw.edu>
2018-10-26 15:40:10 -0700

    Merge pull request #3591 from RosettaCommons/fordas/distributed_test_update
    Fixup distributed test runner.
    
    Old repository SHA1: 32f492c6681bac722028a2a0b54fa05fabeab7e4

--- source/src/python/PyRosetta/src/test/T900_distributed.py
+++ source/src/python/PyRosetta/src/test/T900_distributed.py
@@ -1,14 +1,23 @@
 from __future__ import print_function
 import sys
+import os
 
 if tuple(sys.version_info) < (3, 5):
-    print("Unsupported system for pyrosetta.distributed: %s")
+    print("Unsupported python version for pyrosetta.distributed: %s" % sys.version_info)
     sys.exit(0)
+else:
+    print("sys.version_info: %s", sys.version_info)
 
 import tempfile
 import venv
 import subprocess
 import shlex
+
+import pyrosetta.rosetta
+
+if not hasattr(pyrosetta.rosetta, "cereal"):
+    print("Unsupported non-serialization build for pyrosetta.distributed.")
+    sys.exit(0)
 
 
 def e(cmd):
