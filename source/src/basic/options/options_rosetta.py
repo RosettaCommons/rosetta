0aab87cd394909a8918716a0c503c8ed1bb59d47
Andy Watkins <andy.watkins2@gmail.com>
2019-02-14 23:50:15 -0500

    Merge pull request #3803 from RosettaCommons/everyday847/allow_complex_loop_graph
    allow_complex_loop_graph default true
    
    Old repository SHA1: 88c1011f64f5c903e034e56d9e578ac554354085

--- source/src/basic/options/options_rosetta.py
+++ source/src/basic/options/options_rosetta.py
@@ -1674,7 +1674,7 @@ Options = Option_Group( '',
 		), #-score:occ_sol_fitted
     Option_Group('loop_close',
 		  Option( 'loop_fixed_cost', 'Real', desc="For loop_close term, a fixed cost of instantiating an end of a Gaussian chain; calibrated based on RNA bulge/loop data", default="-0.29" ),
-		  Option( 'allow_complex_loop_graph', 'Boolean', desc='LoopGraph hack - allow sharing of loops between cycles', default='false' ),
+		  Option( 'allow_complex_loop_graph', 'Boolean', desc='LoopGraph hack - allow sharing of loops between cycles', default='true' ),
 		  Option( 'use_6D_potential', 'Boolean', desc='for implicit modeling of RNA loops, use 6D potentials instead of simple 1D Gaussian chain model', default='false' ),
 		  Option( 'force_6D_potential_file', 'String', desc='use this 6D potential for all loop_close instead of database files (for testing only!)', default='' ),
 		  Option( 'use_cubic_interp', 'Boolean', desc='cubic interpolation for 6D potential, instead of polylinear', default='true' ),
