e3cf9b8726b284610e09f7ce4c89d75ee83a24ba
Phuong Nguyen <tranphuonguns@gmail.com>
2019-08-02 08:52:13 -0700

    Merge pull request #4103 from RosettaCommons/tigerous/sci-tests-loop-model-ccd-12res
    Tigerous/sci tests loop model ccd 12res
    
    Old repository SHA1: 757359f41776ea85850e8b62e4b709cae63f18d6

---
+++ tests/scientific/tests/loop_modeling_ccd_12res/1.submit.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# :noTabs=true:
+
+# (c) Copyright Rosetta Commons Member Institutions.
+# (c) This file is part of the Rosetta software suite and is made available under license.
+# (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+# (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+# (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+## @file  _template_/1.submit.py
+## @brief this script is part of <template> scientific test
+## @author Sergey Lyskov
+## @author Phuong T. Nguyen (tranphuonguns@gmail.com)
+
+import os, sys, time
+import benchmark
+
+benchmark.load_variables()  # Python black magic: load all variables saved by previous script into globals
+config = benchmark.config()
+
+#==> EDIT HERE
+testname    = "loop_modeling_ccd_12res"
+
+debug       = config['debug']
+rosetta_dir = config['rosetta_dir']
+working_dir = config['working_dir']
+hpc_driver  = benchmark.hpc_driver()
+extension   = benchmark.calculate_extension()
+
+#==> EDIT HERE
+command_line = '''
+-database {rosetta_dir}/database
+-in:file:s {rosetta_dir}/tests/scientific/data/{testname}/{target}.pdb
+-in:file:native {rosetta_dir}/tests/scientific/data/{testname}/reference/{target}.pdb
+-loops:loop_file {rosetta_dir}/tests/scientific/data/{testname}/{target}.loop
+-loops:extended true
+-loops:remodel quick_ccd
+-loops:refine refine_kic
+-refine_outer_cycles 5
+-loops:frag_sizes 9 3 1
+-loops:frag_files {rosetta_dir}/tests/scientific/data/{testname}/{target}.200.9mers {rosetta_dir}/tests/scientific/data/{testname}/{target}.200.3mers none
+-ex1
+-ex2
+-score:weights ref2015
+-nstruct {nstruct}
+-out:file:scorefile {prefix}/{target}.score
+
+-multiple_processes_writing_to_one_directory
+-no_color
+'''.replace('\n', ' ').replace('  ', ' ')
+
+#==> EDIT HERE
+nstruct = 2 if debug else 500
+
+#==> EDIT HERE
+targets = '2cpl 2ebn 2exo 2pia 2rn2 2sil 2tgi'.split()
+targets = targets[:2] if debug else targets
+
+#print(f'extension: {extension}')
+#print(f'command_line: {command_line}')
+#print(f'config: {benchmark.config()}')
+#print(hpc_driver)
+
+hpc_logs = f'{working_dir}/hpc-logs'
+if not os.path.exists(hpc_logs): os.makedirs(hpc_logs)
+hpc_job_ids = []
+for target in targets:
+    prefix = f'{working_dir}/output/{target}'
+    if not os.path.exists(prefix): os.makedirs(prefix)
+
+    hpc_job_ids.append( hpc_driver.submit_hpc_job(
+        name=f'{testname}-{target}',
+
+        #==> EDIT HERE
+        executable = f'{rosetta_dir}/source/bin/loopmodel.{extension}',
+        arguments = command_line.format_map(vars()),
+        working_dir = prefix,
+        jobs_to_queue = min(nstruct, 50),
+        log_dir = hpc_logs,
+        time=24,
+        block=False)
+    )
+
+
+# if not debug:
+#     hpc_driver.wait_until_complete(hpc_job_ids, silent=True)
+#     time.sleep(64)  # waiting for NFS caching
+hpc_driver.wait_until_complete(hpc_job_ids, silent=True)
+
+#==> EDIT HERE
+benchmark.save_variables('debug targets nstruct working_dir testname')  # Python black magic: save all listed variable to json file for next script use (save all variables if called without argument)
