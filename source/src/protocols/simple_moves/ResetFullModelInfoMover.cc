83033bfba866f3b9930e039cb92fa7c96a06ba8e
Andy Watkins <andy.watkins2@gmail.com>
2019-08-28 20:28:39 -0400

    Merge pull request #4183 from RosettaCommons/everyday847/update_VirtualRootMover
    Make VirtualRootMover FullModelInfo aware
    
    Old repository SHA1: ebee3a9ac4bfcc07a1e146a48e709ca36adbda9a

---
+++ source/src/protocols/simple_moves/ResetFullModelInfoMover.cc
@@ -0,0 +1,165 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file protocols/simple_moves/ResetFullModelInfoMover.cc
+/// @brief Ensure synchronized full model info
+/// @author Andy Watkins (andy.watkins2@gmail.com)
+
+// Unit headers
+#include <protocols/simple_moves/ResetFullModelInfoMover.hh>
+#include <protocols/simple_moves/ResetFullModelInfoMoverCreator.hh>
+
+// Core headers
+#include <core/pose/Pose.hh>
+#include <core/pose/full_model_info/util.hh>
+#include <core/pose/full_model_info/FullModelInfo.hh>
+
+// Basic/Utility headers
+#include <basic/Tracer.hh>
+#include <utility/tag/Tag.hh>
+#include <utility/pointer/memory.hh>
+
+// XSD Includes
+#include <utility/tag/XMLSchemaGeneration.hh>
+#include <protocols/moves/mover_schemas.hh>
+
+static basic::Tracer TR( "protocols.simple_moves.ResetFullModelInfoMover" );
+
+namespace protocols {
+namespace simple_moves {
+
+/////////////////////
+/// Constructors  ///
+/////////////////////
+
+/// @brief Default constructor
+ResetFullModelInfoMover::ResetFullModelInfoMover():
+	protocols::moves::Mover( ResetFullModelInfoMover::mover_name() )
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Copy constructor
+ResetFullModelInfoMover::ResetFullModelInfoMover( ResetFullModelInfoMover const & src ):
+	protocols::moves::Mover( src )
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Destructor (important for properly forward-declaring smart-pointer members)
+ResetFullModelInfoMover::~ResetFullModelInfoMover(){}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Mover Methods ///
+/////////////////////
+
+/// @brief Apply the mover
+void
+ResetFullModelInfoMover::apply( core::pose::Pose & pose ) {
+	// If the pose has a FullModelInfo object this operation has obsoleted it.
+	// Fix that, rather bluntly.
+	core::pose::full_model_info::FullModelInfoOP full_model_info( new core::pose::full_model_info::FullModelInfo( pose ) );
+	core::pose::full_model_info::set_full_model_info( pose, full_model_info );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief Show the contents of the Mover
+void
+ResetFullModelInfoMover::show(std::ostream & output) const
+{
+	protocols::moves::Mover::show(output);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Rosetta Scripts Support ///
+///////////////////////////////
+
+/// @brief parse XML tag (to use this Mover in Rosetta Scripts)
+void
+ResetFullModelInfoMover::parse_my_tag(
+	utility::tag::TagCOP ,
+	basic::datacache::DataMap& ,
+	protocols::filters::Filters_map const & ,
+	protocols::moves::Movers_map const & ,
+	core::pose::Pose const & )
+{
+
+}
+void ResetFullModelInfoMover::provide_xml_schema( utility::tag::XMLSchemaDefinition & xsd )
+{
+
+	using namespace utility::tag;
+	AttributeList attlist;
+
+	//here you should write code to describe the XML Schema for the class.  If it has only attributes, simply fill the probided AttributeList.
+
+	protocols::moves::xsd_type_definition_w_attributes( xsd, mover_name(), "Resets a Pose's FullModelInfo", attlist );
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// @brief required in the context of the parser/scripting scheme
+protocols::moves::MoverOP
+ResetFullModelInfoMover::fresh_instance() const
+{
+	return utility::pointer::make_shared< ResetFullModelInfoMover >();
+}
+
+/// @brief required in the context of the parser/scripting scheme
+protocols::moves::MoverOP
+ResetFullModelInfoMover::clone() const
+{
+	return utility::pointer::make_shared< ResetFullModelInfoMover >( *this );
+}
+
+std::string ResetFullModelInfoMover::get_name() const {
+	return mover_name();
+}
+
+std::string ResetFullModelInfoMover::mover_name() {
+	return "ResetFullModelInfoMover";
+}
+
+
+
+/////////////// Creator ///////////////
+
+protocols::moves::MoverOP
+ResetFullModelInfoMoverCreator::create_mover() const
+{
+	return utility::pointer::make_shared< ResetFullModelInfoMover >();
+}
+
+std::string
+ResetFullModelInfoMoverCreator::keyname() const
+{
+	return ResetFullModelInfoMover::mover_name();
+}
+
+void ResetFullModelInfoMoverCreator::provide_xml_schema( utility::tag::XMLSchemaDefinition & xsd ) const
+{
+	ResetFullModelInfoMover::provide_xml_schema( xsd );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// private methods ///
+///////////////////////
+
+
+std::ostream &
+operator<<( std::ostream & os, ResetFullModelInfoMover const & mover )
+{
+	mover.show(os);
+	return os;
+}
+
+} //simple_moves
+} //protocols
---
+++ source/src/protocols/simple_moves/ResetFullModelInfoMover.hh
@@ -0,0 +1,116 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file protocols/simple_moves/ResetFullModelInfoMover.hh
+/// @brief Ensure synchronized full model info
+/// @author Andy Watkins (andy.watkins2@gmail.com)
+
+#ifndef INCLUDED_protocols_simple_moves_ResetFullModelInfoMover_HH
+#define INCLUDED_protocols_simple_moves_ResetFullModelInfoMover_HH
+
+// Unit headers
+#include <protocols/simple_moves/ResetFullModelInfoMover.fwd.hh>
+#include <protocols/moves/Mover.hh>
+
+// Protocol headers
+#include <protocols/filters/Filter.fwd.hh>
+
+// Core headers
+#include <core/pose/Pose.fwd.hh>
+
+// Basic/Utility headers
+#include <basic/datacache/DataMap.fwd.hh>
+//#include <utility/tag/XMLSchemaGeneration.fwd.hh> //transcluded from Mover
+
+namespace protocols {
+namespace simple_moves {
+
+///@brief Ensure synchronized full model info
+class ResetFullModelInfoMover : public protocols::moves::Mover {
+
+public:
+
+	/////////////////////
+	/// Constructors  ///
+	/////////////////////
+
+	/// @brief Default constructor
+	ResetFullModelInfoMover();
+
+	/// @brief Copy constructor (not needed unless you need deep copies)
+	ResetFullModelInfoMover( ResetFullModelInfoMover const & src );
+
+	/// @brief Destructor (important for properly forward-declaring smart-pointer members)
+	~ResetFullModelInfoMover() override;
+
+
+public:
+
+	/////////////////////
+	/// Mover Methods ///
+	/////////////////////
+
+
+	/// @brief Apply the mover
+	void
+	apply( core::pose::Pose & pose ) override;
+
+	void
+	show( std::ostream & output = std::cout ) const override;
+
+
+public:
+
+	///////////////////////////////
+	/// Rosetta Scripts Support ///
+	///////////////////////////////
+
+	/// @brief parse XML tag (to use this Mover in Rosetta Scripts)
+	void
+	parse_my_tag(
+		utility::tag::TagCOP tag,
+		basic::datacache::DataMap & data,
+		protocols::filters::Filters_map const & filters,
+		protocols::moves::Movers_map const & movers,
+		core::pose::Pose const & pose ) override;
+
+	//ResetFullModelInfoMover & operator=( ResetFullModelInfoMover const & src );
+
+	/// @brief required in the context of the parser/scripting scheme
+	protocols::moves::MoverOP
+	fresh_instance() const override;
+
+	/// @brief required in the context of the parser/scripting scheme
+	protocols::moves::MoverOP
+	clone() const override;
+
+	std::string
+	get_name() const override;
+
+	static
+	std::string
+	mover_name();
+
+	static
+	void
+	provide_xml_schema( utility::tag::XMLSchemaDefinition & xsd );
+
+private: // methods
+
+private: // data
+
+};
+
+std::ostream &
+operator<<( std::ostream & os, ResetFullModelInfoMover const & mover );
+
+} //simple_moves
+} //protocols
+
+#endif //protocols_simple_moves_ResetFullModelInfoMover_HH
