12426fb10a6e98cb7a8542cda4879171d8c157f1
Rocco Moretti <rmorettiase@gmail.com>
2019-11-04 16:01:08 -0600

    Merge pull request #4334 from RosettaCommons/roccomoretti/pyrosetta_zeromq_autoupdate
    Update the PyRosetta build.py to automatically update the zeromq submodule.
    
    If we need the zeromq module to build PyRosetta, make sure it's updated.
    
    Old repository SHA1: 23e40ee7497985dbb5787ce1f4b59f1c204102a8

--- source/src/python/PyRosetta/build.py
+++ source/src/python/PyRosetta/build.py
@@ -562,7 +562,10 @@ def generate_bindings(rosetta_source_path):
     ''' Generate bindings using binder tools and return list of source files '''
     setup_source_directory_links(rosetta_source_path)
     maybe_version = '--version {} '.format(Options.version) if Options.version else ''
-    execute('Updating compilation submodules, version, options and residue-type-enum files...', 'cd {rosetta_source_path} && ./update_submodules.sh PyRosetta && ./version.py {maybe_version} && ./update_options.sh && ./update_ResidueType_enum_files.sh'.format(**vars()) )
+    submodule_extras = "PyRosetta"
+    if Options.zmq:
+        submodule_extras = submodule_extras + " zeromq"
+    execute('Updating compilation submodules, version, options and residue-type-enum files...', 'cd {rosetta_source_path} && ./update_submodules.sh {submodule_extras} && ./version.py {maybe_version} && ./update_options.sh && ./update_ResidueType_enum_files.sh'.format(**vars()) )
 
     prefix = get_binding_build_root(rosetta_source_path, source=True) + '/'
 
