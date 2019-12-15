72987a32b8c2a0ec0bdd372f5930c32eb75b0002
Brian Coventry <bcov@uw.edu>
2019-12-15 08:20:57 -0800

    Merge pull request #4419 from RosettaCommons/bcov/FixMaster2
    Fixing the clang tidy error in master
    
    Old repository SHA1: afa5dc96f1cdc0885ef67c01ac793e2c9ccd8ee3

--- source/src/core/energy_methods/ArgCationPiEnergyCreator.hh
+++ source/src/core/energy_methods/ArgCationPiEnergyCreator.hh
@@ -31,17 +31,15 @@ class ArgCationPiEnergyCreator : public scoring::methods::EnergyMethodCreator
 {
 public:
 	/// @brief Instantiate a new ArgCationPiEnergy
-	virtual
 	scoring::methods::EnergyMethodOP
 	create_energy_method(
 		scoring::methods::EnergyMethodOptions const &
-	) const;
+	) const override;
 
 	/// @brief Return the set of score types claimed by the EnergyMethod
 	/// this EnergyMethodCreator creates in its create_energy_method() function
-	virtual
 	scoring::ScoreTypes
-	score_types_for_method() const;
+	score_types_for_method() const override;
 
 };
 
