ee557b38ddd6016852a6352ce7d3d42f49b87598
Sergey Lyskov <3302736+lyskov@users.noreply.github.com>
2022-11-07 08:55:01 -0700

    increasing PyRosetta unit-test-suite timeout
    Old repository SHA1: 20a5bfe160d0310bb7266ea8124fa2b8c5d99c1c

--- tests/benchmark/tests/PyRosetta.py
+++ tests/benchmark/tests/PyRosetta.py
@@ -85,7 +85,7 @@ def run_unit_tests(rosetta_dir, working_dir, platform, config, hpc_driver=None, 
 
         #gui_flag = '--enable-gui' if platform['os'] == 'mac' else ''
         gui_flag, res, output = '', result.exitcode, result.output
-        command_line = f'{python_virtual_environment.activate} && cd {result.pyrosetta_path}/build && {python_virtual_environment.python} {rosetta_dir}/source/test/timelimit.py 32 {python_virtual_environment.python} self-test.py {gui_flag} -j{jobs}{additional_flags}'
+        command_line = f'{python_virtual_environment.activate} && cd {result.pyrosetta_path}/build && {python_virtual_environment.python} {rosetta_dir}/source/test/timelimit.py 64 {python_virtual_environment.python} self-test.py {gui_flag} -j{jobs}{additional_flags}'
         output += '\nRunning PyRosetta tests: ' + command_line + '\n'
 
         res, o = execute('Running PyRosetta tests...', command_line, return_='tuple')
