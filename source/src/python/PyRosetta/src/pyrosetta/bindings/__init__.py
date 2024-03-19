38e59a8b98db9e5862f5df88b8f06025d561756a
Jason C. Klima <klimaj@uw.edu>
2024-03-19 10:39:09 -0700

    Support python bitwise operators for residue selectors in PyRosetta (#33)

--- source/src/python/PyRosetta/src/pyrosetta/bindings/__init__.py
+++ source/src/python/PyRosetta/src/pyrosetta/bindings/__init__.py
@@ -2,3 +2,4 @@ import pyrosetta.bindings.pose
 import pyrosetta.bindings.energies
 import pyrosetta.bindings.homogeneous_transform
 import pyrosetta.bindings.rt
+import pyrosetta.bindings.residue_selectors
