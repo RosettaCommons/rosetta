702ef2a69c62e822680a688526f2f95a5de89dc5
Ginsparg <abgvg9@uab.edu>
2024-10-04 16:10:12 -0400

    till broken, trying more return statements Please enter the commit message for your changes. Lines starting

--- source/src/protocols/motifs/IdentifyLigandMotifs.cc
+++ source/src/protocols/motifs/IdentifyLigandMotifs.cc
@@ -753,9 +753,12 @@ utility::vector1< core::Real > IdentifyLigandMotifs::evaluate_motifs_of_pose(cor
 	//clone a non-pointer of working_pose_ to pass into process_for_motifs, this may resolve the pointer issue that seems to occur when I pass the PoseOP working_pose into process_for_motifs
 	core::pose::Pose working_pose_copy = *((*working_pose).clone());
 
+	return placement_motifs_data;
 
 	//use ilm process_for_motifs to obtain motifs from the pose
 	process_for_motifs(working_pose_copy, pdb_name, placement_library, prot_pos_that_made_motifs_size);
+
+	return placement_motifs_data;
 
 	//convert the prot_pos vector from size to int (easier to use int because this interacts with values from vectors that are pulled from args that don't seem to be able to be pulled as size; I can convert those to size, but this is a seemingly equivalent workaround)
 	utility::vector1< int > prot_pos_that_made_motifs = prot_pos_that_made_motifs_size;
