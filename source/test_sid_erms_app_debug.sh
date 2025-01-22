6a2f485dcb5a1ea44221be5fccef7da1ec3976d8
bolz.13 <bolz.13@asccbc-wklind01.asc.ohio-state.edu>
2025-01-22 14:29:48 -0500

    cleaning up top level directory and pilot app.cc for merging to main

--- source/test_sid_erms_app_debug.sh
+++
@@ -1,7 +0,0 @@
-./bin/SID_ERMS_Rescore.default.linuxgccdebug \
-    -database ../database \
-    -in::file::l ../inputs/inputs/in_list \
-    -out:file:o ../inputs/test_output.out \
-    -complex_type ../inputs/C2.sym \
-    -ERMS ../inputs/exp_ERMS.csv \
-    -RMSE true 
