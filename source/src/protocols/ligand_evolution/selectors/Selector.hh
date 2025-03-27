80dd00bc09d2543ce4b9a50f13c49219ee5cdd3b
Paul Eisenhuth <eisenhuth451@gmail.com>
2025-03-27 14:32:50 +0100

    Rosetta Evolutionary Ligand (REvoLd) (#303)
    This PR aims to include the newly developed application REvoLd into Rosetta.
    
    REvoLd preprint: https://arxiv.org/abs/2404.17329
    
    The algorithm optimizes small molecule ligands within combinatorial libraries like Enamine REAL. Most code changes are self enclosed and do not affect any functionality outside of the application. The only exception are changes to cmake compile directives for mpi which were broken.
    
    A guide on how to run REvoLd is available here: https://docs.rosettacommons.org/docs/wiki/revold
    
    Unit and integration test run locally and passed all.
    
    ---------
    
    Co-authored-by: Paul Eisenhuth <eisenhuth@informatik.uni-leipzig.de>

---
+++ source/src/protocols/ligand_evolution/selectors/Selector.hh
@@ -0,0 +1,47 @@
+// -*- mode:c++;tab-width:2;indent-tabs-mode:t;show-trailing-whitespace:t;rm-trailing-spaces:t -*-
+// vi: set ts=2 noet:
+//
+// (c) Copyright Rosetta Commons Member Institutions.
+// (c) This file is part of the Rosetta software suite and is made available under license.
+// (c) The Rosetta software is developed by the contributing members of the Rosetta Commons.
+// (c) For more information, see http://www.rosettacommons.org. Questions about this can be
+// (c) addressed to University of Washington CoMotion, email: license@uw.edu.
+
+/// @file   protocols/ligand_evolution/selectors/Selector.hh
+/// @brief  Class declaration for %Selector
+/// @author Paul Eisenhuth (eisenhuth451@gmail.com)
+
+#ifndef INCLUDED_protocols_ligand_evolution_Selector_HH
+#define INCLUDED_protocols_ligand_evolution_Selector_HH
+
+// unit headers
+#include <protocols/ligand_evolution/selectors/Selector.fwd.hh>
+
+// project headers
+#include <protocols/ligand_evolution/Population.hh>
+
+// utility headers
+#include <utility/VirtualBase.hh>
+#include <utility/vector1.hh>
+
+namespace protocols {
+namespace ligand_evolution {
+
+/// @brief: An abstract class to give an interface for subset selection of %Population
+/// @details comming soon.
+class Selector : public utility::VirtualBase {
+public:
+
+	/// @brief Applies the specified rules to create a list of indices of selected individuals
+	virtual utility::vector1< Individual > apply( Population& population, core::Size size, bool remove ) const = 0;
+
+	/// @brief Return the name of this selector
+	virtual std::string const& name() const = 0;
+
+};
+
+
+}
+}
+
+#endif
