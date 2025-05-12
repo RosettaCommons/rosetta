4519cf709fda4ea29c91303f4f3064cd5da7d518
Ginsparg <abgvg9@uab.edu>
2025-05-12 14:26:05 -0400

    fixing namespace issues

--- source/src/protocols/motifs/motif_utils.cc
+++ source/src/protocols/motifs/motif_utils.cc
@@ -1562,12 +1562,12 @@ void sanitize_motif_jump(core::kinematics::Jump & jump)
 }
 
 // @brief function to orthonormalize rotation of motif jump
-xyzMatrix<core::Real> orthonormalize_rotation(const xyzMatrix<core::Real> & R)
+numeric::xyzMatrix<core::Real> orthonormalize_rotation(const numeric::xyzMatrix<core::Real> & R)
 {
 	//extract rotation matrix columns
-	xyzVector<core::Real> col1 = R.get_column(1);
-	xyzVector<core::Real> col2 = R.get_column(2);
-	xyzVector<core::Real> col3 = R.get_column(3);
+	numeric::xyzVector<core::Real> col1 = R.get_column(1);
+	numeric::xyzVector<core::Real> col2 = R.get_column(2);
+	numeric::xyzVector<core::Real> col3 = R.get_column(3);
 
 	//orthonormalize first column (simply normalize)
 	col1.normalize();
@@ -1581,7 +1581,7 @@ xyzMatrix<core::Real> orthonormalize_rotation(const xyzMatrix<core::Real> & R)
 	col3.normalize();
 
 	//build a new orthonormalized matrix based on the orthonormalized columns
-	xyzMatrix<core::Real> R_orthonormalized;
+	numeric::xyzMatrix<core::Real> R_orthonormalized;
 	R_orthonormalized.set_column(1, col1);
 	R_orthonormalized.set_column(2, col2);
 	R_orthonormalized.set_column(3, col3);
