f2a21edb60c0517be35e64a18f39b21b45f92be3
Jason C. Klima <klimaj@uw.edu>
2025-06-28 14:14:24 -0700

    Update imports

--- source/src/python/PyRosetta/src/pyrosetta/bindings/__init__.py
+++ source/src/python/PyRosetta/src/pyrosetta/bindings/__init__.py
@@ -1,6 +1,7 @@
+import pyrosetta.bindings.warnings # Warnings are bound before other bindings so they are emitted from other bindings
+import pyrosetta.bindings.scores
 import pyrosetta.bindings.pose
 import pyrosetta.bindings.energies
 import pyrosetta.bindings.homogeneous_transform
 import pyrosetta.bindings.rt
 import pyrosetta.bindings.residue_selectors
-import pyrosetta.bindings.warnings
