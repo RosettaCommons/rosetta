41dc373e8f9fb14dd066c4d9ef0bba623809b7c7
Eric Bell <ewbell94@gmail.com>
2025-07-09 14:53:04 -0500

    Added stereochemistry call in rdkitConf

--- source/scripts/python/public/fake_rotlib.py
+++ source/scripts/python/public/fake_rotlib.py
@@ -196,6 +196,7 @@ def geomOptimize(mol, maxIter, mmff=False):
 
 #Generate conformations of NCAA for rotamer library
 def rdkitConf(ncaamol, noext, topN, instructions, mmff, nProc):
+    Chem.AssignStereochemistryFrom3D(ncaamol)
     print("Generating RDKit Conformations...")
     confs = AllChem.EmbedMultipleConfs(ncaamol, useRandomCoords=True, numConfs=int(10*topN), numThreads=0, useExpTorsionAnglePrefs=False)
     print("Aligning conformations...")
