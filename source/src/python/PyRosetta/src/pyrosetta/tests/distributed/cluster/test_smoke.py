4d738f4b8a9771c6d8f822f76080b647094327b1
Jason C. Klima <klimaj@uw.edu>
2025-08-01 22:09:01 -0700

    Set unittest.skipIf decorator for python 3.9 and 3.12

--- source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/test_smoke.py
+++ source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/test_smoke.py
@@ -878,7 +878,7 @@ class SerializationTest(unittest.TestCase):
 
 
 class MultipleClientsTest(unittest.TestCase):
-    @unittest.skip("Exit code 137 on benchmark server.")
+    @unittest.skipIf(sys.version_info.major == 3 and sys.version_info.minor in (9, 12), "Exit code 137 on benchmark server.")
     def test_clients(self):
         """Smoke test for the use case of multiple clients with PyRosettaCluster."""
         pyrosetta.distributed.init(
@@ -1107,6 +1107,7 @@ class ResourcesTest(unittest.TestCase):
                 scores_dict = get_scores_dict(decoy_file)
                 self.assertEqual(scores_dict["scores"]["sequence"], sequence)
 
+    @unittest.skipIf(sys.version_info.major == 3 and sys.version_info.minor in (9, 12), "Exit code 137 on benchmark server.")
     def test_resources_clients(self):
         """Smoke test for the use case of abstract resource constraints for dask workers with multiple clients in PyRosettaCluster."""
         pyrosetta.distributed.init(
@@ -1748,7 +1749,7 @@ class GeneratorTest(TestBase, unittest.TestCase):
             msg="Dry run failed while yielding results.",
         )
 
-    @unittest.skip("Exit code 137 on benchmark server.")
+    @unittest.skipIf(sys.version_info.major == 3 and sys.version_info.minor in (9, 12), "Exit code 137 on benchmark server.")
     def test_generate_partition_clients(self):
         """
         Test for `PyRosettaCluster().generate()` with `save_all=True`,
