c3360989c98a5776a8dcba7f43d49e7fcbe879ec
Jason C. Klima <klimaj@uw.edu>
2025-09-20 17:10:57 -0700

    Fix _get_stream_protocol for protocol <= 2

--- source/src/python/PyRosetta/src/pyrosetta/secure_unpickle.py
+++ source/src/python/PyRosetta/src/pyrosetta/secure_unpickle.py
@@ -510,4 +510,7 @@ class SecureSerializerBase(object):
 
     @staticmethod
     def _get_stream_protocol(obj: bytes) -> int:
-        return obj[1] if len(obj) >= 2 and obj[0] == pickle.PROTO[0] else -1
+        if len(obj) >= 2 and obj[0] == pickle.PROTO[0]:
+            return obj[1] if 0 <= obj[1] <= pickle.HIGHEST_PROTOCOL else -1
+        else:
+            return 0
