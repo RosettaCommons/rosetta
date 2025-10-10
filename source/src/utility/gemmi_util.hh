720d571d3ebc9204f18b8827856608697b941ee5
Rocco Moretti <rmorettiase@gmail.com>
2025-10-10 14:23:25 -0500

    Move functions to cc file.

--- source/src/utility/gemmi_util.hh
+++ source/src/utility/gemmi_util.hh
@@ -29,47 +29,17 @@ namespace utility {
 ///////////////  READING UTILS
 
 /// As the default as_char() is not robust to empty strings
-inline
 char
-as_char(std::string const & value, char null) {
-	if ( value.size() == 0 ) {
-		return null;
-	}
-	if ( value.size() == 2 && (value == "''"  || value == "\"\"" ) ) {
-		return null;
-	}
-	return gemmi::cif::as_char(value, null);
-}
+as_char(std::string const & value, char null);
 
 /// @brief find the index for the given column name in the table.
 /// If it can't be found, return a negative number
-inline int find_gemmi_column(gemmi::cif::Table & table, std::string const & name) {
-	if ( table.width() == 0 ) { return -1; } // No columns
-	gemmi::cif::Table::Row const & tags = table.tags();
-	for ( int ii = 0; ii < int(tags.size()); ++ii ) {
-		std::string const & tag = tags[ii];
-		if ( name == tag || name == tag.substr( table.prefix_length ) ) {
-			return ii;
-		}
-	}
-	return -1;
-}
+int find_gemmi_column(gemmi::cif::Table & table, std::string const & name);
 
 ///////////////  WRITING UTILS
 
-inline
 void
-normalize_table_name(std::string & table_name) {
-	if ( table_name.size() == 0 ) {
-		table_name = "_TABLE.";
-	}
-	if ( table_name[0] != '_' ) {
-		table_name = '_' + table_name;
-	}
-	if ( table_name[ table_name.size()-1 ] != '.' ) {
-		table_name = table_name + '.';
-	}
-}
+normalize_table_name(std::string & table_name);
 
 /// @brief Gets a table with the given name (cif category) and column names for writing
 /// If the table does not exist, create it.
@@ -78,31 +48,8 @@ normalize_table_name(std::string & table_name) {
 /// (The returned table is simply a view to the underlying Block.)
 ///
 /// Rows can be added with gemmi_append_row() below
-inline
 gemmi::cif::Table
-gemmi_get_table(gemmi::cif::Block & block, std::string table_name, std::vector<std::string> const & columns ) {
-	normalize_table_name(table_name);
-
-	if ( ! block.has_mmcif_category(table_name) ) {
-		block.init_loop(table_name, columns);
-	} else {
-		// Already has the table, make sure we have the columns.
-		block.find_mmcif_category(table_name).ensure_loop();
-		gemmi::cif::Loop * loop = block.find_mmcif_category(table_name).get_loop();
-		runtime_assert( loop != nullptr );
-		std::vector<std::string> new_tags;
-		for ( std::string const & tag: columns ) {
-			if ( ! loop->has_tag( table_name + tag ) ) {
-				new_tags.push_back( table_name + tag );
-			}
-		}
-		if ( ! new_tags.empty() ) {
-			loop->add_columns( new_tags, "?" );
-		}
-	}
-
-	return block.find( table_name, columns );
-}
+gemmi_get_table(gemmi::cif::Block & block, std::string table_name, std::vector<std::string> const & columns );
 
 template < class Iterable >
 void
@@ -127,21 +74,8 @@ gemmi_append_row(gemmi::cif::Table & table, std::initializer_list<std::string> c
 
 /// @brief Adds a new table (actually a 'Loop' object) to the given block, with the given column names
 /// Returns a reference to the newly added loop object (which can be augmented with the `gemmi_add_row()` function)
-inline
 gemmi::cif::Loop &
-gemmi_add_table(gemmi::cif::Block & block, std::string table_name, std::vector<std::string> const & columns) {
-	if ( table_name.size() == 0 ) {
-		table_name = "_TABLE.";
-	}
-	if ( table_name[0] != '_' ) {
-		table_name = '_' + table_name;
-	}
-	if ( table_name[ table_name.size()-1 ] != '.' ) {
-		table_name = table_name + '.';
-	}
-
-	return block.init_loop(table_name, columns);
-}
+gemmi_add_table(gemmi::cif::Block & block, std::string table_name, std::vector<std::string> const & columns);
 
 /// @brief Adds a row to the table. Takes care of quoting the entries properly
 template< class Iterable >
