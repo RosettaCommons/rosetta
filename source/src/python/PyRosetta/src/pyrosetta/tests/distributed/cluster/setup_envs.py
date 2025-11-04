720c114503148cfdce8b45244838dcfbd3f36696
Jason C. Klima <klimaj@uw.edu>
2025-11-04 15:36:31 -0800

    uv add pip

--- source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/setup_envs.py
+++ source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/setup_envs.py
@@ -115,9 +115,9 @@ def setup_uv_environment(env_dir):
     )
 
     # Install pyrosetta-installer
-    print("Adding 'pyrosetta-installer' to uv environment...")
+    print("Adding 'pyrosetta-installer' and 'pip' to uv environment...")
     subprocess.run(
-        ["uv", "add", "-p", str(env_path), "pyrosetta-installer"],
+        ["uv", "add", "-p", str(env_path), "pyrosetta-installer", "pip"],
         check=True,
     )
 
