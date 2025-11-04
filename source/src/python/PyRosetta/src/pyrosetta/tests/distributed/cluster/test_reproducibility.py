2ffa8d0d6be1a9f7045bded1af0b6ebb825c77a7
Jason C. Klima <klimaj@uw.edu>
2025-11-04 15:36:13 -0800

    Run python unbuffered

--- source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/test_reproducibility.py
+++ source/src/python/PyRosetta/src/pyrosetta/tests/distributed/cluster/test_reproducibility.py
@@ -1744,14 +1744,14 @@ class TestEnvironmentReproducibility(unittest.TestCase):
         original_output_path = os.path.join(original_env_dir, f"{environment_manager}_original_outputs")
         original_scorefile_name = "test_scores.json"
         if environment_manager == "pixi":
-            cmd = "pixi run python {0} --env_manager '{1}' --output_path '{2}' --scorefile_name '{3}'".format(
+            cmd = "pixi run python -u {0} --env_manager '{1}' --output_path '{2}' --scorefile_name '{3}'".format(
                 test_script,
                 environment_manager,
                 original_output_path,
                 original_scorefile_name,
             )
         elif environment_manager == "uv":
-            cmd = "uv run -p {0} python {1} --env_manager '{2}' --output_path '{3}' --scorefile_name '{4}'".format(
+            cmd = "uv run -p {0} python -u {1} --env_manager '{2}' --output_path '{3}' --scorefile_name '{4}'".format(
                 original_env_dir,
                 test_script,
                 environment_manager,
@@ -1759,7 +1759,7 @@ class TestEnvironmentReproducibility(unittest.TestCase):
                 original_scorefile_name,
             )
         elif environment_manager in ("conda", "mamba"):
-            cmd = "conda run -p {0} python {1} --env_manager '{2}' --output_path '{3}' --scorefile_name '{4}'".format(
+            cmd = "conda run -p {0} python -u {1} --env_manager '{2}' --output_path '{3}' --scorefile_name '{4}'".format(
                 original_env_dir,
                 test_script,
                 environment_manager,
@@ -1837,7 +1837,7 @@ class TestEnvironmentReproducibility(unittest.TestCase):
         reproduce_scorefile_name = "test_scores.json"
         if environment_manager == "pixi":
             cmd = (
-                f"pixi run python {test_script} "
+                f"pixi run python -u {test_script} "
                 f"--env_manager '{environment_manager}' "
                 f"--output_path '{reproduce_output_path}' "
                 f"--scorefile_name '{reproduce_scorefile_name}' "
@@ -1847,7 +1847,7 @@ class TestEnvironmentReproducibility(unittest.TestCase):
             )
         elif environment_manager == "uv":
             cmd = (
-                f"uv run -p {reproduce_env_dir} python {test_script} "
+                f"uv run -p {reproduce_env_dir} python -u {test_script} "
                 f"--env_manager '{environment_manager}' "
                 f"--output_path '{reproduce_output_path}' "
                 f"--scorefile_name '{reproduce_scorefile_name}' "
@@ -1857,7 +1857,7 @@ class TestEnvironmentReproducibility(unittest.TestCase):
             )
         elif environment_manager in ("conda", "mamba"):
             cmd = (
-                f"conda run -p {reproduce_env_dir} python {test_script} "
+                f"conda run -p {reproduce_env_dir} python -u {test_script} "
                 f"--env_manager '{environment_manager}' "
                 f"--output_path '{reproduce_output_path}' "
                 f"--scorefile_name '{reproduce_scorefile_name}' "
