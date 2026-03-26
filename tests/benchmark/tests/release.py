a32b37594da7a413b5e162b3c93d851d5d4941fd
Sergey Lyskov <3302736+lyskov@users.noreply.github.com>
2026-03-26 13:15:00 -0600

    update PyRosetta release links (#617)

--- tests/benchmark/tests/release.py
+++ tests/benchmark/tests/release.py
@@ -114,6 +114,16 @@ def release(name, package_name, package_dir, working_dir, platform, config, rele
                 shutil.move(archive, release_path + '/' + os.path.basename(archive) )
 
 
+                # creating a local symlink with simple `latest` ie: `pyrosetta-latest-cp39-cp39-macosx_12_0_arm64.whl` --> to exact version from `pyrosetta-2025.41+release.de3cc17d50-cp39-cp39-macosx_12_0_arm64.whl`
+                symlink_frst_parition = os.path.basename(archive).partition('-')
+                symlink_path = release_path + '/' + symlink_frst_parition[0] + '-0-cp' + symlink_frst_parition[2].partition('-cp')[2]
+
+                if os.path.islink(symlink_path): os.unlink(symlink_path)
+                os.symlink(os.path.basename(archive), symlink_path)
+
+
+
+
         # removing old archives and adjusting _latest_html_
         files = [f for f in os.listdir(release_path) if f != _latest_html_  and  f[0] != '.' ]
         files.sort(key=lambda f: os.path.getmtime(release_path+'/'+f))
