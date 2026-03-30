2dd1fe6a9853ee7723b2104feca2f7c13e1e1a9c
Jason C. Klima <klimaj@uw.edu>
2026-03-30 14:03:12 -0700

    Fix spacing

--- source/src/python/PyRosetta/src/pyrosetta/distributed/cluster/converters.py
+++ source/src/python/PyRosetta/src/pyrosetta/distributed/cluster/converters.py
@@ -864,5 +864,4 @@ def _parse_output_scorefile_types(objs: Any) -> Union[List[str], NoReturn]:
 
 def _version_tuple_to_str(version_tuple: Tuple[int, int, int]) -> str:
     """Return a version string from a version tuple."""
-
     return ".".join(map(str, version_tuple))
