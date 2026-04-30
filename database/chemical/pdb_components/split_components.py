137845187959248a12cf10e5b5ce9e9a55d81f1c
Rocco Moretti <rmorettiase@gmail.com>
2026-04-30 18:58:44 -0500

    Futher split the A's, due to new five-letter CCD codes.

--- database/chemical/pdb_components/split_components.py
+++ database/chemical/pdb_components/split_components.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
-# components.cif is 0-9, A-Z, so we don't need to store in a dict temporarily
+stem = None
+outf = None
 
-line5 = "0"
-outf = open("components.0.cif", "w")
+def get_stem(line):
+    ccd_id = line.split("_")[1].strip()
+    if len(ccd_id) <= 3:
+        return ccd_id[0]
+    elif len(ccd_id) <= 5 :
+        return ccd_id[:-2]
+    else:
+        raise ValueError(f"ISSUE WITH CCD IDs -- they shouldn't be more than five characters yet. (Got {ccd_id}) Likely formatting issue in file!")
 
 with open("components.cif") as f:
     for line in f:
         if line[0:5] == "data_":
-            if line[5] != line5:
-                outf.close()
-                line5 = line[5]
-                outf = open("components.{}.cif".format(line5), "w")
+            if get_stem(line) != stem:
+                if outf is not None:
+                    outf.close()
+                stem = get_stem(line)
+                outf = open("components.{}.cif".format(stem), "a")
         outf.write(line)
--- database/chemical/pdb_components/update_components.sh
+++ database/chemical/pdb_components/update_components.sh
@@ -2,5 +2,6 @@
 
 curl -o ./components.cif.gz https://files.wwpdb.org/pub/pdb/data/monomers/components.cif.gz 
 gunzip components.cif.gz
+rm components.*.cif
 python split_components.py
 rm components.cif
